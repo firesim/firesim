864b2338d76c809163c0d827bad386b6ad45b606
Donggyu Kim <dgkim@eecs.berkeley.edu>
2014-09-24 01:52:25 -0700

    [MIDAS] fix Makefile

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,7 +7,7 @@ object FAEE {
   def main(args: Array[String]) {
     val chiselArgs = args.slice(1, args.length)
     val res = args(0) match {
-      case "GCD" =>
+      case "GCDWrapper" =>
         chiselMainTest(chiselArgs, () => DaisyWrapper(new GCD))(
           c => new GCDDaisyTests(c))
       case _ =>
