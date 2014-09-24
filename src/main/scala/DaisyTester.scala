2a18d622f3818f429af6a5486eb77208cb889738
Donggyu Kim <dgkim@eecs.berkeley.edu>
2014-09-24 00:33:13 -0700

    [MIDAS] read & verify daisy chains

--- src/main/scala/DaisyTester.scala
+++ src/main/scala/DaisyTester.scala
@@ -1,9 +1,12 @@
 package faee
 
 import Chisel._
+import scala.collection.mutable.ArrayBuffer
 
 abstract class DaisyTester[+T <: DaisyWrapper[Module]](c: T, isTrace: Boolean = true) extends Tester(c, isTrace) {
   val ioMap = DaisyBackend.ioMap
+  val stateNames = ArrayBuffer[String]()
+  val stateWidths = ArrayBuffer[Int]()
 
   override def poke(data: Bits, x: BigInt) {
     if (ioMap contains data) {
@@ -21,6 +24,11 @@ abstract class DaisyTester[+T <: DaisyWrapper[Module]](c: T, isTrace: Boolean = 
     }
   }
 
+  def peek(name: String) = {
+    val cmd = "wire_peek " + name
+    Literal.toLitVal(emulatorCmd(cmd))
+  }
+
   override def expect(data: Bits, expected: BigInt) = {
     if (ioMap contains data) {
       super.expect(ioMap(data), expected)
@@ -34,12 +42,52 @@ abstract class DaisyTester[+T <: DaisyWrapper[Module]](c: T, isTrace: Boolean = 
   }
 
   def pokeSteps(n: Int) {
+    if (isTrace) println("Poke Steps")
     while(peek(c.io.stepsIn.ready) == 0)
       takeSteps(1)
     poke(c.io.stepsIn.bits, n)
     poke(c.io.stepsIn.valid, 1)
     takeSteps(1)
     poke(c.io.stepsIn.valid, 0)
+    if (isTrace) println("==========")
+  }
+
+
+  def readDaisyChain(out: DecoupledIO[UInt]) = {
+    val chain = new StringBuilder
+
+    if (isTrace) println("Read Daisy Chain")
+    do {
+      poke(out.ready, 1)
+      takeSteps(1)
+    } while(peek(dumpName(out.valid)) == 0)
+
+    var count = 0
+    do {
+      val bit = peek(dumpName(out.bits))
+      chain append "%d".format(bit)
+      takeSteps(1)
+      count += 1
+    } while(peek(dumpName(out.valid)) == 1)
+    if (isTrace) println("Read Count = %d".format(count))
+
+    chain.result
+  }
+  
+  def verifyDaisyChain(chain: String) {
+    val values = ArrayBuffer[BigInt]()
+    for (name <- stateNames) {
+      values += peek(name)
+    }
+
+    var start = 0
+    for ((width, i) <- stateWidths.zipWithIndex ; if start < chain.length) {
+      val end = math.min(start + width, chain.length)
+      val value = values(i).toString(2).reverse.padTo(width, '0').reverse
+      expect(value == chain.substring(start, end), stateNames(i))
+      println("%s %s".format(value, chain.substring(start, end)))
+      start += end
+    }
   }
 
   def daisyStep(n: Int) {
@@ -53,6 +101,23 @@ abstract class DaisyTester[+T <: DaisyWrapper[Module]](c: T, isTrace: Boolean = 
     val target = t + n
     daisyStep(n)
     if (isTrace) println("STEP " + n + " -> " + target)
+    val chain = readDaisyChain(c.io.stateOut)
+    verifyDaisyChain(chain)
     t += n
-  }  
+  }
+
+  def readChainFile {
+    val targetPath = c.target.getPathName(".")
+    val targetPrefix = Driver.backend.extractClassName(c.target)
+    val filename = targetPrefix + ".state.chain"
+    val basedir = ensureDir(Driver.targetDir)
+    val lines = scala.io.Source.fromFile(basedir + "/" + filename).getLines
+    for (line <- lines) {
+      val tokens = line split " "
+      stateNames += (targetPath + (tokens.head stripPrefix targetPrefix))
+      stateWidths += tokens.last.toInt 
+    }
+  }
+
+  readChainFile 
 }
