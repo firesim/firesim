801f51dfce0d7fbb7f26d60c76a195910e6a1011
Donggyu Kim <dgkim@eecs.berkeley.edu>
2014-12-09 16:36:48 -0800

    [MIDAS] initial commit for daisy

--- project/build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-import Keys._
-
-object DebugBuild extends Build {
-  lazy val chisel   = Project("chisel",   base=file("chisel"))
-  lazy val tutorial = Project("tutorial", base=file("tutorial/examples")).dependsOn(chisel)
-  lazy val mini     = Project("riscv-mini", base=file("riscv-mini")).dependsOn(chisel)
-  lazy val root     = Project("debug-machine", base=file(".")).dependsOn(tutorial, mini)
-}
