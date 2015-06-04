0d8ec8c87f145022d3e304615cc578eb190c5bcf
Donggyu Kim <dgkim@eecs.berkeley.edu>
2015-06-03 18:20:56 -0700

    support tokens greater than the axi bus width

--- src/main/scala/tester.scala
+++ src/main/scala/tester.scala
@@ -96,49 +96,60 @@ abstract class SimAXI4WrapperTester[+T <: SimAXI4Wrapper[SimNetwork]](c: T, isTr
   extends SimTester(c, isTrace) {
   val inMap = c.inMap
   val outMap = c.outMap
+  val inWidths = c.inMap map { case (k, v) => (v, k.needWidth) }
+  val outWidths = c.outMap map { case (k, v) => (v, k.needWidth) }
+  val mask = (BigInt(1) << c.axiDataWidth) - 1
 
   def pokeChannel(addr: Int, data: BigInt) {
-    do {
-      poke(c.io.M_AXI.aw.bits.id, 0)
-      poke(c.io.M_AXI.aw.bits.addr, addr << 2)
-      poke(c.io.M_AXI.aw.valid, 1)
-      poke(c.io.M_AXI.w.bits.data, data)
-      poke(c.io.M_AXI.w.valid, 1)
-      takeSteps(1)
-    } while (peek(c.io.M_AXI.aw.ready) == 0 || peek(c.io.M_AXI.w.ready) == 0)
+    val limit = (inWidths(addr) - 1) / c.axiDataWidth + 1
+    for (i <- limit - 1 to 0 by -1) {
+      val maskedData = (data >> (i * c.axiDataWidth)) & mask
+      do {
+        poke(c.io.M_AXI.aw.bits.id, 0)
+        poke(c.io.M_AXI.aw.bits.addr, addr << 2)
+        poke(c.io.M_AXI.aw.valid, 1)
+        poke(c.io.M_AXI.w.bits.data, maskedData)
+        poke(c.io.M_AXI.w.valid, 1)
+        takeSteps(1)
+      } while (peek(c.io.M_AXI.aw.ready) == 0 || peek(c.io.M_AXI.w.ready) == 0)
 
-    do {
-      poke(c.io.M_AXI.aw.valid, 0)
-      poke(c.io.M_AXI.w.valid, 0)
-      takeSteps(1)
-    } while (peek(c.io.M_AXI.b.valid) == 0)
+      do {
+        poke(c.io.M_AXI.aw.valid, 0)
+        poke(c.io.M_AXI.w.valid, 0)
+        takeSteps(1)
+      } while (peek(c.io.M_AXI.b.valid) == 0)
 
-    assert(peek(c.io.M_AXI.b.bits.id) == 0)
-    poke(c.io.M_AXI.b.ready, 1)
-    takeSteps(1)
-    poke(c.io.M_AXI.b.ready, 0)
+      assert(peek(c.io.M_AXI.b.bits.id) == 0)
+      poke(c.io.M_AXI.b.ready, 1)
+      takeSteps(1)
+      poke(c.io.M_AXI.b.ready, 0)
+    }
   }
 
   def peekChannel(addr: Int) = {
-    while (peek(c.io.M_AXI.ar.ready) == 0) {
+    var data = BigInt(0)
+    val limit = (outWidths(addr) - 1) / c.axiDataWidth + 1
+    for (i <- 0 until limit) {
+      while (peek(c.io.M_AXI.ar.ready) == 0) {
+        takeSteps(1)
+      }
+
+      poke(c.io.M_AXI.ar.bits.addr, addr << 2)
+      poke(c.io.M_AXI.ar.bits.id, 0)
+      poke(c.io.M_AXI.ar.valid, 1)
       takeSteps(1)
-    }
+      poke(c.io.M_AXI.ar.valid, 0)
 
-    poke(c.io.M_AXI.ar.bits.addr, addr << 2)
-    poke(c.io.M_AXI.ar.bits.id, 0)
-    poke(c.io.M_AXI.ar.valid, 1)
-    takeSteps(1)
-    poke(c.io.M_AXI.ar.valid, 0)
+      while (peek(c.io.M_AXI.r.valid) == 0) {
+        takeSteps(1)
+      }
 
-    while (peek(c.io.M_AXI.r.valid) == 0) {
+      data |= peek(c.io.M_AXI.r.bits.data) << (i * c.axiDataWidth)
+      assert(peek(c.io.M_AXI.r.bits.id) == 0)
+      poke(c.io.M_AXI.r.ready, 1)
       takeSteps(1)
+      poke(c.io.M_AXI.r.ready, 0)
     }
-
-    val value = peek(c.io.M_AXI.r.bits.data)
-    assert(peek(c.io.M_AXI.r.bits.id) == 0)
-    poke(c.io.M_AXI.r.ready, 1)
-    takeSteps(1)
-    poke(c.io.M_AXI.r.ready, 0)
-    value
+    data
   }
 }
