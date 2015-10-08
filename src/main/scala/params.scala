88ec48a9f4fd09b9e8ab2a51888acce8551b6f26
Donggyu Kim <dgkim@eecs.berkeley.edu>
2015-10-08 12:54:26 -0700

    clean up memory stuff

--- src/main/scala/params.scala
+++ src/main/scala/params.scala
@@ -1,6 +1,7 @@
 package strober
 
 import Chisel._
+import junctions.{MIFAddrBits, MIFDataBits, MIFTagBits, MIFDataBeats}
 
 // AXI Params
 case object MAXIAddrWidth extends Field[Int]
@@ -15,12 +16,7 @@ case object SAXIDataWidth extends Field[Int]
 case object SAXITagWidth extends Field[Int]
 
 // Mem Params
-case object MemDataCount extends Field[Int]
-case object MemDataWidth extends Field[Int]
-case object MemAddrWidth extends Field[Int]
-case object MemTagWidth extends Field[Int]
-case object MemBlockSize extends Field[Int]
-case object MemBlockOffset extends Field[Int]
+case object MemBlockBytes extends Field[Int]
 
 // Simulation Params
 case object SampleNum extends Field[Int]
@@ -31,27 +27,27 @@ object AXI4Params {
   val mask = (key: Any, site: View, here: View, up: View) => key match {
     case MAXIAddrWidth => Dump("AXI_ADDR_WIDTH", 32)
     case MAXIDataWidth => Dump("AXI_DATA_WIDTH", 32)
-    case MAXITagWidth => 12
-    case MAXIAddrSize => 10
+    case MAXITagWidth  => 12
+    case MAXIAddrSize  => 10
     case MAXIAddrOffset => log2Up(site(MAXIAddrWidth)>>3)
     case ResetAddr => Dump("RESET_ADDR", (1 << site(MAXIAddrSize)) - 1)
     case SRAMRestartAddr => Dump("SRAM_RESTART_ADDR", site(ResetAddr) - 1)
     case SAXIAddrWidth => 32 
     case SAXIDataWidth => 64 
     case SAXITagWidth => 6
-    case MemAddrWidth => Dump("MEM_ADDR_WIDTH", 32)
-    case MemDataWidth => Dump("MEM_DATA_WIDTH", 16 << 3)
-    case MemDataCount => Dump("MEM_DATA_COUNT", (site(MemBlockSize) << 3) / site(MemDataWidth))
-    case MemTagWidth => 5
-    case MemBlockSize => 16 // in bytes
-    case MemBlockOffset => Dump("MEM_BLOCK_OFFSET", log2Up(site(MemBlockSize))) // bit width 
+
+    case MIFAddrBits => Dump("MEM_ADDR_WIDTH", 32)
+    case MIFDataBits => Dump("MEM_DATA_WIDTH", 16 << 3)
+    case MIFTagBits => 5
+    case MIFDataBeats => 0
+    case MemBlockBytes  => 16
   }
 }
 
 object SimParams {
   val mask = (key: Any, site: View, here: View, up: View) => key match {
-    case SampleNum => Dump("SAMPLE_NUM", 30)
-    case TraceLen => Dump("TRACE_LEN", 16)
+    case SampleNum  => Dump("SAMPLE_NUM",  30)
+    case TraceLen   => Dump("TRACE_LEN",   16)
     case DaisyWidth => Dump("DAISY_WIDTH", 32)
   }
 }
