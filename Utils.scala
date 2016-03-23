fe59210eae1861160b66d6703b4b339f7670006a
jackkoenig <jack.koenig3@eecs.berkeley.edu>
2016-03-23 15:47:03 -0700

    Update to new FIRRTL spec, add tests, add submodules for testing.

--- Utils.scala
+++ Utils.scala
@@ -1,6 +1,7 @@
 
 package midas
 
+//import Chisel._
 import firrtl._
 import firrtl.Utils._
 
@@ -23,86 +24,64 @@ object Utils {
   //private def getStmts[A <: Stmt](s: Stmt): Seq[A] = {
   //  s match {
   //    case a: A => Seq(a)
-  //    case b: Block => b.stmts.map(getStmts[A]).flatten
+  //    case b: Begin => b.stmts.map(getStmts[A]).flatten
   //    case _ => Seq()
   //  }
   //}
-  //private def getStmts[A <: Stmt](m: Module): Seq[A] = getStmts[A](m.stmt)
+  //private def getStmts[A <: Stmt](m: InModule): Seq[A] = getStmts[A](m.stmt)
 
-  def getDefRegs(s: Stmt): Seq[DefReg] = {
+  def getDefRegs(s: Stmt): Seq[DefRegister] = {
     s match {
-      case r: DefReg => Seq(r)
-      case b: Block => b.stmts.map(getDefRegs).flatten
+      case r: DefRegister => Seq(r)
+      case b: Begin => b.stmts.map(getDefRegs).flatten
       case _ => Seq()
     }
   }
-  def getDefRegs(m: Module): Seq[DefReg] = getDefRegs(m.stmt)
+  def getDefRegs(m: InModule): Seq[DefRegister] = getDefRegs(m.body)
 
-  def getDefInsts(s: Stmt): Seq[DefInst] = {
+  def getDefInsts(s: Stmt): Seq[DefInstance] = {
     s match {
-      case i: DefInst => Seq(i)
-      case b: Block => b.stmts.map(getDefInsts).flatten
+      case i: DefInstance => Seq(i)
+      case b: Begin => b.stmts.map(getDefInsts).flatten
       case _ => Seq()
     }
   }
-  def getDefInsts(m: Module): Seq[DefInst] = getDefInsts(m.stmt)
-
-  def getDefInstRef(inst: DefInst): Ref = {
-    inst.module match {
-      case ref: Ref => ref
-      case _ => throw new Exception("Invalid module expression for DefInst: " + inst.serialize)
+  def getDefInsts(m: Module): Seq[DefInstance] =
+    m match { case m: InModule => getDefInsts(m.body)
+      case m: ExModule => Seq()
     }
-  }
-
-  // DefInsts have an Expression for the module, this expression should be a reference this 
-  //   reference has a tpe that should be a bundle representing the IO of that module class
-  def getDefInstType(inst: DefInst): BundleType = {
-    val ref = getDefInstRef(inst)
-    ref.tpe match {
-      case b: BundleType => b
-      case _ => throw new Exception("Invalid reference type for DefInst: " + inst.serialize)
-    }
-  }
-
-  def getModuleFromDefInst(nameToModule: Map[String, Module], inst: DefInst): Module = {
-    val instModule = getDefInstRef(inst)
-    if(!nameToModule.contains(instModule.name))
-      throw new Exception(s"Module ${instModule.name} not found in circuit!")
-    else 
-      nameToModule(instModule.name)
-  }
 
   // Takes a set of strings or ints and returns equivalent expression node
   //   Strings correspond to subfields/references, ints correspond to indexes
   // eg. Seq(io, port, ready)    => io.port.ready
   //     Seq(io, port, 5, valid) => io.port[5].valid
   //     Seq(3)                  => UInt("h3")
-  def buildExp(names: Seq[Any]): Exp = {
-    def rec(names: Seq[Any]): Exp = {
+  def buildExp(names: Seq[Any]): Expression = {
+    def rec(names: Seq[Any]): Expression = {
       names.head match {
         // Useful for adding on indexes or subfields
-        case head: Exp => head 
+        case head: Expression => head
         // Int -> UInt/SInt/Index
-        case head: Int => 
+        case head: Int =>
           if( names.tail.isEmpty ) // Is the UInt/SInt inference good enough?
-            if( head > 0 ) UIntValue(head, UnknownWidth) else SIntValue(head, UnknownWidth)
-          else Index(rec(names.tail), head, UnknownType)
+            if( head > 0 ) UIntValue(head, UnknownWidth()) else SIntValue(head, UnknownWidth())
+          else WSubIndex(rec(names.tail), head, UnknownType(), UNKNOWNGENDER)
         // String -> Ref/Subfield
-        case head: String => 
-          if( names.tail.isEmpty ) Ref(head, UnknownType)
-          else Subfield(rec(names.tail), head, UnknownType)
+        case head: String =>
+          if( names.tail.isEmpty ) Ref(head, UnknownType())
+          else WSubField(rec(names.tail), head, UnknownType(), UNKNOWNGENDER)
         case _ => throw new Exception("Invalid argument type to buildExp! " + names)
       }
     }
     rec(names.reverse) // Let user specify in more natural format
   }
-  def buildExp(name: Any): Exp = buildExp(Seq(name))
+  def buildExp(name: Any): Expression = buildExp(Seq(name))
 
-  def genPrimopReduce(op: Primop, args: Seq[Exp]): Exp = {
+  def genPrimopReduce(op: PrimOp, args: Seq[Expression]): Expression = {
     if( args.length == 0 ) throw new Exception("genPrimopReduce called on empty sequence!")
     else if( args.length == 1 ) args.head
-    else if( args.length == 2 ) DoPrimop(op, Seq(args.head, args.last), Seq(), UnknownType)
-    else DoPrimop(op, Seq(args.head, genPrimopReduce(op, args.tail)), Seq(), UnknownType)
+    else if( args.length == 2 ) DoPrim(op, Seq(args.head, args.last), Seq(), UnknownType())
+    else DoPrim(op, Seq(args.head, genPrimopReduce(op, args.tail)), Seq(), UnknownType())
   }
 
   // Checks if a firrtl.Port matches the MIDAS SimPort pattern
@@ -113,8 +92,8 @@ object Utils {
     port.tpe match {
       case b: BundleType => {
         b.fields map { field =>
-          if( field.name == "hostIn" ) field.dir == Reverse
-          else if( field.name == "hostOut" ) field.dir == Default
+          if( field.name == "hostIn" ) field.flip == REVERSE
+          else if( field.name == "hostOut" ) field.flip == DEFAULT
           else false
         } reduce ( _ & _ )
       }
@@ -131,7 +110,7 @@ object Utils {
     }
   }
 
-  // From simulation host decoupled, return hostbits field 
+  // From simulation host decoupled, return hostbits field
   def getHostBits(field: Field): Field = {
     try {
       val b = field.tpe.asInstanceOf[BundleType]
@@ -162,160 +141,99 @@ object Utils {
     rec(tpe, Seq[Any](), Seq[Seq[Any]]())
   }
 
+  // MIDAS Queue, ideally we could directly use Chisel instead of a FIRRTL string
+
   // Queue
   // TODO
+  //  - Make more robust (use Chisel instead of FIRRTL string?)
   //  - Insert null tokens upon hostReset (or should this be elsewhere?)
-  def buildSimQueue(name: String, tpe: Type): Module = {
+  def buildSimQueue(name: String, tpe: Type, init: Boolean = true): Module = {
     val scopeSpaces = " " * 4 // Spaces before lines in module scope, for default assignments
     val templatedQueue =
-//      """
-//    circuit `NAME:
-//      module `NAME : 
-//        input hostClock : Clock
-//        input hostReset : UInt<1>
-//        output io : {flip enq : {flip hostReady : UInt<1>, hostValid : UInt<1>, hostBits : `TYPE}, deq : {flip hostReady : UInt<1>, hostValid : UInt<1>, hostBits : `TYPE}, count : UInt<3>}
-//        
-//        io.count := UInt<1>("h00")
-//        `DEFAULT_ASSIGN
-//        io.deq.hostValid := UInt<1>("h00")
-//        io.enq.hostReady := UInt<1>("h00")
-//        cmem ram : `TYPE[4], hostClock
-//        reg T_80 : UInt<2>, hostClock, hostReset
-//        onreset T_80 := UInt<2>("h00")
-//        reg T_82 : UInt<2>, hostClock, hostReset
-//        onreset T_82 := UInt<2>("h00")
-//        reg maybe_full : UInt<1>, hostClock, hostReset
-//        onreset maybe_full := UInt<1>("h00")
-//        node ptr_match = eq(T_80, T_82)
-//        node T_87 = eq(maybe_full, UInt<1>("h00"))
-//        node empty = and(ptr_match, T_87)
-//        node full = and(ptr_match, maybe_full)
-//        node maybe_flow = and(UInt<1>("h00"), empty)
-//        node do_flow = and(maybe_flow, io.deq.hostReady)
-//        node T_93 = and(io.enq.hostReady, io.enq.hostValid)
-//        node T_95 = eq(do_flow, UInt<1>("h00"))
-//        node do_enq = and(T_93, T_95)
-//        node T_97 = and(io.deq.hostReady, io.deq.hostValid)
-//        node T_99 = eq(do_flow, UInt<1>("h00"))
-//        node do_deq = and(T_97, T_99)
-//        when do_enq :
-//          infer accessor T_101 = ram[T_80]
-//          T_101 <> io.enq.hostBits
-//          node T_109 = eq(T_80, UInt<2>("h03"))
-//          node T_111 = and(UInt<1>("h00"), T_109)
-//          node T_114 = addw(T_80, UInt<1>("h01"))
-//          node T_115 = mux(T_111, UInt<1>("h00"), T_114)
-//          T_80 := T_115
-//          skip
-//        when do_deq :
-//          node T_117 = eq(T_82, UInt<2>("h03"))
-//          node T_119 = and(UInt<1>("h00"), T_117)
-//          node T_122 = addw(T_82, UInt<1>("h01"))
-//          node T_123 = mux(T_119, UInt<1>("h00"), T_122)
-//          T_82 := T_123
-//          skip
-//        node T_124 = neq(do_enq, do_deq)
-//        when T_124 :
-//          maybe_full := do_enq
-//          skip
-//        node T_126 = eq(empty, UInt<1>("h00"))
-//        node T_128 = and(UInt<1>("h00"), io.enq.hostValid)
-//        node T_129 = or(T_126, T_128)
-//        io.deq.hostValid := T_129
-//        node T_131 = eq(full, UInt<1>("h00"))
-//        node T_133 = and(UInt<1>("h00"), io.deq.hostReady)
-//        node T_134 = or(T_131, T_133)
-//        io.enq.hostReady := T_134
-//        infer accessor T_135 = ram[T_82]
-//        wire T_149 : `TYPE
-//        T_149 <> T_135
-//        when maybe_flow :
-//          T_149 <> io.enq.hostBits
-//          skip
-//        io.deq.hostBits <> T_149
-//        node ptr_diff = subw(T_80, T_82)
-//        node T_157 = and(maybe_full, ptr_match)
-//        node T_158 = cat(T_157, ptr_diff)
-//        io.count := T_158
-//      """
-      """
-circuit `NAME:
-  module `NAME : 
+    """
+circuit `NAME :
+  module `NAME :
     input hostClock : Clock
     input hostReset : UInt<1>
     output io : {flip enq : {flip hostReady : UInt<1>, hostValid : UInt<1>, hostBits : `TYPE}, deq : {flip hostReady : UInt<1>, hostValid : UInt<1>, hostBits : `TYPE}, count : UInt<3>}
-    
-    io.count := UInt<1>("h00")
-    `DEFAULT_ASSIGN
-    io.deq.hostValid := UInt<1>("h00")
-    io.enq.hostReady := UInt<1>("h00")
-    cmem ram : `TYPE[4], hostClock
-    reg T_404 : UInt<2>, hostClock, hostReset
-    onreset T_404 := UInt<2>("h00")
-    reg T_406 : UInt<2>, hostClock, hostReset
-    onreset T_406 := UInt<2>("h00")
-    reg maybe_full : UInt<1>, hostClock, hostReset
-    onreset maybe_full := UInt<1>("h00")
-    reg add_token_on_reset : UInt<1>, hostClock, hostReset
-    onreset add_token_on_reset := UInt<1>("h01")
-    add_token_on_reset := UInt<1>("h00")
-    node ptr_match = eq(T_404, T_406)
-    node T_414 = eq(maybe_full, UInt<1>("h00"))
-    node empty = and(ptr_match, T_414)
+
+    io is invalid
+    cmem ram : `TYPE[4]
+    reg T_53 : UInt<2>, hostClock with : (reset => (hostReset, UInt<2>("h00")))
+    reg T_55 : UInt<2>, hostClock with : (reset => (hostReset, UInt<2>("h00")))
+    reg maybe_full : UInt<1>, hostClock with : (reset => (hostReset, UInt<1>("h00")))
+    node ptr_match = eq(T_53, T_55)
+    node T_60 = eq(maybe_full, UInt<1>("h00"))
+    node empty = and(ptr_match, T_60)
     node full = and(ptr_match, maybe_full)
     node maybe_flow = and(UInt<1>("h00"), empty)
     node do_flow = and(maybe_flow, io.deq.hostReady)
-    node T_420 = and(io.enq.hostReady, io.enq.hostValid)
-    node T_422 = eq(do_flow, UInt<1>("h00"))
-    node do_enq = and(T_420, T_422)
-    node T_424 = and(io.deq.hostReady, io.deq.hostValid)
-    node T_426 = eq(do_flow, UInt<1>("h00"))
-    node do_deq = and(T_424, T_426)
-    node T_428 = or(do_enq, add_token_on_reset)
-    when T_428 :
-      infer accessor T_443 = ram[T_404]
-      T_443 := io.enq.hostBits
-      node T_473 = eq(T_404, UInt<2>("h03"))
-      node T_475 = and(UInt<1>("h00"), T_473)
-      node T_478 = addw(T_404, UInt<1>("h01"))
-      node T_479 = mux(T_475, UInt<1>("h00"), T_478)
-      T_404 := T_479
+    node T_66 = and(io.enq.hostReady, io.enq.hostValid)
+    node T_68 = eq(do_flow, UInt<1>("h00"))
+    node do_enq = and(T_66, T_68)
+    node T_70 = and(io.deq.hostReady, io.deq.hostValid)
+    node T_72 = eq(do_flow, UInt<1>("h00"))
+    node do_deq = and(T_70, T_72)
+    when do_enq :
+      infer mport T_74 = ram[T_53], hostClock
+      T_74 <- io.enq.hostBits
+      node T_79 = eq(T_53, UInt<2>("h03"))
+      node T_81 = and(UInt<1>("h00"), T_79)
+      node T_84 = add(T_53, UInt<1>("h01"))
+      node T_85 = tail(T_84, 1)
+      node T_86 = mux(T_81, UInt<1>("h00"), T_85)
+      T_53 <= T_86
       skip
     when do_deq :
-      node T_481 = eq(T_406, UInt<2>("h03"))
-      node T_483 = and(UInt<1>("h00"), T_481)
-      node T_486 = addw(T_406, UInt<1>("h01"))
-      node T_487 = mux(T_483, UInt<1>("h00"), T_486)
-      T_406 := T_487
+      node T_88 = eq(T_55, UInt<2>("h03"))
+      node T_90 = and(UInt<1>("h00"), T_88)
+      node T_93 = add(T_55, UInt<1>("h01"))
+      node T_94 = tail(T_93, 1)
+      node T_95 = mux(T_90, UInt<1>("h00"), T_94)
+      T_55 <= T_95
       skip
-    node T_488 = neq(do_enq, do_deq)
-    when T_488 :
-      maybe_full := do_enq
+    node T_96 = neq(do_enq, do_deq)
+    when T_96 :
+      maybe_full <= do_enq
       skip
-    node T_490 = eq(empty, UInt<1>("h00"))
-    node T_492 = and(UInt<1>("h00"), io.enq.hostValid)
-    node T_493 = or(T_490, T_492)
-    io.deq.hostValid := T_493
-    node T_495 = eq(full, UInt<1>("h00"))
-    node T_497 = and(UInt<1>("h00"), io.deq.hostReady)
-    node T_498 = or(T_495, T_497)
-    io.enq.hostReady := T_498
-    infer accessor T_513 = ram[T_406]
-    wire T_599 : `TYPE
-    T_599 := T_513
-    when maybe_flow :
-      T_599 := io.enq.hostBits
+    node T_98 = eq(empty, UInt<1>("h00"))
+    node T_100 = and(UInt<1>("h00"), io.enq.hostValid)
+    node T_101 = or(T_98, T_100)
+    io.deq.hostValid <= T_101
+    node T_103 = eq(full, UInt<1>("h00"))
+    node T_105 = and(UInt<1>("h00"), io.deq.hostReady)
+    node T_106 = or(T_103, T_105)
+    io.enq.hostReady <= T_106
+    infer mport T_107 = ram[T_55], hostClock
+    node T_111 = mux(maybe_flow, io.enq.hostBits, T_107)
+    io.deq.hostBits <- T_111
+    node T_115 = sub(T_53, T_55)
+    node ptr_diff = tail(T_115, 1)
+    node T_117 = and(maybe_full, ptr_match)
+    node T_118 = cat(T_117, ptr_diff)
+    io.count <= T_118
+    """ +
+    (if (init)
+    """
+    reg init : UInt<1>, hostClock with : (reset => (hostReset, UInt<1>("h01")))
+    when init :
+      infer mport T_121 = ram[T_53], hostClock
+      T_121 <- io.enq.hostBits
+      node T_126 = eq(T_53, UInt<2>("h03"))
+      node T_128 = and(UInt<1>("h00"), T_126)
+      node T_131 = add(T_53, UInt<1>("h01"))
+      node T_132 = tail(T_131, 1)
+      node T_133 = mux(T_128, UInt<1>("h00"), T_132)
+      T_53 <= T_133
+      init <= UInt<1>("h00")
       skip
-    io.deq.hostBits := T_599
-    node ptr_diff = subw(T_404, T_406)
-    node T_629 = and(maybe_full, ptr_match)
-    node T_630 = cat(T_629, ptr_diff)
-    io.count := T_630
-      """
+    """
+    else "")
+
     // Generate initial values
     val signals = enumerateMembers(tpe) map ( Seq("io", "deq", "hostBits") ++ _ )
     val defaultAssign = signals map { sig =>
-      scopeSpaces + Connect(NoInfo, buildExp(sig), UIntValue(0, UnknownWidth)).serialize
+      scopeSpaces + Connect(NoInfo, buildExp(sig), UIntValue(0, UnknownWidth())).serialize
     }
 
     val concreteQueue = templatedQueue.replaceAllLiterally("`NAME", name).
@@ -326,4 +244,9 @@ circuit `NAME:
     ast.modules.head
   }
 
+  // Useful for recreating above FIRRTL queue
+  def genSimQueueFirrtl = {
+    val c = Chisel.Driver.emit(() => new GenSimQueue)
+    println(c)
+  }
 }
