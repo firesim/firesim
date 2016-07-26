0bbfd6b1bff7386e7444524dd54008e43f4041c6
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-07-26 10:47:24 -0700

    clean up passes

--- src/main/scala/passes/analyses.scala
+++ src/main/scala/passes/analyses.scala
@@ -4,8 +4,29 @@ package passes
 import firrtl._
 import firrtl.ir._
 import firrtl.Mappers._
+import scala.collection.mutable.{Stack, HashSet, ArrayBuffer} 
 
-object Analyses extends firrtl.passes.Pass {
+private[passes] object Utils {
+  def shims = StroberCompiler.context.shims
+  def wrappers = StroberCompiler.context.wrappers
+  def getChildren(parent: String) = StroberCompiler.context.children(parent)
+
+  def dfs(heads: Seq[DefModule], modules: Seq[DefModule])(visit: DefModule => DefModule): Seq[DefModule] = {
+    val stack = Stack[DefModule](heads:_*)
+    val visited = HashSet[String]((heads map (_.name)):_*)
+    val results = ArrayBuffer[DefModule]()
+    while (!stack.isEmpty) {
+      val top = stack.pop
+      val children = getChildren(top.name).toSet
+      results += visit(top)
+      modules filter (x => children(x.name) && !visited(x.name)) foreach (stack push _)
+      visited ++= children
+    }
+    results.toSeq
+  }
+}
+
+private[strober] object Analyses extends firrtl.passes.Pass {
   def name = "[strober] Analyze Circuit"
   
   def collectChildren(m: Module) {
--- src/main/scala/passes/fame1.scala
+++ src/main/scala/passes/fame1.scala
@@ -4,19 +4,27 @@ package passes
 import firrtl._
 import firrtl.ir._
 import firrtl.Mappers._
-import scala.collection.mutable.{ArrayBuffer, HashMap, HashSet}
+import scala.collection.mutable.{ArrayBuffer, HashSet, HashMap}
 
-object Fame1Transform extends firrtl.passes.Pass {
+private[strober] object Fame1Transform extends firrtl.passes.Pass {
   def name = "[strober] Fame1 Transforms"
   import Utils._
+
+  private val ut = UnknownType
+  private val ug = UNKNOWNGENDER
 
   private def transform(m: DefModule): DefModule = m match {
     case m: ExtModule => m
     case m: Module =>
       val enables = HashSet[String]()
+      val raddrs = HashSet[String]()
+      val raddrIns = HashMap[String, Expression]()
+      val stmts = ArrayBuffer[Statement]()
+
       def collectEnables(s: Statement): Statement = {
         s map collectEnables match {
           case mem: DefMemory =>
+            raddrs ++= mem.readers map (r => s"${mem.name}.${r}.addr")
             enables ++= mem.readers map (r => s"${mem.name}.${r}.en")
             enables ++= mem.writers map (w => s"${mem.name}.${w}.en")
             enables ++= mem.readwriters map (rw => s"${mem.name}.${rw}.en")
@@ -25,49 +33,37 @@ object Fame1Transform extends firrtl.passes.Pass {
         }
       }
 
-      val connectMap = HashMap[Expression, Expression]()
       def collectConnects(s: Statement): Statement = {
         s map collectConnects match {
-          case con: Connect if enables(expToString(con.loc)) =>
-            connectMap(con.loc) = con.expr
+          case con: Connect if raddrs(con.loc.serialize) =>
+            raddrIns(con.expr.serialize) = con.loc
             con
           case s => s
         }
       }
 
-      val targetFire = Port(NoInfo, "targetFire", Input, UIntType(IntWidth(1)))
-      val whens = ArrayBuffer[Statement]()
+      val targetFirePort = Port(NoInfo, "targetFire", Input, UIntType(IntWidth(1)))
+      def targetFire = Reference(targetFirePort.name, ut)
+      def notTargetFire = DoPrim(PrimOps.Not, Seq(targetFire), Nil, ut)
       def connectTargetFire(s: Statement): Statement = {
         s map connectTargetFire match {
-          case inst: DefInstance =>
-            Begin(Seq(
-              inst,
-              Connect(NoInfo, buildExp(inst.name, targetFire.name), buildExp(targetFire.name))
-            ))
           case inst: WDefInstance =>
-            Begin(Seq(
-              inst,
-              Connect(NoInfo, buildExp(inst.name, targetFire.name), buildExp(targetFire.name))
-            ))
+            val pin = WSubField(Reference(inst.name, ut), targetFire.name, ut, ug)
+            Block(Seq(inst, Connect(NoInfo, pin, targetFire)))
           case reg: DefRegister =>
-            whens += Conditionally(reg.info,
-              DoPrim(PrimOps.Not, Seq(buildExp(targetFire.name)), Seq(), UnknownType),
-              Connect(NoInfo, buildExp(reg.name), buildExp(reg.name)), EmptyStmt)
+            def regRef = Reference(reg.name, ut)
+            stmts += Conditionally(NoInfo, targetFire, EmptyStmt, Connect(NoInfo, regRef, regRef))
             reg
-          case Connect(info, loc, exp) if connectMap contains loc =>
-            val nodeName = s"""${expToString(loc) replace (".", "_")}_fire"""
-            Begin(Seq(
-              DefNode(NoInfo, nodeName,
-                DoPrim(PrimOps.And, Seq(exp, buildExp(targetFire.name)), Seq(), UnknownType)
-              ),
-              Connect(NoInfo, loc, buildExp(nodeName))
-            ))
+          case Connect(info, loc, exp) if enables(loc.serialize) =>
+            val node = DefNode(NoInfo, s"""${loc.serialize replace (".", "_")}_fire""",
+              DoPrim(PrimOps.And, Seq(exp, targetFire), Seq(), ut))
+            Block(Seq(node, Connect(info, loc, Reference(node.name, ut))))
           case s => s
         }
       }
 
-      Module(m.info, m.name, m.ports :+ targetFire, Begin(
-        (m.body map collectEnables map collectConnects map connectTargetFire) +: whens))
+      Module(m.info, m.name, m.ports :+ targetFirePort, Block(
+        (m.body map collectEnables map collectConnects map connectTargetFire) +: stmts.toSeq))
   }
 
   def run(c: Circuit) = {
@@ -77,18 +73,15 @@ object Fame1Transform extends firrtl.passes.Pass {
         val connects = ArrayBuffer[Statement]()
         def connectTargetFire(s: Statement): Statement = {
           s map connectTargetFire match {
-            case inst: DefInstance if inst.name == "target" =>
-              targets += inst.module
-              connects += Connect(NoInfo, buildExp(inst.name, "targetFire"), buildExp("fire"))
-              inst
             case inst: WDefInstance if inst.name == "target" =>
+              val pin = WSubField(Reference(inst.name, ut), "targetFire", ut, ug)
               targets += inst.module
-              connects += Connect(NoInfo, buildExp(inst.name, "targetFire"), buildExp("fire"))
+              connects += Connect(NoInfo, pin, Reference("fire", ut))
               inst
             case s => s
           }
         }
-        val body = Begin((m.body map connectTargetFire) +: connects)
+        val body = Block((m.body map connectTargetFire) +: connects)
         val targetModules = c.modules filter (x => targets(x.name))
         (dfs(targetModules, c.modules)(transform) map (x => x.name -> x)) :+
         (m.name -> Module(m.info, m.name, m.ports, body))
