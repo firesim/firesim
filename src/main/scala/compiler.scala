b1f7981918da3dca29885e7b32780e9e21801aea
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-08-01 17:04:49 -0700

    [MIDAS] [WIP] generate register scan chains and dump their metadata

--- src/main/scala/compiler.scala
+++ src/main/scala/compiler.scala
@@ -1,36 +1,46 @@
 package strober
 
 import firrtl._
-import firrtl.ir._
 import firrtl.passes._
-import Annotations.AnnotationMap
-import scala.collection.mutable.{HashMap, HashSet}
+import Annotations.{AnnotationMap, TransID}
+import scala.collection.mutable.{ArrayBuffer, HashMap, HashSet}
+import scala.collection.immutable.ListSet
 import scala.util.DynamicVariable
 
 private class TransformContext {
   val shims = HashSet[String]()
   val wrappers = HashSet[String]()
-  val modules = HashSet[String]()
-  val children = HashMap[String, Vector[String]]()
+  val params = HashMap[String, cde.Parameters]()
+  val childInsts = HashMap[String, ListSet[String]]()
+  val childMods = HashMap[String, ListSet[String]]()
+  val instToMod = HashMap[String, String]()
+  val chains = Map(
+    ChainType.Trace -> HashMap[String, Seq[ir.Statement]](),
+    ChainType.Regs  -> HashMap[String, Seq[ir.Statement]](),
+    ChainType.SRAM  -> HashMap[String, Seq[ir.Statement]](),
+    ChainType.Cntr  -> HashMap[String, Seq[ir.Statement]]()
+  )
 }
 
-class StroberTransforms extends Transform with SimpleRun {
+class StroberTransforms(chain: java.io.Writer) extends Transform with SimpleRun {
   val passSeq = Seq(
     strober.passes.Analyses,
-    strober.passes.Fame1Transform
+    strober.passes.Fame1Transform,
+    strober.passes.AddDaisyChains,
+    new strober.passes.DumpChains(chain)
   )
-  def execute(circuit: Circuit, annotationMap: AnnotationMap) =
+  def execute(circuit: ir.Circuit, annotationMap: AnnotationMap) =
     run(circuit, passSeq)
 }
 
-class StroberCompiler extends Compiler {
+class StroberCompiler(chain: java.io.Writer) extends Compiler {
   def transforms(writer: java.io.Writer): Seq[Transform] = Seq(
     new Chisel3ToHighFirrtl,
     new IRToWorkingIR,
     new ResolveAndCheck,
     new HighFirrtlToMiddleFirrtl,
-    new InferReadWrite,
-    new StroberTransforms,
+    new InferReadWrite(TransID(-1)),
+    new StroberTransforms(chain),
     new EmitFirrtl(writer)
   )
 }
@@ -39,28 +49,8 @@ object StroberCompiler {
   private val contextVar = new DynamicVariable[Option[TransformContext]](None)
   private[strober] def context = contextVar.value.getOrElse (new TransformContext)
 
-  private def moduleToFirrtl(c: chisel3.internal.firrtl.Circuit, dir: java.io.File) = {
-    val firrtl = s"${dir}/${c.name}.fir"
-    chisel3.Driver.dumpFirrtl(c, Some(new java.io.File(firrtl)))
-    firrtl
-  }
-
-  private def transform(name: String, highFirrtl: String, dir: java.io.File) = {
-    val firrtl = s"${dir}/${name}-strober.fir"
-    val annotations = new AnnotationMap(Seq(new InferReadWriteAnnotation(name)))
-    Driver.compile(highFirrtl, firrtl, new StroberCompiler, annotations=annotations)
-    firrtl
-  }
-
-  private def firrtlToVerilog(name: String, firrtl: String, dir: java.io.File) = {
-    val v = s"${dir}/${name}.v"
-    Driver.compile(firrtl, v, new VerilogCompiler)
-    v
-  }
-
-  private def dumpIoMap[T <: SimNetwork](c: ZynqShim[T]) {
+  private def dumpIoMap[T <: SimNetwork](c: ZynqShim[T], targetName: String) {
     object MapType extends Enumeration { val IoIn, IoOut, InTr, OutTr = Value }
-    val targetName = c.sim match { case sim: SimWrapper[_] => sim.target.name }
     val nameMap = (c.sim.io.inputs ++ c.sim.io.outputs).toMap
     def dump(map_t: MapType.Value, arg: (chisel3.Bits, Int)) = arg match {case (wire, id) =>
       s"${map_t.id} ${targetName}.${nameMap(wire)} ${id} ${SimUtils.getChunks(wire)(c.sim.channelWidth)}\n"} 
@@ -68,8 +58,8 @@ object StroberCompiler {
     sb append (c.IN_ADDRS map (x => (x._1, x._2 - c.CTRL_NUM))  map {dump(MapType.IoIn,  _)} mkString "")
     sb append (c.OUT_ADDRS map (x => (x._1, x._2 - c.CTRL_NUM)) map {dump(MapType.IoOut, _)} mkString "")
 
-    val filePath = s"${chisel3.Driver.targetDir}/${targetName}.map"
-    val file = new java.io.FileWriter(new java.io.File(filePath))
+    val file = new java.io.FileWriter(
+      new java.io.File(chisel3.Driver.targetDir, s"${targetName}.map"))
     try {
       file write sb.result
     } finally {
@@ -78,9 +68,8 @@ object StroberCompiler {
     }
   }
 
-  private def dumpHeader[T <: SimNetwork](c: ZynqShim[T]) {
+  private def dumpHeader[T <: SimNetwork](c: ZynqShim[T], targetName: String) {
     implicit val channelWidth = c.sim.channelWidth
-    val targetName = c.sim match { case sim: SimWrapper[_] => sim.target.name }
     def dump(arg: (String, Int)) = s"#define ${arg._1} ${arg._2}\n"
     val consts = List(
       "RESET_ADDR"         -> c.RESET_ADDR,
@@ -104,8 +93,8 @@ object StroberCompiler {
     sb append "#define __%s_H\n".format(targetName.toUpperCase)
     consts foreach (sb append dump(_))
     sb append "#endif  // __%s_H\n".format(targetName.toUpperCase)
-    val filePath = s"${chisel3.Driver.targetDir}/${targetName}-const.h"
-    val file = new java.io.FileWriter(new java.io.File(filePath))
+    val file = new java.io.FileWriter(
+      new java.io.File(chisel3.Driver.targetDir, s"${targetName}-const.h"))
     try {
       file write sb.result
     } finally {
@@ -116,26 +105,40 @@ object StroberCompiler {
 
   def apply[T <: chisel3.Module](args: Array[String], w: => T) = {
     contextVar.withValue(Some(new TransformContext)) {
+      // Generate Chirrtl
       chisel3.Driver.parseArgs(args)
       val circuit = chisel3.Driver.elaborate(() => w)
       val dir = new java.io.File(chisel3.Driver.targetDir) ; dir.mkdirs
-      val highFirrtl = moduleToFirrtl(circuit, dir)
-      val dut = (circuit.components find (_.name == circuit.name)).get.id 
-      dut match {
+      val chirrtl = new java.io.File(dir, s"${circuit.name}.ir")
+      chisel3.Driver.dumpFirrtl(circuit, Some(chirrtl))
+      val dut = (circuit.components find (_.name == circuit.name)).get.id
+      // Run Strober Custom Passes
+      val targetName = dut match {
         case w: SimWrapper[_] =>
           context.wrappers += w.name
+          context.params(w.name) = w.p
+          w.target.name
         case w: ZynqShim[_] =>
           context.shims += w.name
           context.wrappers += w.sim.name
+          context.params(w.sim.name) = w.sim.p
+          w.sim match { case w: SimWrapper[_] => w.target.name }
       }
-      val firrtl = transform(circuit.name, highFirrtl, dir)
-      firrtlToVerilog(circuit.name, firrtl, dir)
+      val chain = new java.io.File(dir, s"${targetName}.chain")
+      val firrtl = new java.io.File(dir, s"${circuit.name}-strober.fir")
+      val annotations = new AnnotationMap(Seq(new InferReadWriteAnnotation(circuit.name, TransID(-1))))
+      Driver.compile(chirrtl.toString, firrtl.toString,
+        new StroberCompiler(new java.io.FileWriter(chain)), annotations=annotations)
+      // Dump meta data
       dut match {
         case w: ZynqShim[_] =>
-          dumpIoMap(w)
-          dumpHeader(w)
+          dumpIoMap(w, targetName)
+          dumpHeader(w, targetName)
         case _ =>
       }
+      // Compile Verilog
+      val verilog = new java.io.File(dir, s"${circuit.name}.v")
+      Driver.compile(firrtl.toString, verilog.toString, new VerilogCompiler)
       circuit
     }
   }
