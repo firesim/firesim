d493236222852bf80ebde3284177359dbd82e287
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-10-30 20:11:01 -0700

    [MIDAS] fix replay_vpi with new context implementation

---
+++ src/main/cc/vcs_main.h
@@ -0,0 +1,18 @@
+extern "C" {
+extern int vcs_main(int argc, char** argv);
+}
+
+struct target_args_t {
+  target_args_t(int c, char** v):
+    argc(c), argv(v) { }
+  int argc;
+  char** argv;
+};
+
+int target_thread(void *arg) {
+  target_args_t* targs = reinterpret_cast<target_args_t*>(arg);
+  int argc = targs->argc;
+  char** argv = targs->argv;
+  delete targs;
+  return vcs_main(argc, argv);
+}
