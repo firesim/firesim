747860cda22216659382a7818af47d0a2c25a581
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-11-01 17:45:58 -0700

    can pass chirrtl to compiler

--- src/main/scala/replay/compiler.scala
+++ src/main/scala/replay/compiler.scala
@@ -1,6 +1,7 @@
 package strober
 package replay
 
+import firrtl.ir.Circuit
 import firrtl.Annotations.{AnnotationMap, TransID}
 import scala.collection.mutable.{ArrayBuffer, HashMap, HashSet}
 import scala.util.DynamicVariable
@@ -22,18 +23,22 @@ private class Compiler(conf: File) extends firrtl.Compiler {
 }
 
 object Compiler {
-  def apply[T <: chisel3.Module](w: => T, dir: File): firrtl.ir.Circuit = {
+  def apply(chirrtl: Circuit, io: chisel3.Data, dir: File): Circuit = {
     dir.mkdirs
-    lazy val dut = w
-    val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => dut))
     val conf = new File(dir, s"${chirrtl.main}.conf")
     val annotations = new AnnotationMap(Seq(
       firrtl.passes.InferReadWriteAnnotation(chirrtl.main, TransID(-1)),
       firrtl.passes.memlib.ReplSeqMemAnnotation(s"-c:${chirrtl.main}:-o:$conf", TransID(-2))))
     val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
     val result = new Compiler(conf) compile (chirrtl, annotations, verilog)
-    genVerilogFragment(dut, new FileWriter(new File(dir, s"${chirrtl.main}.vfrag")))
+    genVerilogFragment(chirrtl.main, io, new FileWriter(new File(dir, s"${chirrtl.main}.vfrag")))
     verilog.close
     result.circuit
+  }
+
+  def apply[T <: chisel3.Module](w: => T, dir: File): Circuit = {
+    lazy val dut = w
+    val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => dut))
+    apply(chirrtl, dut.io, dir)
   }
 }
