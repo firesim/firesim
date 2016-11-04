4e61f24ce77f585ceeb59e7eb3bbc8951a4544b9
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-11-03 19:24:30 -0700

    fix simif_zynq init

--- src/main/cc/simif_zynq.h
+++ src/main/cc/simif_zynq.h
@@ -8,9 +8,6 @@ class simif_zynq_t: public virtual simif_t
   public:
     simif_zynq_t();
     virtual ~simif_zynq_t() { }
-    virtual void init(int argc, char** argv, bool log = false, bool fast_loadmem = false) {
-      simif_t::init(argc, argv, log, false); // no fast loadmem available
-    }
 
   private:
     volatile uintptr_t* dev_vaddr;
