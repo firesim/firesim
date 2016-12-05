42b3f63c1959bc40cd406f2ee293542d6a03e8ff
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-12-05 12:58:31 -0800

    [MIDAS] fix SimpleLatencyPipe for correct timing

--- src/main/scala/midas/widgets/SimpleLatencyPipe.scala
+++ src/main/scala/midas/widgets/SimpleLatencyPipe.scala
@@ -21,7 +21,7 @@ class SimDecoupledIO[+T <: Data](gen: T)(implicit val p: Parameters) extends Bun
 }
 
 class MemModelIO(implicit p: Parameters) extends WidgetIO()(p){
-  val tNasti = Flipped(HostPort((new NastiIO), false))
+  val tNasti = Flipped(HostPort(new NastiIO, false))
   val tReset = Flipped(Decoupled(Bool()))
   val host_mem = new NastiIO
 }
@@ -31,72 +31,72 @@ abstract class MemModel(implicit p: Parameters) extends Widget()(p){
 }
 
 class SimpleLatencyPipe(implicit p: Parameters) extends MemModel {
-  val tNasti = io.tNasti.hBits
-  val tFire = io.tNasti.toHost.hValid && io.tNasti.fromHost.hReady && io.tReset.valid
-
-  val ar_buf = Module(new Queue(new NastiReadAddressChannel,   4, flow=true))
-  val aw_buf = Module(new Queue(new NastiWriteAddressChannel,  4, flow=true))
-  val w_buf  = Module(new Queue(new NastiWriteDataChannel,    16, flow=true))
-  val r_buf  = Module(new Queue(new NastiReadDataChannel,     16, flow=true))
-  val b_buf  = Module(new Queue(new NastiWriteResponseChannel, 4, flow=true))
-
-  // Bad assumption: We have no outstanding read or write requests to host
-  // during target reset. This will be handled properly in the fully fledged
-  // memory model; i'm too lazy to properly handle this here.
-  val targetReset = tFire && io.tReset.bits
-  ar_buf.reset := reset || targetReset
-  aw_buf.reset := reset || targetReset
-  r_buf.reset := reset || targetReset
-  b_buf.reset := reset || targetReset
-  w_buf.reset := reset || targetReset
-
+  val arBuf = Module(new Queue(new NastiReadAddressChannel,   4, flow=true))
+  val awBuf = Module(new Queue(new NastiWriteAddressChannel,  4, flow=true))
+  val wBuf  = Module(new Queue(new NastiWriteDataChannel,    16, flow=true))
+  val rBuf  = Module(new Queue(new NastiReadDataChannel,     16, flow=true))
+  val bBuf  = Module(new Queue(new NastiWriteResponseChannel, 4, flow=true))
 
   // Timing Model
   val cycles = RegInit(UInt(0, 64))
-  val r_cycles = Module(new Queue(UInt(width=64), 4))
-  val w_cycles = Module(new Queue(UInt(width=64), 4))
+  val rCycles = Module(new Queue(UInt(width=64), 4))
+  val wCycles = Module(new Queue(UInt(width=64), 4))
   val latency = RegInit(UInt(16, 32))
-  r_cycles.reset := reset || targetReset
-  w_cycles.reset := reset || targetReset
-  attach(latency, "LATENCY")
 
+  val rCycleFire = rCycles.io.deq.valid && rCycles.io.deq.bits <= cycles
+  val wCycleFire = wCycles.io.deq.valid && wCycles.io.deq.bits <= cycles
+  val tStall = (rCycleFire && !rBuf.io.deq.valid) || (wCycleFire && !bBuf.io.deq.valid)
+  val tNasti = io.tNasti.hBits
+  val tFire = io.tNasti.toHost.hValid && io.tNasti.fromHost.hReady && io.tReset.valid && !tStall
   io.tNasti.toHost.hReady := tFire
   io.tNasti.fromHost.hValid := tFire
   io.tReset.ready := tFire
 
   when(tFire) { cycles := cycles + UInt(1) }
-  r_cycles.io.enq.bits := cycles + latency
-  w_cycles.io.enq.bits := cycles + latency
-  r_cycles.io.enq.valid := tNasti.ar.fire() && tFire
-  w_cycles.io.enq.valid := tNasti.w.fire()  && tNasti.w.bits.last && tFire
-  r_cycles.io.deq.ready := tNasti.r.fire()  && tNasti.r.bits.last && tFire
-  w_cycles.io.deq.ready := tNasti.b.fire()  && tFire
+  rCycles.io.enq.bits := cycles + latency
+  wCycles.io.enq.bits := cycles + latency
+  rCycles.io.enq.valid := tNasti.ar.fire() && tFire
+  wCycles.io.enq.valid := tNasti.w.fire()  && tNasti.w.bits.last && tFire
+  rCycles.io.deq.ready := tNasti.r.fire()  && tNasti.r.bits.last && tFire
+  wCycles.io.deq.ready := tNasti.b.fire()  && tFire
+
+  // Bad assumption: We have no outstanding read or write requests to host
+  // during target reset. This will be handled properly in the fully fledged
+  // memory model; i'm too lazy to properly handle this here.
+  val targetReset = tFire && io.tReset.bits
+  arBuf.reset := reset || targetReset
+  awBuf.reset := reset || targetReset
+  rBuf.reset := reset || targetReset
+  bBuf.reset := reset || targetReset
+  wBuf.reset := reset || targetReset
+  rCycles.reset := reset || targetReset
+  wCycles.reset := reset || targetReset
 
   // Requests
-  tNasti.ar.ready := ar_buf.io.enq.ready && r_cycles.io.enq.ready
-  tNasti.aw.ready := aw_buf.io.enq.ready && w_cycles.io.enq.ready
-  tNasti.w.ready  := w_buf.io.enq.ready  && w_cycles.io.enq.ready
-  ar_buf.io.enq.valid := tNasti.ar.valid && tFire && !io.tReset.bits
-  aw_buf.io.enq.valid := tNasti.aw.valid && tFire && !io.tReset.bits
-  w_buf.io.enq.valid  := tNasti.w.valid  && tFire && !io.tReset.bits
-  ar_buf.io.enq.bits  := tNasti.ar.bits
-  aw_buf.io.enq.bits  := tNasti.aw.bits
-  w_buf.io.enq.bits   := tNasti.w.bits
-  io.host_mem.aw <> aw_buf.io.deq
-  io.host_mem.ar <> ar_buf.io.deq
-  io.host_mem.w  <> w_buf.io.deq
+  tNasti.ar.ready := arBuf.io.enq.ready && rCycles.io.enq.ready
+  tNasti.aw.ready := awBuf.io.enq.ready && wCycles.io.enq.ready
+  tNasti.w.ready  := wBuf.io.enq.ready  && wCycles.io.enq.ready
+  arBuf.io.enq.valid := tNasti.ar.valid && tFire && !io.tReset.bits
+  awBuf.io.enq.valid := tNasti.aw.valid && tFire && !io.tReset.bits
+  wBuf.io.enq.valid  := tNasti.w.valid  && tFire && !io.tReset.bits
+  arBuf.io.enq.bits  := tNasti.ar.bits
+  awBuf.io.enq.bits  := tNasti.aw.bits
+  wBuf.io.enq.bits   := tNasti.w.bits
+  io.host_mem.aw <> awBuf.io.deq
+  io.host_mem.ar <> arBuf.io.deq
+  io.host_mem.w  <> wBuf.io.deq
 
   // Response
-  tNasti.r.bits <> r_buf.io.deq.bits
-  tNasti.b.bits <> b_buf.io.deq.bits
-  tNasti.r.valid := r_buf.io.deq.valid && (r_cycles.io.deq.bits <= cycles) && r_cycles.io.deq.valid
-  tNasti.b.valid := b_buf.io.deq.valid && (w_cycles.io.deq.bits <= cycles) && w_cycles.io.deq.valid
-  r_buf.io.deq.ready := tNasti.r.ready && tFire && (r_cycles.io.deq.bits <= cycles) && r_cycles.io.deq.valid
-  b_buf.io.deq.ready := tNasti.b.ready && tFire && (w_cycles.io.deq.bits <= cycles) && w_cycles.io.deq.valid
-
-  r_buf.io.enq <> io.host_mem.r
-  b_buf.io.enq <> io.host_mem.b
+  tNasti.r.bits <> rBuf.io.deq.bits
+  tNasti.b.bits <> bBuf.io.deq.bits
+  tNasti.r.valid := rBuf.io.deq.valid && rCycleFire
+  tNasti.b.valid := bBuf.io.deq.valid && wCycleFire
+  rBuf.io.deq.ready := tNasti.r.ready && tFire && rCycleFire
+  bBuf.io.deq.ready := tNasti.b.ready && tFire && wCycleFire
+  rBuf.io.enq <> io.host_mem.r
+  bBuf.io.enq <> io.host_mem.b
 
   // Connect all programmable registers to the control interrconect
+  attach(latency, "LATENCY")
   genCRFile()
 }
