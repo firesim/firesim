e1c9b9f762b097e8cc4f49f67f3289451681718c
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-01-03 20:35:19 -0800

    clean up makefile

--- src/main/cc/utils/utils.mk
+++ src/main/cc/utils/utils.mk
@@ -1,37 +1,35 @@
 # compile DRAMSim2
-ifneq ($(CXX),cl)
 dramsim_o := $(foreach f, \
-                $(patsubst %.cpp, %.o, $(wildcard $(midas_dir)/dramsim2/*.cpp)), \
+                $(patsubst %.cpp, %.$(o), $(wildcard $(midas_dir)/dramsim2/*.cpp)), \
                 $(GEN_DIR)/$(notdir $(f)))
-$(dramsim_o): $(GEN_DIR)/%.o: $(midas_dir)/dramsim2/%.cpp
+$(dramsim_o): $(GEN_DIR)/%.$(o): $(midas_dir)/dramsim2/%.cpp
+ifneq ($(CXX),cl)
 	$(CXX) $(CXXFLAGS) -DNO_STORAGE -DNO_OUTPUT -Dmain=nomain -c -o $@ $<
 else
-dramsim_o := $(foreach f, \
-		$(patsubst %.cpp, %.obj, $(wildcard $(midas_dir)/dramsim2/*.cpp)), \
-                $(GEN_DIR)/$(notdir $(f)))
-$(dramsim_o): $(GEN_DIR)/%.obj: $(midas_dir)/dramsim2/%.cpp
 	$(CXX) $(CXXFLAGS) /DNO_STORAGE /DNO_OUTPUT /Dmain=nomain /c /Fo$(call path,$@) $(call path,$<)
 endif
 
 # Compile utility code
 lib_files := biguint mm mm_dramsim2 $(if $(filter $(CXX),cl),,midas_context)
 lib_cc    := $(addprefix $(util_dir)/, $(addsuffix .cc, $(lib_files)))
+lib_o     := $(addprefix $(GEN_DIR)/, $(addsuffix .$(o), $(lib_files)))
+
+$(lib_o): $(GEN_DIR)/%.$(o): $(util_dir)/%.cc
 ifneq ($(CXX),cl)
-lib_o     := $(addprefix $(GEN_DIR)/, $(addsuffix .o, $(lib_files)))
-
-$(lib_o): $(GEN_DIR)/%.o: $(util_dir)/%.cc
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
+else
+	$(CXX) $(CXXFLAGS) /c /Fo$(call path,$@) $(call path,$<)
+endif
 
+ifneq ($(CXX),cl)
 lib       := $(GEN_DIR)/libmidas.a
+else
+lib       := $(GEN_DIR)/midas.lib
+endif
+
 $(lib): $(lib_o) $(dramsim_o)
+ifneq ($(CXX),cl)
 	$(AR) rcs $@ $^
 else
-lib_o     := $(addprefix $(GEN_DIR)/, $(addsuffix .obj, $(lib_files)))
-
-$(lib_o): $(GEN_DIR)/%.obj: $(util_dir)/%.cc
-	$(CXX) $(CXXFLAGS) /c /Fo$(call path,$@) $(call path,$<)
-
-lib       := $(GEN_DIR)/midas.lib
-$(lib): $(lib_o) $(dramsim_o)
 	$(AR) -OUT:$(call path,$@) $(foreach f, $^, $(call path,$f))
 endif
