cb9f7af2c2dc396c20f0716562d3f035290d2827
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-03-05 22:16:59 -0800

    [MIDAS] fix configs

--- src/main/scala/midas/Config.scala
+++ src/main/scala/midas/Config.scala
@@ -13,50 +13,40 @@ case object Platform extends Field[PlatformType]
 case object EnableSnapshot extends Field[Boolean]
 case object MemModelKey extends Field[Option[Parameters => MemModel]]
 
-class SimConfig extends Config(
-  (key, site, here) => key match {
-    case TraceMaxLen    => 1024
-    case SRAMChainNum   => 1
-    case ChannelLen     => 16
-    case ChannelWidth   => 32
-    case DaisyWidth     => 32
-    case EnableSnapshot => false
-    case CtrlNastiKey   => NastiParameters(32, 32, 12)
-    case MemNastiKey    => NastiParameters(64, 32, 6)
-    case MemModelKey    => Some((p: Parameters) => new SimpleLatencyPipe()(p))
-    case FpgaMMIOSize   => BigInt(1) << 12 // 4 KB
-  }
-)
+class SimConfig extends Config((site, here, up) => {
+  case TraceMaxLen    => 1024
+  case SRAMChainNum   => 1
+  case ChannelLen     => 16
+  case ChannelWidth   => 32
+  case DaisyWidth     => 32
+  case EnableSnapshot => false
+  case CtrlNastiKey   => NastiParameters(32, 32, 12)
+  case MemNastiKey    => NastiParameters(64, 32, 6)
+  case MemModelKey    => Some((p: Parameters) => new SimpleLatencyPipe()(p))
+  case FpgaMMIOSize   => BigInt(1) << 12 // 4 KB
+})
 
-class ZynqConfig extends Config(new Config(
-  (key, site, here) => key match {
-    case Platform       => Zynq
-    case MasterNastiKey => site(CtrlNastiKey)
-    case SlaveNastiKey  => site(MemNastiKey)
-  }) ++ new SimConfig
-)
+class ZynqConfig extends Config(new Config((site, here, up) => {
+  case Platform       => Zynq
+  case MasterNastiKey => site(CtrlNastiKey)
+  case SlaveNastiKey  => site(MemNastiKey)
+}) ++ new SimConfig)
 
-class ZynqConfigWithSnapshot extends Config(new Config(
-  (key, site, here) => key match {
-    case EnableSnapshot => true
-  }) ++ new ZynqConfig
-)
+class ZynqConfigWithSnapshot extends Config(new Config((site, here, up) => {
+  case EnableSnapshot => true
+}) ++ new ZynqConfig)
 
-class CatapultConfig extends Config(new Config(
-  (key, site, here) => key match {
-    case Platform       => Catapult
-    case PCIeWidth      => 640
-    case ChannelWidth   => 64
-    case DaisyWidth     => 64
-    case SoftRegKey     => SoftRegParam(32, 64)
-    case CtrlNastiKey   => NastiParameters(64, 32, 12)
-    case NastiKey       => site(CtrlNastiKey)
-    case MemModelKey    => None
-  }) ++ new SimConfig
-)
+class CatapultConfig extends Config(new Config((site, here, up) => {
+  case Platform       => Catapult
+  case PCIeWidth      => 640
+  case ChannelWidth   => 64
+  case DaisyWidth     => 64
+  case SoftRegKey     => SoftRegParam(32, 64)
+  case CtrlNastiKey   => NastiParameters(64, 32, 12)
+  case NastiKey       => site(CtrlNastiKey)
+  case MemModelKey    => None
+}) ++ new SimConfig)
 
-class CatapultConfigWithSnapshot extends Config(new Config(
-  (key, site, here) => key match {
-    case EnableSnapshot => true
-  }) ++ new CatapultConfig
-)
+class CatapultConfigWithSnapshot extends Config(new Config((site, here, up) => {
+  case EnableSnapshot => true
+}) ++ new CatapultConfig)
