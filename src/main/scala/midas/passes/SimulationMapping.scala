a89bc2166c5e78d11f20eb4c4e435172e62932e2
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-05-04 15:48:55 -0700

    decoupled strober meta data from midas

--- src/main/scala/midas/passes/SimulationMapping.scala
+++ src/main/scala/midas/passes/SimulationMapping.scala
@@ -7,7 +7,6 @@ import firrtl.ir._
 import firrtl.Mappers._
 import firrtl.Utils.BoolType
 import Utils._
-import MidasTransforms._
 import java.io.{File, FileWriter, StringWriter}
 
 private[passes] class SimulationMapping(
