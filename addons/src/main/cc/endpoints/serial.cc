219a474dd241e01de535e77d929b106501012464
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-09-29 17:00:02 -0700

    move endpoints from midas-top

---
+++ addons/src/main/cc/endpoints/serial.cc
@@ -0,0 +1,43 @@
+#include "serial.h"
+
+serial_t::serial_t(simif_t* sim, fesvr_proxy_t* fesvr):
+  endpoint_t(sim), fesvr(fesvr)
+{
+}
+
+void serial_t::send() {
+  if (data.in.fire()) {
+    write(SERIALWIDGET_0(in_bits), data.in.bits);
+    write(SERIALWIDGET_0(in_valid), data.in.valid);
+  }
+  if (data.out.fire()) {
+    write(SERIALWIDGET_0(out_ready), data.out.ready);
+  }
+}
+
+void serial_t::recv() {
+  data.in.ready = read(SERIALWIDGET_0(in_ready));
+  data.out.valid = read(SERIALWIDGET_0(out_valid));
+  if (data.out.valid) {
+    data.out.bits = read(SERIALWIDGET_0(out_bits));
+  }
+}
+
+void serial_t::work() {
+  data.out.ready = true;
+  do {
+    this->recv();
+
+    data.in.valid = fesvr->data_available();
+    if (data.in.fire()) {
+      data.in.bits = fesvr->recv_word();
+    }
+    if (data.out.fire()) {
+      fesvr->send_word(data.out.bits);
+    }
+
+    this->send();
+
+    fesvr->tick(); 
+  } while (data.in.fire() || data.out.fire());
+}
---
+++ addons/src/main/cc/endpoints/uart.cc
@@ -0,0 +1,94 @@
+#include "uart.h"
+#ifndef _WIN32
+#include <unistd.h>
+#include <fcntl.h>
+
+/* There is no "backpressure" to the user input for sigs. only one at a time
+ * non-zero value represents unconsumed special char input.
+ *
+ * Reset to zero once consumed.
+ */
+char specialchar = 0;
+
+void sighand(int s) {
+    switch (s) {
+        case SIGINT:
+            // ctrl-c
+            specialchar = 0x3;
+            break;
+        default:
+            specialchar = 0x0;
+    }
+}
+#endif
+
+uart_t::uart_t(simif_t* sim): endpoint_t(sim)
+{
+#ifndef _WIN32
+    // Don't block on stdin reads if there is nothing typed in
+    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+
+    // signal handler so ctrl-c doesn't kill simulation
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = sighand;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+#endif
+}
+
+void uart_t::send() {
+  if (data.in.fire()) {
+    write(UARTWIDGET_0(in_bits), data.in.bits);
+    write(UARTWIDGET_0(in_valid), data.in.valid);
+  }
+  if (data.out.fire()) {
+    write(UARTWIDGET_0(out_ready), data.out.ready);
+  }
+}
+
+void uart_t::recv() {
+  data.in.ready = read(UARTWIDGET_0(in_ready));
+  data.out.valid = read(UARTWIDGET_0(out_valid));
+  if (data.out.valid) {
+    data.out.bits = read(UARTWIDGET_0(out_bits));
+  }
+}
+
+void uart_t::tick() {
+  data.out.ready = true;
+  data.in.valid = false;
+  do {
+    this->recv();
+
+#ifndef _WIN32
+    if (data.in.ready) {
+        char inp;
+        int readamt;
+        if (specialchar) {
+            // send special character (e.g. ctrl-c)
+            inp = specialchar;
+            specialchar = 0;
+            readamt = 1;
+        } else {
+            // else check if we have input on stdin
+            readamt = ::read(STDIN_FILENO, &inp, 1);
+        }
+
+        if (readamt > 0) {
+            data.in.bits = inp;
+            data.in.valid = true;
+        }
+    }
+#endif
+
+    if (data.out.fire()) {
+      fprintf(stdout, "%c", data.out.bits);
+      // always flush to get char-by-char output (not line-buffered)
+      fflush(stdout);
+    }
+
+    this->send();
+    data.in.valid = false;
+  } while(data.in.fire() || data.out.fire());
+}
