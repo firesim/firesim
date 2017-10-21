5a6b947a8b51c1d2795d635f82e378f02d04714c
David Biancolin <david.biancolin@gmail.com>
2017-10-21 16:26:13 -0700

    Support 16G of host memory in F1

--- src/main/cc/simif.cc
+++ src/main/cc/simif.cc
@@ -166,7 +166,8 @@ void simif_t::load_mem(std::string filename) {
 }
 
 void simif_t::read_mem(size_t addr, mpz_t& value) {
-  write(LOADMEM_R_ADDRESS, addr);
+  write(LOADMEM_R_ADDRESS_H, addr >> 32);
+  write(LOADMEM_R_ADDRESS_L, addr & ((1 << 32) - 1));
   const size_t size = MEM_DATA_CHUNK;
   data_t data[size];
   for (size_t i = 0 ; i < size ; i++) {
@@ -176,7 +177,8 @@ void simif_t::read_mem(size_t addr, mpz_t& value) {
 }
 
 void simif_t::write_mem(size_t addr, mpz_t& value) {
-  write(LOADMEM_W_ADDRESS, addr);
+  write(LOADMEM_W_ADDRESS_H, addr >> 32);
+  write(LOADMEM_W_ADDRESS_L, addr & ((1 << 32) - 1));
   size_t size;
   data_t* data = (data_t*)mpz_export(NULL, &size, -1, sizeof(data_t), 0, 0, value);
   for (size_t i = 0 ; i < MEM_DATA_CHUNK ; i++) {
