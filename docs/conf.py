18107757e676805d1b333d6fb39b353f4c0a3311
Sagar Karandikar <sagark@eecs.berkeley.edu>
2018-05-19 17:55:04 +0000

    update requirements.txt to match readthedocs versions

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+#    'collapse_navigation': True,
+#    'display_version': True,
+#    'navigation_depth': 4,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
