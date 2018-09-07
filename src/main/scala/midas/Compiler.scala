4389d735221ecfdf01150f63a98aa53eeb3aba2e
alonamid <alonamid@eecs.berkeley.edu>
2018-09-07 22:48:12 +0000

    annotations state bug fix

--- src/main/scala/midas/Compiler.scala
+++ src/main/scala/midas/Compiler.scala
@@ -59,8 +59,7 @@ object MidasCompiler {
       chirrtl, firrtl.ChirrtlForm, targetAnnos ++ midasAnnos),
       customTransforms :+ midasTransforms)
 
-    val result = (new VerilogCompiler).compileAndEmit(firrtl.CircuitState(
-      midas.circuit, firrtl.HighForm, midasAnnos))
+    val result = (new VerilogCompiler).compileAndEmit(midas)
     val verilog = new FileWriter(new File(dir, s"FPGATop.v"))
     verilog.write(result.getEmittedCircuit.value)
     verilog.close
