fb7e0bae44fd62123b16b5968e7efb6a7aa3ae53
David Biancolin <david.biancolin@gmail.com>
2018-09-09 23:03:38 -0700

    [MIDAS] Document endpoint class a little more

--- src/main/cc/endpoints/endpoint.h
+++ src/main/cc/endpoints/endpoint.h
@@ -5,28 +5,36 @@
 
 #include "simif.h"
 
+// Endpoints are widgets that are directly exposed to simulation timing (they
+// produce and/or consume simulation tokens). They must be tick()-ed for the
+// simulation to make forward progress.
+
 class endpoint_t
 {
 public:
   endpoint_t(simif_t* s): sim(s) { }
-  virtual void init() {}; // FIXME; should be pure;
+  // Initialize FPGA-hosted endpoint state -- this can't be done in the constructor
+  virtual void init() = 0;
+  // Does work that allows the endpoint to advance in simulation time (one or more cycles)
   virtual void tick() = 0;
+  // Indicates the simulation should terminate.
+  // Tie off to false if the endpoint will never call for the simulation to teriminate.
   virtual bool done() = 0;
 
 protected:
-  inline void write(size_t addr, data_t data) {
+  void write(size_t addr, data_t data) {
     sim->write(addr, data);
   }
-  
-  inline data_t read(size_t addr) {
+
+  data_t read(size_t addr) {
     return sim->read(addr);
   }
 
-  inline ssize_t pull(size_t addr, char *data, size_t size) {
+  ssize_t pull(size_t addr, char *data, size_t size) {
     return sim->pull(addr, data, size);
   }
 
-  inline ssize_t push(size_t addr, char *data, size_t size) {
+  ssize_t push(size_t addr, char *data, size_t size) {
     if (size == 0)
       return 0;
     return sim->push(addr, data, size);
