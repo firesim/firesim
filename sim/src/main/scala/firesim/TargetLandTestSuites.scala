640448b0eca76fe40ffcdc6be59636fa72ad77c1
David Biancolin <david.biancolin@gmail.com>
2018-09-21 21:22:51 -0700

    Clean up blockdev test suite generation a bit

--- sim/src/main/scala/firesim/TargetLandTestSuites.scala
+++ sim/src/main/scala/firesim/TargetLandTestSuites.scala
@@ -5,28 +5,22 @@ import scala.collection.mutable.LinkedHashSet
 
 import freechips.rocketchip.system.{TestGeneration, RocketTestSuite}
 
-object BlockdevTestSuite extends RocketTestSuite {
+class BlockdevTestSuite(prefix: String, val names: LinkedHashSet[String]) extends RocketTestSuite {
   val envName = ""
   // base_dir is is defined in firesim's Makefrag
-  val dir = "$(base_dir)/target-rtl/firechip/tests"
-  val makeTargetName = "blkdev-tests"
+  val dir = "$(fc_test_dir)"
+  val makeTargetName = prefix + "-blkdev-tests"
   def kind = "blockdev"
   // Blockdev tests need an image, which complicates this
-  def additionalArgs = "+blkdev-in-mem=8"
+  def additionalArgs = "+blkdev-in-mem=128"
   override def toString = s"$makeTargetName = \\\n" +
     // Make variable with the binaries of the suite
-    names.map(n => s"\t$n.riscv").mkString(" \\\n") + "\n" +
+    names.map(n => s"\t$n.riscv").mkString(" \\\n") + "\n\n" +
     // Variables with binary specific arguments
     names.map(n => s"$n.riscv_ARGS=$additionalArgs").mkString(" \n") +
     postScript
-
-  override def postScript = s"""
-
-$$(base_dir)/target-rtl/firechip/tests/%blkdev.riscv: $$(output_dir)/%blockdev.riscv.ext2
-\tcd make -f $$(base_dir)/target-rtl/firechip/Makefile
-
-""" + super.postScript
-
-  val names = LinkedHashSet("blkdev", "big-blkdev")
 }
+
+object FastBlockdevTests extends BlockdevTestSuite("fast", LinkedHashSet("blkdev"))
+object AllBlockdevTests extends BlockdevTestSuite("all", LinkedHashSet("blkdev", "big-blkdev"))
 
