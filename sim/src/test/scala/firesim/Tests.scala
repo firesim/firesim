7e50e52ed6feac54619fb9de5ce1efc8d11791a4
David Biancolin <david.biancolin@gmail.com>
2018-09-24 16:52:39 -0700

    [scalatests] Add a rocket-based design with 2:1 division

--- sim/src/test/scala/firesim/Tests.scala
+++ sim/src/test/scala/firesim/Tests.scala
@@ -96,5 +96,8 @@ abstract class FireSimTestSuite(
 class RocketF1Tests extends FireSimTestSuite(
   FireSimGeneratorArgs("FireSimNoNIC", "FireSimRocketChipConfig", "FireSimConfig"))
 
+class RocketF1ClockDivTests extends FireSimTestSuite(
+  FireSimGeneratorArgs("FireSimNoNIC", "FireSimRocketChipConfig", "FireSimClockDivConfig"))
+
 class BoomF1Tests extends FireSimTestSuite(
   FireSimGeneratorArgs("FireBoomNoNIC", "FireSimBoomConfig", "FireSimConfig"))
