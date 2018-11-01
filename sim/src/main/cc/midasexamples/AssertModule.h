190ac2f5e1b7d436dff2461246179a594eb6c5d2
David Biancolin <david.biancolin@gmail.com>
2018-10-31 18:43:33 -0700

    [midasexamples] Add a child module to AssertModule

--- sim/src/main/cc/midasexamples/AssertModule.h
+++ sim/src/main/cc/midasexamples/AssertModule.h
@@ -13,12 +13,13 @@ public:
         poke(reset, 1);
         poke(io_a, 0);
         poke(io_b, 0);
+        poke(io_c, 0);
         step(1);
         poke(reset, 0);
         step(1);
         poke(io_cycleToFail, 1024);
         poke(io_a, 1);
-        step(2049, false);
+        step(2053, false);
         while (!done()) {
             assert_endpoint->tick();
             if (assert_endpoint->terminate()) {
@@ -26,14 +27,17 @@ public:
                     poke(io_cycleToFail, 2048);
                     poke(io_b, 1);
                     poke(io_a, 0);
-                    assert_endpoint->resume();
-                } else {
+                } else if (assertions_thrown == 1) {
+                    poke(io_cycleToFail, 2052);
                     poke(io_b, 0);
-                    assert_endpoint->resume();
+                    poke(io_c, 1);
+                } else {
+                    poke(io_c, 0);
                 }
+                assert_endpoint->resume();
                 assertions_thrown++;
             }
         }
-        expect(assertions_thrown == 2, "EXPECT: Two assertions thrown");
+        expect(assertions_thrown == 3, "EXPECT: Three assertions thrown");
     };
 };
