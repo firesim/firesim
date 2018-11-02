3c725e12d52b71baa6cd5ab6056ce7ffebc8c836
David Biancolin <david.biancolin@gmail.com>
2018-11-02 05:33:29 +0000

    [Configs] Fix multicore configs, add back L2TLB, perf counters

--- sim/src/main/scala/firesim/TargetConfigs.scala
+++ sim/src/main/scala/firesim/TargetConfigs.scala
@@ -30,16 +30,10 @@ class WithNICKey extends Config((site, here, up) => {
   case NICKey => NICConfig(inBufPackets = 10)
 })
 
-class WithLargeTLBs extends Config((site, here, up) => {
+class WithRocketL2TLBs(entries: Int) extends Config((site, here, up) => {
   case RocketTilesKey => up(RocketTilesKey) map (tile => tile.copy(
-    icache = tile.icache map (_.copy(
-      nTLBEntries = 32 // TLB reach = 32 * 4KB = 128KB
-    )),
-    dcache = tile.dcache map (_.copy(
-      nTLBEntries = 32 // TLB reach = 32 * 4KB = 128KB
-    )),
     core = tile.core.copy(
-      nL2TLBEntries = 1024 // TLB reach = 1024 * 4KB = 4MB
+      nL2TLBEntries = entries
     )
   ))
 })
@@ -50,11 +44,9 @@ class WithPerfCounters extends Config((site, here, up) => {
   ))
 })
 
-class BoomWithLargeTLBs extends Config((site, here, up) => {
+class WithBoomL2TLBs(entries: Int) extends Config((site, here, up) => {
   case BoomTilesKey => up(BoomTilesKey) map (tile => tile.copy(
-    core = tile.core.copy(
-      nL2TLBEntries = 1024 // TLB reach = 1024 * 4KB = 4MB
-    )
+    core = tile.core.copy(nL2TLBEntries = entries)
   ))
 })
 
@@ -84,9 +76,13 @@ class FireSimRocketChipConfig extends Config(
   new WithUARTKey ++
   new WithNICKey ++
   new WithBlockDevice ++
-  new WithLargeTLBs ++
+  new WithRocketL2TLBs(1024) ++
   new WithPerfCounters ++
   new freechips.rocketchip.system.DefaultConfig)
+
+class WithNDuplicatedRocketCores(n: Int) extends Config((site, here, up) => {
+  case RocketTilesKey => List.tabulate(n)(i => up(RocketTilesKey).head.copy(hartId = i))
+})
 
 class FireSimRocketChipTracedConfig extends Config(
   new WithTraceRocket ++ new FireSimRocketChipConfig)
@@ -99,7 +95,7 @@ class FireSimRocketChipSingleCoreTracedConfig extends Config(
 
 // dual core config
 class FireSimRocketChipDualCoreConfig extends Config(
-  new WithNBigCores(2) ++
+  new WithNDuplicatedRocketCores(2) ++
   new FireSimRocketChipSingleCoreConfig)
 
 class FireSimRocketChipDualCoreTracedConfig extends Config(
@@ -107,7 +103,7 @@ class FireSimRocketChipDualCoreTracedConfig extends Config(
 
 // quad core config
 class FireSimRocketChipQuadCoreConfig extends Config(
-  new WithNBigCores(4) ++
+  new WithNDuplicatedRocketCores(4) ++
   new FireSimRocketChipSingleCoreConfig)
 
 class FireSimRocketChipQuadCoreTracedConfig extends Config(
@@ -115,7 +111,7 @@ class FireSimRocketChipQuadCoreTracedConfig extends Config(
 
 // hexa core config
 class FireSimRocketChipHexaCoreConfig extends Config(
-  new WithNBigCores(6) ++
+  new WithNDuplicatedRocketCores(6) ++
   new FireSimRocketChipSingleCoreConfig)
 
 class FireSimRocketChipHexaCoreTracedConfig extends Config(
@@ -123,7 +119,7 @@ class FireSimRocketChipHexaCoreTracedConfig extends Config(
 
 // octa core config
 class FireSimRocketChipOctaCoreConfig extends Config(
-  new WithNBigCores(8) ++
+  new WithNDuplicatedRocketCores(8) ++
   new FireSimRocketChipSingleCoreConfig)
 
 class FireSimRocketChipOctaCoreTracedConfig extends Config(
@@ -137,9 +133,21 @@ class FireSimBoomConfig extends Config(
   new WithUARTKey ++
   new WithNICKey ++
   new WithBlockDevice ++
-  new BoomWithLargeTLBs ++
+  new WithBoomL2TLBs(1024) ++
   // Using a small config because it has 64-bit system bus, and compiles quickly
   new boom.system.SmallBoomConfig)
+
+// A safer implementation than the one in BOOM in that it
+// duplicates whatever BOOMTileKey.head is present N times. This prevents
+// accidentally (and silently) blowing away configurations that may change the
+// tile in the "up" view
+class WithNDuplicatedBoomCores(n: Int) extends Config((site, here, up) => {
+  case BoomTilesKey => List.tabulate(n)(i => up(BoomTilesKey).head.copy(hartId = i))
+})
+
+class FireSimBoomDualCoreConfig extends Config(
+  new WithNDuplicatedBoomCores(2) ++
+  new FireSimBoomConfig)
 
 class FireSimBoomTracedConfig extends Config(
   new WithTraceBoom ++ new FireSimBoomConfig)
