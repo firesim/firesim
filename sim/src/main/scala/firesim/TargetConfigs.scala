9aa465c124dac160bc355210cc8acffda5a3bb80
alonamid <alonamid@eecs.berkeley.edu>
2018-11-15 01:06:46 +0000

    limit supernode mem

--- sim/src/main/scala/firesim/TargetConfigs.scala
+++ sim/src/main/scala/firesim/TargetConfigs.scala
@@ -173,25 +173,31 @@ class WithNumNodes(n: Int) extends Config((pname, site, here) => {
 
 class SupernodeFireSimRocketChipConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipConfig)
 
 class SupernodeFireSimRocketChipSingleCoreConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipSingleCoreConfig)
 
 class SupernodeFireSimRocketChipDualCoreConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipDualCoreConfig)
 
 class SupernodeFireSimRocketChipQuadCoreConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipQuadCoreConfig)
 
 class SupernodeFireSimRocketChipHexaCoreConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipHexaCoreConfig)
 
 class SupernodeFireSimRocketChipOctaCoreConfig extends Config(
   new WithNumNodes(4) ++
+  new WithExtMemSize(0x200000000L) ++ // 8GB
   new FireSimRocketChipOctaCoreConfig)
 
