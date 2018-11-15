b0ecbe7e4ea8d7e6c3da4ef8d53faa209ad0a3e8
Howard Mao <zhehao.mao@gmail.com>
2018-11-15 06:48:36 +0000

    enable larger tests in pagerank-remote2

--- deploy/workloads/pagerank-remote2/overlay/run-primary.sh
+++ deploy/workloads/pagerank-remote2/overlay/run-primary.sh
@@ -5,7 +5,7 @@ STARTPAGE=4
 
 mknod /dev/remote-mem c 254 0
 
-for size in 1024 #2048 4096 8192
+for size in 1024 2048 4096 #8192
 do
     /root/load-memblade.riscv -d $DSTMAC -s $STARTPAGE /root/pagerank-data-${size}.bin
     /root/pagerank-remote.riscv -d $DSTMAC -s $STARTPAGE -n $size -p 2 -i 0
--- deploy/workloads/pagerank-remote2/overlay/run-secondary.sh
+++ deploy/workloads/pagerank-remote2/overlay/run-secondary.sh
@@ -5,7 +5,7 @@ STARTPAGE=4
 
 mknod /dev/remote-mem c 254 0
 
-for size in 1024 #2048 4096 8192
+for size in 1024 2048 4096 #8192
 do
     /root/pagerank-remote.riscv -d $DSTMAC -s $STARTPAGE -n $size -p 2 -i 1
 done
