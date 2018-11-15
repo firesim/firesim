570540f0ad643cf80d9d0a9fffcf04eaf9ff5680
Howard Mao <zhehao.mao@gmail.com>
2018-11-15 22:05:43 +0000

    make sure multicast ethernet packets are handled properly

--- target-design/switch/flit.h
+++ target-design/switch/flit.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+#define BROADCAST_ADJUSTED (0xffff)
+
 /* ----------------------------------------------------
  * buffer flit operations
  *
@@ -58,8 +60,13 @@ int write_last_flit(uint8_t * send_buf, int tokenid, int is_last) {
 
 /* get dest mac from flit, then get port from mac */
 uint16_t get_port_from_flit(uint64_t flit, int current_port) {
+    uint16_t is_multicast = (flit >> 16) & 0x1;
     uint16_t flit_low = (flit >> 48) & 0xFFFF; // indicates dest
     uint16_t sendport = (__builtin_bswap16(flit_low));
+
+    if (is_multicast)
+	return BROADCAST_ADJUSTED;
+
     sendport = sendport & 0xFFFF;
     //printf("mac: %04x\n", sendport);
     if (sendport != 0xffff) {
