71f2b9e885c81754debfd2b1b039b5fb4f9a8070
Howard Mao <zhehao.mao@gmail.com>
2018-11-22 04:28:31 +0000

    add address range divided counters

--- src/main/cc/endpoints/fpga_memory_model.h
+++ src/main/cc/endpoints/fpga_memory_model.h
@@ -14,6 +14,30 @@
 constexpr int HISTOGRAM_SIZE = 1024;
 constexpr int BIN_SIZE = 36;
 constexpr data_t BIN_H_MASK = (1L << (BIN_SIZE - 32)) - 1;
+constexpr data_t NUM_RANGE_MASK = (1L << 4) - 1;
+constexpr data_t RANGE_BYTES_MASK = (1L << 16) - 1;
+constexpr uint64_t MEM_SIZE (1L << 35);
+
+class AddrRangeCounter: public FpgaModel {
+  public:
+    AddrRangeCounter(simif_t *sim, AddressMap addr_map, std::string name):
+      FpgaModel(sim, addr_map), name(name) {};
+    ~AddrRangeCounter(void) { /*delete [] range_bytes;*/ }
+
+    void init();
+    void profile() {}
+    void finish();
+
+    std::string name;
+    uint64_t *range_bytes;
+    size_t nranges;
+
+  private:
+    std::string enable = name + "Ranges_enable";
+    std::string dataH  = name + "Ranges_dataH";
+    std::string dataL  = name + "Ranges_dataL";
+    std::string addr   = name + "Ranges_addr";
+};
 
 class Histogram: public FpgaModel {
 public:
@@ -46,18 +70,27 @@ private:
   std::vector<uint32_t> profile_reg_addrs;
   std::ofstream stats_file;
   std::vector<Histogram> histograms;
+  std::vector<AddrRangeCounter> rangectrs;
   std::set<std::string> configuration_exclusion {
     "Hist_dataL",
     "Hist_dataH",
     "Hist_addr",
-    "Hist_enable"
+    "Hist_enable",
+    "Ranges_dataL",
+    "Ranges_dataH",
+    "Ranges_addr",
+    "Ranges_enable"
   };
 
   std::set<std::string> profile_exclusion {
     "Hist_dataL",
     "Hist_dataH",
     "Hist_addr",
-    "Hist_enable"
+    "Hist_enable",
+    "Ranges_dataL",
+    "Ranges_dataH",
+    "Ranges_addr",
+    "Ranges_enable"
   };
 
   bool has_latency_histograms() { return histograms.size() > 0; };
