4f2179959947ecfd10f6c5c7cbf2961df84c7a5b
Sarah Zhou <jxsz@berkeley.edu>
2018-11-24 21:58:21 +0000

    add new gapbs-scripts dir

---
+++ deploy/workloads/runscripts/gapbs-scripts/test.cmd
@@ -0,0 +1,6 @@
+./bc -f benchmark/graphs/kron.sg -n1 > benchmark/out/bc-kron.out
+./bfs -f benchmark/graphs/kron.sg -n1 > benchmark/out/bfs-kron.out
+./cc -f benchmark/graphs/kron.sg -n1 > benchmark/out/cc-kron.out
+./pr -f benchmark/graphs/kron.sg -n1 > benchmark/out/pr-kron.out
+./sssp -f benchmark/graphs/kron.wsg -n1 > benchmark/out/sssp-kron.out
+./tc -f benchmark/graphs/kronU.sg -n1 > benchmark/out/tc-kron.out
