c6e379b379fe8ced169dd9c67abe4864c56dad8e
Sarah Zhou <jxsz@berkeley.edu>
2018-11-27 05:46:17 +0000

    use diff jsons and inis for gapbs ref vs non-ref

--- deploy/workloads/runscripts/gapbs-scripts/gen_run_scripts.sh
+++ deploy/workloads/runscripts/gapbs-scripts/gen_run_scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #default values
-command_file=test.cmd
+command_file=ref.cmd
 t_pwd="/gapbs"
 jsonFlag=false
-input_type=test
+input_type=ref
 verifyFlag=false
 binariesFlag=false
 
@@ -11,26 +11,28 @@ binariesFlag=false
 bootbinary="bbl-vmlinux"
 output="/benchmark/out/"
 root_fs="gapbs.img"
+ini_file="gapbs.ini"
 
 #more default values
-KRON_ARGS=-g10
 SUITE="bc bfs cc cc_sv pr sssp tc ../gapbs.sh"
-workload_file="gapbs.json"
+workload_file="overlay/ref/gapbs.json"
 workload=$(basename $workload_file .json)
+
 function usage
 {
-    echo "usage gen_run_scripts.sh [--binaries] [--json] [-h] --input [test | graph500 | ref] [--verify]"
+    echo "usage: gen_run_scripts.sh [--binaries] [--json-ini] [-h] --input [test | graph500 | ref]"
 }
 
 while test $# -gt 0
 do
     case "$1" in
-         --json)
+         --json-ini)
              jsonFlag=true
              ;;
          --input)
              shift;
              input_type=$1
+             workload_file="overlay/$input_type/gapbs.json"
              command_file=$1.cmd
              ;;
          -h)
@@ -49,11 +51,9 @@ done
 if [ "$input_type" = graph500 ];
 then
     KRON_ARGS=-g20
-elif [ "$input_type" = ref ];
+elif [ "$input_type" = test ];
 then
-    echo "ref currently not supported"
-    exit
-    KRON_ARGS=-g27
+    KRON_ARGS=-g10
 fi
 
 if [ "$binariesFlag" = true ] && [ ! -d overlay/$input_type ];
@@ -63,10 +63,14 @@ then
     make converter
     CXX=${RISCV}/bin/riscv64-unknown-linux-gnu-g++ CXX_FLAGS+=--static make
     mkdir -p $overlay_dir/benchmark/graphs
-    cp $SUITE $overlay_dir/
-    ./converter $KRON_ARGS -wb $overlay_dir/benchmark/graphs/kron.wsg
-    ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kron.sg
-    ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kronU.sg
+    if [ "$input_type" = ref ]; then
+        aws s3 cp s3://gapbs-ref-graphs $overlay_dir/benchmark/graphs --recursive
+    else
+        cp $SUITE $overlay_dir/
+        ./converter $KRON_ARGS -wb $overlay_dir/benchmark/graphs/kron.wsg
+        ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kron.sg
+        ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kronU.sg
+    fi
     cd ..
 fi
 overlay_dir="overlay/$input_type"
@@ -105,9 +109,18 @@ while IFS= read -r command; do
         echo "    }," >> $workload_file
     fi
 done < $command_file
+
 if [ "$jsonFlag" = true ]; then
     echo "$(head -n -1 $workload_file)" > $workload_file
     echo "    }" >> $workload_file
     echo "  ]" >> $workload_file
     echo "}" >> $workload_file
+    if [ "$input_type" = ref ]; then
+        sed -i 's/f1_2xlarges.*/f1_2xlarges=30/' $ini_file
+        sed -i 's/no_net_num.*/no_net_num_nodes=30/' $ini_file
+    else
+        sed -i 's/f1_2xlarges.*/f1_2xlarges=6/' $ini_file
+        sed -i 's/no_net_num.*/no_net_num_nodes=6/' $ini_file
+    fi
+    cp $ini_file overlay/$input_type/
 fi
