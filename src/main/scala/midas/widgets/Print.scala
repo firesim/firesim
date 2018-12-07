730c0c519e008c286fdddc32844b14b4828c82f1
David Biancolin <david.biancolin@gmail.com>
2018-12-07 12:53:11 -0800

    [MIDAS] [WIP] Get print synthesis generating correctly

--- src/main/scala/midas/widgets/Print.scala
+++ src/main/scala/midas/widgets/Print.scala
@@ -26,7 +26,7 @@ class PrintRecord(argTypes: Seq[firrtl.ir.Type]) extends Record {
   }
   val enable = Output(Bool())
   val args: Seq[(String, Data)] =argTypes.zipWithIndex.map({ case (tpe, idx) =>
-    "args_${idx}" -> Output(regenLeafType(tpe))
+    s"args_${idx}" -> Output(regenLeafType(tpe))
   })
   val elements = ListMap((Seq("enable" -> enable) ++ args):_*)
   override def cloneType = new PrintRecord(argTypes).asInstanceOf[this.type]
@@ -44,6 +44,9 @@ class PrintRecordBag(prefix: String, printPorts: Seq[firrtl.ir.Port]) extends Re
   })
   val elements = ListMap(ports:_*)
   override def cloneType = new PrintRecordBag(prefix, printPorts).asInstanceOf[this.type]
+
+  // Generates a Bool indicating if at least one Printf has it's enable set on this cycle
+  def hasEnabledPrint(): Bool = elements.map(_._2.enable).foldLeft(false.B)(_ || _)
 }
 
 class PrintRecordEndpoint extends Endpoint {
@@ -69,91 +72,40 @@ class PrintWidgetIO(private val record: PrintRecordBag)(implicit p: Parameters) 
   val hPort = Flipped(HostPort(record))
 }
 
-class PrintWidget(printRecord: PrintRecord)(implicit p: Parameters) extends EndpointWidget()(p) {
+class PrintWidget(printRecord: PrintRecordBag)(implicit p: Parameters) extends EndpointWidget()(p)
+    with UnidirectionalDMAToHostCPU {
   val io = IO(new PrintWidgetIO(printRecord))
-  io <> DontCare
+  lazy val toHostCPUQueueDepth = 512 * 4 // 4 Ultrascale+ URAMs
+  lazy val dmaSize = BigInt(512 * 4)
 
-//  val printPort = io.hPort.hBits
-//  val fire = Wire(Bool())
-//  val cycles = Reg(UInt(48.W))
-//  val enable = RegInit(false.B)
-//  val enableAddr = attach(enable, "enable")
-//  val printAddrs = collection.mutable.ArrayBuffer[Int]()
-//  val countAddrs = collection.mutable.ArrayBuffer[Int]()
-//  val channelWidth = io.dma.get.nastiXDataBits
-//  val printWidth = (printPort.elements foldLeft 56)(_ + _._2.getWidth - 1)
-//  val valid = (printPort.elements foldLeft false.B)( _ || _._2(0))
-//  val ps = printPort.elements.toSeq map (_._2 >> 1)
-//  val vs = printPort.elements.toSeq map (_._2(0))
-//  val data = Cat(Cat(ps.reverse), Cat(vs.reverse) | 0.U(8.W), cycles)
-//  /* FIXME */ scala.Predef.assert(printWidth <= channelWidth)
-//
-//  val prints = (0 until printWidth by channelWidth).zipWithIndex map { case (off, i) =>
-//    val width = channelWidth min (printWidth - off)
-//    val wires = Wire(Decoupled(UInt(width.W)))
-//    val queue = BRAMQueue(wires, 8 * 1024)
-//    wires.bits  := data(width + off - 1, off)
-//    wires.valid := fire && enable && valid
-//    if (countAddrs.isEmpty) {
-//      val count = RegInit(0.U(24.W))
-//      count suggestName "count"
-//      when (wires.fire() === queue.fire()) {
-//      }.elsewhen(wires.fire()) {
-//        count := count + 1.U
-//      }.elsewhen(queue.fire()) {
-//        count := count - 1.U
-//      }
-//      countAddrs += attach(count, "prints_count", ReadOnly)
-//    }
-//
-//    io.dma.foreach({ dma =>
-//      val arQueue   = Queue(dma.ar, 10)
-//      val readBeats = RegInit(0.U(9.W))
-//      readBeats suggestName "readBeats"
-//      when(dma.r.fire()) {
-//        readBeats := Mux(dma.r.bits.last, 0.U, readBeats + 1.U)
-//      }
-//
-//      queue.ready := dma.r.ready && arQueue.valid
-//      dma.r.valid := queue.valid && arQueue.valid
-//      dma.r.bits.data := queue.bits
-//      dma.r.bits.last := arQueue.bits.len === readBeats
-//      dma.r.bits.id   := arQueue.bits.id
-//      dma.r.bits.user := arQueue.bits.user
-//      dma.r.bits.resp := 0.U
-//      arQueue.ready := dma.r.fire() && dma.r.bits.last
-//
-//      // No write
-//      dma.aw.ready := false.B
-//      dma.w.ready := false.B
-//      dma.b.valid := false.B
-//      dma.b.bits := DontCare
-//    })
-//    wires.ready || !valid
-//  }
-//  fire := (prints foldLeft (io.hPort.toHost.hValid && io.tReset.valid))(_ && _)
-//  io.tReset.ready := fire
-//  io.hPort.toHost.hReady := fire
-//  // We don't generate tokens
-//  io.hPort.fromHost.hValid := true.B
-//  when (fire) {
-//    cycles := Mux(io.tReset.bits, 0.U, cycles + 1.U)
-//  }
-//
-//  override def genHeader(base: BigInt, sb: StringBuilder) {
-//    import CppGenerationUtils._
-//    sb.append(genComment("Print Widget"))
-//    sb.append(genMacro("PRINTS_NUM", UInt32(printPort.elements.size)))
-//    sb.append(genMacro("PRINTS_CHUNKS", UInt32(prints.size)))
-//    sb.append(genMacro("PRINTS_ENABLE", UInt32(base + enableAddr)))
-//    sb.append(genMacro("PRINTS_COUNT_ADDR", UInt32(base + countAddrs.head)))
-//    sb.append(genArray("PRINTS_WIDTHS", printPort.elements.toSeq map (x => UInt32(x._2.getWidth))))
-//    if (!p(HasDMAChannel)) {
-//      sb.append(genArray("PRINTS_DATA_ADDRS", printAddrs.toSeq map (x => UInt32(base + x))))
-//    } else {
-//      sb.append(genMacro("HAS_DMA_CHANNEL"))
-//    }
-//  }
-//
-//  genCRFile()
+  val printPort = io.hPort.hBits
+  val totalPrintWidth = printPort.getWidth
+  /* FIXME */ assert(totalPrintWidth + 1 <= dma.nastiXDataBits)
+  val valid = printPort.hasEnabledPrint
+  val data = Cat(printPort.asUInt, valid) | 0.U(dma.nastiXDataBits.W)
+
+  val tFireHelper = DecoupledHelper(io.hPort.toHost.hValid, io.tReset.valid, outgoingPCISdat.io.enq.ready)
+  io.tReset.ready := tFireHelper.fire(io.tReset.valid)
+  io.hPort.toHost.hReady := tFireHelper.fire(io.hPort.toHost.hValid)
+  outgoingPCISdat.io.enq.bits := data
+  outgoingPCISdat.io.enq.valid := tFireHelper.fire(outgoingPCISdat.io.enq.ready)
+
+  // We don't generate tokens
+  io.hPort.fromHost.hValid := true.B
+
+  //override def genHeader(base: BigInt, sb: StringBuilder) {
+  //  import CppGenerationUtils._
+  //  sb.append(genComment("Print Widget"))
+  //  sb.append(genMacro("PRINTS_NUM", UInt32(printPort.elements.size)))
+  //  sb.append(genMacro("PRINTS_CHUNKS", UInt32(prints.size)))
+  //  sb.append(genMacro("PRINTS_ENABLE", UInt32(base + enableAddr)))
+  //  sb.append(genMacro("PRINTS_COUNT_ADDR", UInt32(base + countAddrs.head)))
+  //  sb.append(genArray("PRINTS_WIDTHS", printPort.elements.toSeq map (x => UInt32(x._2.getWidth))))
+  //  if (!p(HasDMAChannel)) {
+  //    sb.append(genArray("PRINTS_DATA_ADDRS", printAddrs.toSeq map (x => UInt32(base + x))))
+  //  } else {
+  //    sb.append(genMacro("HAS_DMA_CHANNEL"))
+  //  }
+  //}
+  genCRFile()
 }
