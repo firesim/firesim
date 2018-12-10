d7ae53ea90f800ad584a604592644f885bd10d4d
Albert Magyar <albert.magyar@gmail.com>
2018-12-10 15:35:53 -0800

    Add annotation to gate SRAM model extraction

--- targetutils/src/main/scala/midas/annotations.scala
+++ targetutils/src/main/scala/midas/annotations.scala
@@ -2,7 +2,7 @@
 
 package midas.targetutils
 
-import firrtl.annotations.{SingleTargetAnnotation, ComponentName}
+import firrtl.annotations.{SingleTargetAnnotation, ComponentName, ReferenceTarget}
 
 // This is currently consumed by a transformation that runs after MIDAS's core
 // transformations In FireSim, targeting an F1 host, these are consumed by the
@@ -16,4 +16,16 @@ case class FpgaDebugAnnotation(target: chisel3.Data)
 case class FirrtlFpgaDebugAnnotation(target: ComponentName) extends
     SingleTargetAnnotation[ComponentName] {
   def duplicate(n: ComponentName) = this.copy(target = n)
+}
+
+
+// This labels a target Mem so that it is extracted and replaced with a separate model
+case class MemModelAnnotation[T <: chisel3.Data](target: chisel3.MemBase[T])
+    extends chisel3.experimental.ChiselAnnotation {
+  def toFirrtl = FirrtlMemModelAnnotation(target.toNamed.toTarget)
+}
+
+case class FirrtlMemModelAnnotation(target: ReferenceTarget) extends
+    SingleTargetAnnotation[ReferenceTarget] {
+  def duplicate(rt: ReferenceTarget) = this.copy(target = rt)
 }
