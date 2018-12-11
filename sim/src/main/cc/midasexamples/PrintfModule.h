886c987210785172d84c38717e39f7884d617a0f
David Biancolin <david.biancolin@gmail.com>
2018-12-11 13:09:06 -0800

    [midasexamples] Update initialization and teardown of print widget

--- sim/src/main/cc/midasexamples/PrintfModule.h
+++ sim/src/main/cc/midasexamples/PrintfModule.h
@@ -1,26 +1,30 @@
 //See LICENSE for license details.
 
+#include <memory>
+
 #include "simif.h"
 #include "endpoints/synthesized_prints.h"
 
 class PrintfModule_t: virtual simif_t
 {
 public:
-    synthesized_prints_t * print_endpoint;
+    std::unique_ptr<synthesized_prints_t> print_endpoint;
     PrintfModule_t(int argc, char** argv) {
         PRINTWIDGET_0_substruct_create;
         std::vector<std::string> args(argv + 1, argv + argc);
-        print_endpoint = new synthesized_prints_t(this,
-                                                  args,
-                                                  PRINTWIDGET_0_substruct,
-                                                  PRINTWIDGET_0_print_count,
-                                                  PRINTWIDGET_0_print_offsets,
-                                                  PRINTWIDGET_0_format_strings,
-                                                  PRINTWIDGET_0_argument_counts,
-                                                  PRINTWIDGET_0_argument_widths,
-                                                  PRINTWIDGET_0_DMA_ADDR);
+        print_endpoint = std::unique_ptr<synthesized_prints_t>(new synthesized_prints_t(this,
+                                                                  args,
+                                                                  PRINTWIDGET_0_substruct,
+                                                                  PRINTWIDGET_0_print_count,
+                                                                  PRINTWIDGET_0_print_offsets,
+                                                                  PRINTWIDGET_0_format_strings,
+                                                                  PRINTWIDGET_0_argument_counts,
+                                                                  PRINTWIDGET_0_argument_widths,
+                                                                  PRINTWIDGET_0_DMA_ADDR));
     };
-    void run() {
+
+    virtual void run() {
+        print_endpoint->init();
         poke(reset, 1);
         poke(a, 0);
         poke(b, 0);
@@ -33,10 +37,6 @@ public:
         while (!done()) {
             print_endpoint->tick();
         }
-        expect(false, "Flesh me out");
-    };
-
-    ~PrintfModule_t() {
-      delete print_endpoint;
+        print_endpoint->finish();
     };
 };
