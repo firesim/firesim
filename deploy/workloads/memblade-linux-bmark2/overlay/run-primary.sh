d0fa560c932fdb8298b07ed38df47d1409d275ec
Howard Mao <zhehao.mao@gmail.com>
2019-01-05 21:39:04 +0000

    each remote mem client can now safely use maximum bandwidth

--- deploy/workloads/memblade-linux-bmark2/overlay/run-primary.sh
+++ deploy/workloads/memblade-linux-bmark2/overlay/run-primary.sh
@@ -4,6 +4,6 @@ DSTMAC=0400006D1200
 
 mknod /dev/remote-mem c 254 0
 
-/root/memblade-bmark.riscv -n 10000 -s 4 -w 8 -r 8 -p 2 -d $DSTMAC -b 80016 -i
+/root/memblade-bmark.riscv -n 10000 -s 4 -w 16 -r 16 -p 2 -d $DSTMAC -b 80016 -i
 
 poweroff
