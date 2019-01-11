5eae151201fc053f7e9f6a00da2c4d60ef517d82
Dinesh Parimi <dinesh.parimi@berkeley.edu>
2019-01-11 18:35:16 +0000

    Compiles with MIDAS, verilator output missing firesim.d

--- sim/src/main/cc/endpoints/uart.h
+++ sim/src/main/cc/endpoints/uart.h
@@ -1,8 +1,27 @@
 #ifndef __UART_H
 #define __UART_H
 
-#include "serial.h"
+// #include "serial.h"
 #include <signal.h>
+#include "endpoints/endpoint.h"
+#include "fesvr/firesim_fesvr.h"
+
+// temp added from serial.h
+template<class T>
+struct serial_data_t {
+    struct {
+        T bits;
+        bool valid;
+        bool ready;
+        bool fire() { return valid && ready; }
+    } in;
+    struct {
+        T bits;
+        bool ready;
+        bool valid;
+        bool fire() { return valid && ready; }
+    } out;
+};
 
 class uart_t: public endpoint_t
 {
