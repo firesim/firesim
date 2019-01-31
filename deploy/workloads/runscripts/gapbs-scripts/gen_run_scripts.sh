2a4795fc35527eb43a261c65d1ad27363dfd9d1a
Sarah Zhou <jxsz@berkeley.edu>
2019-01-31 05:15:17 +0000

    remove ref functionality in docs, gen_run_scripts, and Makefile

--- deploy/workloads/runscripts/gapbs-scripts/gen_run_scripts.sh
+++ deploy/workloads/runscripts/gapbs-scripts/gen_run_scripts.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 #default values
-command_file=ref.cmd
+command_file=test.cmd
 t_pwd="/gapbs"
 jsonFlag=false
-input_type=ref
-verifyFlag=false
+input_type=test
 binariesFlag=false
+KRON_ARGS=-g10
+SUITE="bc bfs cc cc_sv pr sssp tc ../gapbs.sh"
+workload_file="gapbs.json"
+workload=$(basename $workload_file .json)
 
 #default vals for json
 bootbinary="bbl-vmlinux"
 output="/benchmark/out/"
 root_fs="gapbs.img"
-ini_file="gapbs.ini"
-
-#more default values
-SUITE="bc bfs cc cc_sv pr sssp tc ../gapbs.sh"
-workload_file="overlay/ref/gapbs.json"
-workload=$(basename $workload_file .json)
 
 function usage
 {
-    echo "usage: gen_run_scripts.sh [--binaries] [--json-ini] [-h] --input [test | graph500 | ref]"
+    echo "usage: gen_run_scripts.sh [--binaries] [--json] [-h] --input [test | graph500 | ref]"
 }
 
 while test $# -gt 0
 do
     case "$1" in
-         --json-ini)
+         --json)
              jsonFlag=true
              ;;
          --input)
              shift;
              input_type=$1
-             workload_file="overlay/$input_type/gapbs.json"
              command_file=$1.cmd
              ;;
          -h)
@@ -42,18 +38,17 @@ do
          --binaries)
              binariesFlag=true
              ;;
-          --verify)
-             verifyFlag=true
-             ;;
     esac
     shift
 done
 if [ "$input_type" = graph500 ];
 then
     KRON_ARGS=-g20
-elif [ "$input_type" = test ];
+elif [ "$input_type" = ref ];
 then
-    KRON_ARGS=-g10
+    echo "ref currently not supported"
+    exit
+    KRON_ARGS=-g27
 fi
 
 if [ "$binariesFlag" = true ] && [ ! -d overlay/$input_type ];
@@ -64,13 +59,9 @@ then
     CXX=${RISCV}/bin/riscv64-unknown-linux-gnu-g++ CXX_FLAGS+=--static make
     mkdir -p $overlay_dir/benchmark/graphs
     cp $SUITE $overlay_dir/
-    if [ "$input_type" = ref ]; then
-        aws s3 cp s3://gapbs-ref-graphs $overlay_dir/benchmark/graphs --recursive
-    else
-        ./converter $KRON_ARGS -wb $overlay_dir/benchmark/graphs/kron.wsg
-        ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kron.sg
-        ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kronU.sg
-    fi
+    ./converter $KRON_ARGS -wb $overlay_dir/benchmark/graphs/kron.wsg
+    ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kron.sg
+    ./converter $KRON_ARGS -b $overlay_dir/benchmark/graphs/kronU.sg
     cd ..
 fi
 overlay_dir="overlay/$input_type"
@@ -84,7 +75,7 @@ then
     echo "  \"deliver_dir\" : \"${workload}\"," >> $workload_file
     echo "  \"common_args\" : []," >> $workload_file
     echo "  \"common_files\" : [\"gapbs.sh\"]," >> $workload_file
-    echo "  \"common_outputs\" : [\"/output\"]," >> $workload_file
+    echo "  \"common_simulation_outputs:\" : [\"uartlog\"]," >> $workload_file
     echo "  \"workloads\" : [" >> $workload_file
 fi
 
@@ -97,6 +88,7 @@ while IFS= read -r command; do
     run_script=$overlay_dir/run/${workload}.sh
     run_script_no_overlay=run/${workload}.sh
     echo '#!/bin/bash' > $run_script
+    #verify option always inserted into run_script
     echo $command | sed "s/benchmark/\\${t_pwd}\/benchmark/g" | sed "s/^\./\\${t_pwd}/" | sed "s/-n/\$1 -n/g" |sed "s/ >.*//" >> $run_script
 
     chmod +x $run_script
@@ -109,18 +101,9 @@ while IFS= read -r command; do
         echo "    }," >> $workload_file
     fi
 done < $command_file
-
 if [ "$jsonFlag" = true ]; then
     echo "$(head -n -1 $workload_file)" > $workload_file
     echo "    }" >> $workload_file
     echo "  ]" >> $workload_file
     echo "}" >> $workload_file
-    if [ "$input_type" = ref ]; then
-        sed -i 's/f1_2xlarges.*/f1_2xlarges=30/' $ini_file
-        sed -i 's/no_net_num.*/no_net_num_nodes=30/' $ini_file
-    else
-        sed -i 's/f1_2xlarges.*/f1_2xlarges=6/' $ini_file
-        sed -i 's/no_net_num.*/no_net_num_nodes=6/' $ini_file
-    fi
-    cp $ini_file overlay/$input_type/
 fi
