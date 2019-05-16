545d2184975e5e43e364614d1a09959774ce3673
David Biancolin <david.biancolin@gmail.com>
2019-05-16 19:03:05 +0000

    Chisel-bump related fixes

--- src/main/scala/midas/core/Interfaces.scala
+++ src/main/scala/midas/core/Interfaces.scala
@@ -23,9 +23,8 @@ object HostDecoupled {
   def apply[T <: Data](gen: T): HostDecoupledIO[T] = new HostDecoupledIO(gen)
 }
 
-
 class HostReadyValid extends Bundle {
-  val hReady= Input(Bool())
+  val hReady = Input(Bool())
   val hValid = Output(Bool())
   def fire(): Bool = hReady && hValid
 }
@@ -45,38 +44,11 @@ class HostPortIO[+T <: Data](gen: T, tokenFlip: Boolean) extends Bundle
 {
   val fromHost = Flipped(new HostReadyValid)
   val toHost = new HostReadyValid
-  val hBits  = if (tokenFlip) Flipped(gen.cloneType) else gen.cloneType
+  val hBits  = if (tokenFlip) Flipped(gen) else gen
   override def cloneType: this.type =
     new HostPortIO(gen, tokenFlip).asInstanceOf[this.type]
 }
 
 object HostPort {
   def apply[T <: Data](gen: T, tokenFlip : Boolean = false): HostPortIO[T] = new HostPortIO(gen, tokenFlip)
-}
-
-
-// The below is currently used to generate FIRRTL code for buildSimQueue
-// TODO
-//  - Actually use this code in buildSimQueue instead of one off FIRRTL
-//  - This requires either a change to Bundle API (and possible others)
-//      or using Scala macros (see quasiquotes)
-
-/** An I/O Bundle with simple handshaking using valid and ready signals for
-  * data 'bits'
-  */
-class MidasDecoupledIO[+T <: Data](gen: T) extends Bundle
-{
-  val hReady = Input(Bool())
-  val hValid = Output(Bool())
-  val hBits  = gen.cloneType.asOutput
-  def fire(): Bool = hReady && hValid
-  override def cloneType: this.type =
-    new MidasDecoupledIO(gen).asInstanceOf[this.type]
-}
-
-/** Adds a hReady-hValid handshaking protocol to any interface.
-  * The standard used is that the consumer uses the flipped interface.
-  */
-object MidasDecoupled {
-  def apply[T <: Data](gen: T): MidasDecoupledIO[T] = new MidasDecoupledIO(gen)
 }
