13f0c409cdc1c93b428af6cb7333169085b3c147
David Biancolin <david.biancolin@gmail.com>
2019-05-28 01:47:32 +0000

    Purge SRAM macro handling in MIDAS

--- src/main/scala/midas/passes/MidasTransforms.scala
+++ src/main/scala/midas/passes/MidasTransforms.scala
@@ -19,50 +19,32 @@ private[passes] class WCircuit(
   main: String,
   val sim: SimWrapperIO) extends Circuit(info, modules, main)
 
-object MidasAnnotation {
-  def apply(t: String, conf: File, json: File, lib: Option[File]) =
-    Annotation(CircuitName(t), classOf[MidasTransforms],
-               s"$conf $json %s".format(lib map (_.toString) getOrElse ""))
-  private val matcher = "([^ ]+) ([^ ]+) ([^ ]*)".r
-  def unapply(a: Annotation) = a match {
-    case Annotation(CircuitName(c), t, matcher(conf, json, lib))
-      if t == classOf[MidasTransforms] =>
-        Some(c, new File(conf), new File(json), if (lib.isEmpty) None else Some(new File(lib)))
-    case _ => None
-  }
-}
-
 private[midas] class MidasTransforms(
     io: Seq[(String, chisel3.Data)])
     (implicit param: freechips.rocketchip.config.Parameters) extends Transform {
   def inputForm = LowForm
   def outputForm = LowForm
   val dir = param(OutputDir)
-  def execute(state: CircuitState) = (getMyAnnotations(state): @unchecked) match {
-    case Seq(MidasAnnotation(state.circuit.main, conf, json, lib)) =>
-      val xforms = Seq(
-        firrtl.passes.RemoveValidIf,
-        new firrtl.transforms.ConstantPropagation,
-        firrtl.passes.SplitExpressions,
-        firrtl.passes.CommonSubexpressionElimination,
-        new firrtl.transforms.DeadCodeElimination,
-        new ConfToJSON(conf, json),
-        new barstools.macros.MacroCompilerTransform,
-        firrtl.passes.ResolveKinds,
-        firrtl.passes.RemoveEmpty,
-        // NB: Carelessly removing this pass will break the FireSim manager as we always
-        // need to generate the *.asserts file. Fix by baking into driver.
-        new AssertPass(dir),
-        new PrintSynthesis(dir),
-        new Fame1Transform(Some(lib getOrElse json)),
-        new strober.passes.StroberTransforms(dir, lib getOrElse json)) ++
-      // Any subFields must be flattened out beforing linking in HighForm constructs must Lower 
-      firrtl.CompilerUtils.getLoweringTransforms(HighForm, LowForm) ++ Seq(
-        new SimulationMapping(io),
-        new PlatformMapping(state.circuit.main, dir)
-      )
-      (xforms foldLeft state)((in, xform) =>
-        xform runTransform in).copy(form=outputForm)
+  def execute(state: CircuitState) = {
+    val xforms = Seq(
+      firrtl.passes.RemoveValidIf,
+      new firrtl.transforms.ConstantPropagation,
+      firrtl.passes.SplitExpressions,
+      firrtl.passes.CommonSubexpressionElimination,
+      new firrtl.transforms.DeadCodeElimination,
+      firrtl.passes.ResolveKinds,
+      firrtl.passes.RemoveEmpty,
+      // NB: Carelessly removing this pass will break the FireSim manager as we always
+      // need to generate the *.asserts file. Fix by baking into driver.
+      new AssertPass(dir),
+      new PrintSynthesis(dir),
+      new Fame1Transform) ++
+    // Any subFields must be flattened out beforing linking in HighForm constructs must Lower 
+    firrtl.CompilerUtils.getLoweringTransforms(HighForm, LowForm) ++ Seq(
+      new SimulationMapping(io),
+      new PlatformMapping(state.circuit.main, dir)
+    )
+    (xforms foldLeft state)((in, xform) => xform runTransform in).copy(form=outputForm)
   }
 }
 
