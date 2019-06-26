310613a3db77a09f59cd1d078be244a0c721426a
Howard Mao <zhehao.mao@gmail.com>
2019-06-26 10:54:25 -0700

    get rid of FireSimDDR3FRFCFSConfig75MHz

--- sim/src/main/scala/firesim/SimConfigs.scala
+++ sim/src/main/scala/firesim/SimConfigs.scala
@@ -200,10 +200,6 @@ class FireSimDDR3FRFCFSConfig extends Config(
   new FRFCFS16GBQuadRank ++
   new FireSimConfig)
 
-class FireSimDDR3FRFCFSConfig75MHz extends Config(
-  new WithDesiredHostFrequency(75) ++
-  new FireSimDDR3FRFCFSConfig)
-
 class FireSimDDR3FRFCFSConfigAsserts extends Config(
   new WithSynthAsserts ++
   //new WithPrintfSynthesis ++
