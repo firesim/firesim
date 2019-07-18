6b46fd1864ff4c07515049e7ea99a935195fc47e
alonamid <alonamid@eecs.berkeley.edu>
2019-07-18 02:02:46 +0000

    fix submodule init

--- build-setup-nolog.sh
+++ build-setup-nolog.sh
@@ -65,7 +65,7 @@ done
 # ignore riscv-tools for submodule init recursive
 # you must do this globally (otherwise riscv-tools deep
 # in the submodule tree will get pulled anyway
-git config --global submodule.riscv-tools.update none
+git config --global submodule.toolchains/riscv-tools.update none
 git config --global submodule.experimental-blocks.update none
 git config --global submodule.sims/firesim.update none
 # Disable the REBAR submodule initially, and enable if we're not in library mode
@@ -73,7 +73,7 @@ git config submodule.target-design/chipyard.update none
 git submodule update --init --recursive #--jobs 8
 # unignore riscv-tools,catapult-shell2 globally
 git config --global --unset submodule.sims/firesim.update
-git config --global --unset submodule.riscv-tools.update
+git config --global --unset submodule.toolchains/riscv-tools.update
 git config --global --unset submodule.experimental-blocks.update
 
 if [ "$IS_LIBRARY" = false ]; then
