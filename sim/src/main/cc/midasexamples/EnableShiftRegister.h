2040573692b1a3be496820de5569a07d229f3170
David Biancolin <david.biancolin@gmail.com>
2019-09-10 03:58:20 +0000

    [midasexamples] Update expected token timings in TB until we use wire channels again

--- sim/src/main/cc/midasexamples/EnableShiftRegister.h
+++ sim/src/main/cc/midasexamples/EnableShiftRegister.h
@@ -1,6 +1,7 @@
 //See LICENSE for license details.
 
 #include "simif.h"
+#include "shift_register.h"
 
 class EnableShiftRegister_t: public virtual simif_t
 {
@@ -8,18 +9,24 @@ public:
   EnableShiftRegister_t(int argc, char** argv) {}
   void run() {
     target_reset();
+    // Until we re-enable wire-channel initialization on peek poke, need an
+    // extra cycle for targetreset to propagate
+    step(1);
     std::vector<uint32_t> reg(4, 0);
+    ShiftRegister<uint32_t> outputDelayer(2, 0);
     for (int i = 0 ; i < 64 ; i++) {
       uint32_t in    = rand_next(16);
       uint32_t shift = rand_next(2);
       poke(io_in,    in);
       poke(io_shift, shift);
+      outputDelayer.enqueue(reg[3]);
+      expect(io_out, outputDelayer.current());
       step(1);
       if (shift) {
         for (int j = 3 ; j > 0 ; j--) reg[j] = reg[j-1];
         reg[0] = in;
       }
-      expect(io_out, reg[3]);
+      outputDelayer.step();
     }
   }
 };
--- sim/src/main/cc/midasexamples/ResetShiftRegister.h
+++ sim/src/main/cc/midasexamples/ResetShiftRegister.h
@@ -1,6 +1,7 @@
 //See LICENSE for license details.
 
 #include "simif.h"
+#include "shift_register.h"
 
 class ResetShiftRegister_t: public virtual simif_t
 {
@@ -10,15 +11,21 @@ public:
     std::vector<uint32_t> ins(5, 0);
     int k = 0;
     target_reset();
+    // Until we re-enable wire-channel initialization on peek poke, need an
+    // extra cycle for targetreset to propagate
+    step(1);
+    ShiftRegister<uint32_t> outputDelayer(2, 0);
     for (int i = 0 ; i < 64 ; i++) {
       uint32_t in    = rand_next(16);
       uint32_t shift = rand_next(2);
       if (shift == 1) ins[k % 5] = in;
       poke(io_in,    in);
       poke(io_shift, shift);
+      outputDelayer.enqueue(cycles() < 3 ? 0 : ins[(k + 1) % 5]);
+      expect(io_out, outputDelayer.current());
       step(1);
+      outputDelayer.step();
       if (shift == 1) k++;
-      expect(io_out, cycles() < 3 ? 0 : ins[(k + 1) % 5]);
     }
   }
 };
