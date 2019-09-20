ee6223cf3b407d8a4a56172bb1dfebd3ccc6c938
Nathan Pemberton <nathanp@berkeley.edu>
2019-09-20 00:37:32 +0000

    Make the default initramfs more verbose about which root device is booting

--- wlutil/initramfsRoot/setupSystem.sh
+++ wlutil/initramfsRoot/setupSystem.sh
@@ -9,8 +9,10 @@ mount -t sysfs none /sys
 mount -t devtmpfs none /dev
 
 if [ -e /dev/iceblk ]; then
+  echo "Mounting /dev/iceblk as root device"
   mount -o ro /dev/iceblk /mnt/root
 elif [ -e /dev/vda ]; then
+  echo "Mounting /dev/vda as root device"
   mount -o ro /dev/vda /mnt/root
 else
   echo "Failed to load an appropriate block device. Dropping into emergency shell:"
