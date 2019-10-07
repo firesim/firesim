c357f37011bf888a4054b9b4de42b47c17d39c38
Nathan Pemberton <nathanp@berkeley.edu>
2019-10-06 20:58:26 -0400

    Add /dev/null to special files for initramfs to avoid startup errors

--- wlutil/initramfs/makeDevNodes.sh
+++ wlutil/initramfs/makeDevNodes.sh
@@ -7,13 +7,21 @@
 # initramfs. It must be run in a fakeroot environment in order to create the
 # special device files /dev/console and /dev/tty before packaging..
 
-pushd initramfsRoot/
+orig_pwd=${PWD}
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+
+pushd $tmp_dir
 
 echo "Making Nodes"
+mkdir dev
 mknod -m 622 dev/console c 5 1
-mknod -m 666 /dev/tty c 5 0
+mknod -m 666 dev/tty c 5 0
+mknod -m 666 dev/null c 1 3
 
 echo "Building initrd"
-find -print0 | cpio --owner root:root --null -ov --format=newc > ../initramfsRoot.cpio
+find -print0 | cpio --owner root:root --null -ov --format=newc > $orig_pwd/devNodes.cpio
+popd
+
+rm -r $tmp_dir
 
 exit
