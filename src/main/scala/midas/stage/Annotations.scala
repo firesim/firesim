37b523d148dba2216cb80500e75cbbe82dc949fe
David Biancolin <david.biancolin@gmail.com>
2019-10-10 02:15:54 +0000

    [MIDAS] [Stage] Add another main to generate runtime configs for FASED

--- src/main/scala/midas/stage/Annotations.scala
+++ src/main/scala/midas/stage/Annotations.scala
@@ -44,3 +44,18 @@ object ConfigStringAnnotation extends HasShellOptions {
       shortOption = Some("ggcs"),
       helpValueName = Some("<class name>{[_<additional class names>]}}") ) )
 }
+
+// Used to specify the name of the desired runtime configuration
+// file.  Will be emitted in the TargetDir
+case class RuntimeConfigNameAnnotation(configString: String) extends NoTargetAnnotation
+
+object RuntimeConfigNameAnnotation extends HasShellOptions {
+
+  val options = Seq(
+    new ShellOption[String](
+      longOption = "golden-gate-runtime-config-name",
+      toAnnotationSeq = (a: String) => Seq(RuntimeConfigNameAnnotation(a)),
+      helpText = "Specifies the filename for the generated runtime configuration file.",
+      shortOption = Some("ggrc"),
+      helpValueName = Some("<filename>") ) )
+}
---
+++ src/main/scala/midas/stage/RuntimeConfigGeneratorCli.scala
@@ -0,0 +1,15 @@
+// See LICENSE for license details.
+
+package midas.stage
+
+import firrtl.options.Shell
+
+trait RuntimeConfigGeneratorCli { this: Shell =>
+  parser.note("Golden Gate Runtime Configuration Generator Options")
+  Seq(GoldenGateInputAnnotationFileAnnotation,
+      ConfigPackageAnnotation,
+      ConfigStringAnnotation,
+      RuntimeConfigNameAnnotation
+    )
+    .map(_.addOptions(parser))
+}
