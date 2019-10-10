76b89b580c361cc3753899b80c3fd31e6108a76d
Nathan Pemberton <nathanp@berkeley.edu>
2019-10-10 02:09:57 +0000

    Spike no longer includes dummy_rocc by default (or rather, refuses to build the shared library for it). We include a stable version of spike that is known to work in the unit test now.

--- full_test.sh
+++ full_test.sh
@@ -40,7 +40,7 @@ fi
 # shouldn't be tested (e.g. we exclude the base configs and some specialized
 # tests)
 echo "Running regular tests" | tee -a $LOGNAME
-BULK_EXCLUDE="(br-base|fedora-base|incremental|clean|timeout-build|timeout-run|bare|dummy-bare|spike-jobs|spike|spike-args)"
+BULK_EXCLUDE="(br-base|fedora-base|incremental|clean|timeout-build|timeout-run|bare|dummy-bare|spike-jobs|spike|spike-args|rocc)"
 ./marshal clean test/!$BULK_EXCLUDE.json | tee -a $LOGNAME
 ./marshal test test/!$BULK_EXCLUDE.json | tee -a $LOGNAME
 if [ ${PIPESTATUS[0]} != 0 ]; then
--- test/rocc/build.sh
+++ test/rocc/build.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
+set -e
+
+SPIKE_INSTALL=$PWD/spike_local
+mkdir -p $SPIKE_INSTALL
+
+# Get a version of spike that includes the dummy_rocc accelerator
+if [ ! -d riscv-isa-sim ]; then
+  git clone https://github.com/riscv/riscv-isa-sim.git
+  pushd riscv-isa-sim
+  git checkout 2dbcb01ca1c026b867cf673203646d213f6e6b5c
+  popd
+
+  pushd riscv-isa-sim
+  mkdir build
+  pushd build
+  ../configure --with-fesvr=$RISCV --prefix=$SPIKE_INSTALL
+  popd
+  popd
+fi
+
+pushd riscv-isa-sim/build
+make -j16
+make install
+popd
+
+# Build the benchmark
 make
