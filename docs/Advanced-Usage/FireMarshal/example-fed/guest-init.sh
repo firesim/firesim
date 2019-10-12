f3f8751413d95f3f3964b08451221974c52a5b8d
Nathan Pemberton <nathanp@berkeley.edu>
2019-10-12 01:20:10 +0000

    Removes the bulk of FireMarshal docs and replaces it with just the quickstart and a link to marshal's own readthedocs.

--- docs/Advanced-Usage/FireMarshal/example-fed/guest-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing the real time tool (not the shell builtin)"
-dnf install -y time
-
-echo "Installing the spambayes python module for the spam benchmark"
-pip install spambayes
-
-poweroff
