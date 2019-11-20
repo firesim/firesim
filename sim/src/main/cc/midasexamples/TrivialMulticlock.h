e407176a7c511dce6d9c56f9db9e7e8b094be051
David Biancolin <david.biancolin@gmail.com>
2019-11-19 21:16:52 -0800

    [midasexample] Update multiclock test to add full-rate synchronizers on I/O

--- sim/src/main/cc/midasexamples/TrivialMulticlock.h
+++ sim/src/main/cc/midasexamples/TrivialMulticlock.h
@@ -7,20 +7,30 @@ class MultiClockChecker {
    simif_t * sim;
    uint32_t field_address;
    int numerator, denominator;
-   int cycle = 0;
+   int cycle = -1;
    uint32_t expected_value;
+   uint32_t fast_domain_reg, slow_domain_reg, fast_domain_reg_out;
 
    MultiClockChecker(simif_t * sim, uint32_t field_address, int numerator, int denominator):
      sim(sim), field_address(field_address), numerator(numerator), denominator(denominator) {};
    void expect_and_update(uint64_t poked_value){
-      if (cycle == 0) {
-          expected_value = poked_value;
+      if (cycle > 1 ) sim->expect(field_address, fast_domain_reg_out);
+      if (cycle < 1) {
+        fast_domain_reg_out = slow_domain_reg;
+        slow_domain_reg = fast_domain_reg;
       } else {
-        sim->expect(field_address, expected_value);
-        if ((cycle * numerator) / denominator > ((cycle - 1) * numerator)/ denominator) {
-          expected_value = poked_value;
+        fast_domain_reg_out =  slow_domain_reg;
+        if (((cycle * numerator) / denominator) > (((cycle - 1) * numerator)/ denominator)) {
+          // TODO: Handle the case where numerator * cycle is not a multiple of the division
+          //if (((cycle * numerator) % denominator) != 0) {
+          //  fast_domain_reg_out = slow_domain_reg;
+          //  slow_domain_reg = poked_value;
+          //} else {
+          slow_domain_reg = fast_domain_reg;
+          //}
         }
       }
+      fast_domain_reg = poked_value;
       cycle++;
    };
 };
@@ -34,15 +44,13 @@ public:
     std::vector<MultiClockChecker*> checkers;
     checkers.push_back(new MultiClockChecker(this, halfOut, 1, 2));
     checkers.push_back(new MultiClockChecker(this, thirdOut, 1, 3));
+    // Resolve bug in PeekPoke Bridge
     //checkers.push_back(new MultiClockChecker(this, threeSeventhsOut, 3, 7));
 
     uint32_t current = rand_next(limit);
-    poke(in, current);
-    current = rand_next(limit);
-    step(1);
     for(int i = 1; i < 1024; i++){
-      for(auto checker: checkers) checker->expect_and_update(current);
-      poke(in, current);
+      for(auto checker: checkers) checker->expect_and_update(i);
+      poke(in, i);
       current = rand_next(limit);
       step(1);
     }
--- sim/src/main/scala/midasexamples/TrivialMulticlock.scala
+++ sim/src/main/scala/midasexamples/TrivialMulticlock.scala
@@ -3,7 +3,7 @@
 package firesim.midasexamples
 
 import chisel3._
-import chisel3.experimental.{withClock, RawModule, MultiIOModule}
+import chisel3.experimental.{RawModule, MultiIOModule}
 
 import midas.widgets.{RationalClockBridge, PeekPokeBridge}
 
@@ -11,30 +11,43 @@ class RegisterModule extends MultiIOModule {
   def dataType = UInt(32.W)
   val in = IO(Input(dataType))
   val out = IO(Output(dataType))
-  out := RegNext(in)
+  val slowClock = IO(Input(Clock()))
+  // Register the input and output in the fast domain so that the PeekPoke
+  // bridge is synchronous with the design. The clock crossing is contained in this module
+  val regIn  = RegNext(in)
+  val regOut = Reg(in.cloneType)
+  out := regOut
+  withClock(slowClock) {
+    regOut := RegNext(regIn)
+  }
 }
 
 class TrivialMulticlock extends RawModule {
+  // TODO: Resolve bug in PeekPoke bridge for 3/7 case
   //val clockBridge = Module(new RationalClockBridge(1000, (1,2), (1,3), (3,7)))
   //val List(fullRate, halfRate, thirdRate, threeSeventhsRate) = clockBridge.io.clocks.toList
   val clockBridge = Module(new RationalClockBridge(1000, (1,2), (1,3)))
   val List(fullRate, halfRate, thirdRate) = clockBridge.io.clocks.toList
-  val reset = Wire(Bool())
+  val reset = WireInit(false.B)
 
   withClockAndReset(fullRate, reset) {
     val halfRateInst = Module(new RegisterModule)
-    halfRateInst.clock := halfRate
+    halfRateInst.slowClock := halfRate
     val thirdRateInst = Module(new RegisterModule)
-    thirdRateInst.clock := thirdRate
+    thirdRateInst.slowClock := thirdRate
     thirdRateInst.in := halfRateInst.in
-    //val threeSeventhsRateInst = Module(new RegisterModule)
-    //threeSeventhsRateInst.clock := threeSeventhsRate
-    //threeSeventhsRateInst.in := halfRateInst.in
+    val threeSeventhsRateInst = Module(new RegisterModule)
+    // TODO: See above
+    //threeSeventhsRateInst.slowClock := threeSeventhsRate
+    threeSeventhsRateInst.slowClock := fullRate
+    threeSeventhsRateInst.in := halfRateInst.in
 
-    val peekPokeBridge = PeekPokeBridge(fullRate, reset, ("in", halfRateInst.in),
-                                                         ("halfOut", halfRateInst.out),
-                                                         ("thirdOut", thirdRateInst.out))
-                                                         //("threeSeventhsOut", threeSeventhsRateInst.out))
+    // TODO: Remove reset
+    val peekPokeBridge = PeekPokeBridge(fullRate,
+                                        reset,
+                                        ("in", halfRateInst.in),
+                                        ("halfOut",halfRateInst.out),
+                                        ("thirdOut", thirdRateInst.out),
+                                        ("threeSeventhsOut", threeSeventhsRateInst.out))
   }
 }
-
