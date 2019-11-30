f178386e5f0d2d9d204d463b7bc745df4c6dc0ad
David Biancolin <david.biancolin@gmail.com>
2019-11-29 18:24:51 -0800

    [WIP] Bridge Walkthrough

--- sim/firesim-lib/src/main/scala/bridges/UARTBridge.scala
+++ sim/firesim-lib/src/main/scala/bridges/UARTBridge.scala
@@ -10,21 +10,53 @@ import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.subsystem.PeripheryBusKey
 import sifive.blocks.devices.uart.{UARTPortIO, PeripheryUARTKey}
 
-// We need to box this Int in a case class for the constructor invocation to
-// work correctly.
-case class UARTKey(div: Int)
+//Note: This file is heavily commented as it serves as a bridge walkthrough
+//example in the FireSim docs
 
+// DOC include start: UART Bridge Target-Side Interface
+class UARTBridgeTargetIO extends Bundle {
+  val uart = Flipped(new UARTPortIO)
+  // Note this reset is optional and used only to reset target-state modelled
+  // in the bridge This reset just like any other Bool included in your target
+  // interface, simply appears as another Bool in the input token.
+  val reset = Input(Bool())
+}
+// DOC include end: UART Bridge Target-Side Interface
+
+// DOC include start: UART Bridge Constructor Arg
+// Out bridge module constructor argument. This captures all of the extra
+// metadata we'd like to pass to the host-side BridgeModule. Note, we need to
+// use a single case-class to do so, even if it is simply to wrap a primitive
+// type, as is the case for UART (int)
+case class UARTKey(div: Int)
+// DOC include end: UART Bridge Constructor Arg
+
+// DOC include start: UART Bridge Target-Side Module
 class UARTBridge(implicit p: Parameters) extends BlackBox
     with Bridge[HostPortIO[UARTBridgeTargetIO], UARTBridgeModule] {
+  // Since we're extending BlackBox this is the port will connect to in our target's RTL
   val io = IO(new UARTBridgeTargetIO)
+  // Implement the bridgeIO member of Bridge using HostPort This indicates that
+  // we want to divide io, into a bidirectional token stream with the input
+  // token corresponding to all of the inputs of this BlackBox, and the output token consisting of 
+  // all of the outputs from the BlackBox
   val bridgeIO = HostPort(io)
+
+  // Do some intermediate work to compute our host-side BridgeModule's constructor argument
   val frequency = p(PeripheryBusKey).frequency
   val baudrate = 3686400L
   val div = (p(PeripheryBusKey).frequency / baudrate).toInt
+
+  // And then implement the constructorArg member
   val constructorArg = Some(UARTKey(div))
+
+  // Finally, and this is critical, emit the Bridge Annotations -- without
+  // this, this BlackBox would appear like any other BlackBox to golden-gate
   generateAnnotations()
 }
+// DOC include end: UART Bridge Target-Side Module
 
+// DOC include start: UART Bridge Companion Object
 object UARTBridge {
   def apply(uart: UARTPortIO)(implicit p: Parameters): UARTBridge = {
     val ep = Module(new UARTBridge)
@@ -32,30 +64,46 @@ object UARTBridge {
     ep
   }
 }
+// DOC include end: UART Bridge Companion Object
 
-class UARTBridgeTargetIO extends Bundle {
-  val uart = Flipped(new UARTPortIO)
-  val reset = Input(Bool())
-}
-
-
+// DOC include start: UART Bridge Header
+// Our UARTBridgeModule definition, note:
+// 1) it takes one parameter, key, of type UARTKey --> the same case class we captured from the target-side
+// 2) It accepts one implicit parameter of type Parameters
+// 3) It extends BridgeModule passing the type of the HostInterface
+//
+// While the scala-type system will check if you parameterized BridgeModule
+// correctly, the types of the constructor arugument (in this case UARTKey),
+// don't match, you'll only find out later when Golden Gate attempts to generate your module.
 class UARTBridgeModule(key: UARTKey)(implicit p: Parameters) extends BridgeModule[HostPortIO[UARTBridgeTargetIO]]()(p) {
   val div = key.div
+  // This creates the interfaces for all of the host-side transport
+  // AXI4-lite for the simulation control bus, =
+  // AXI4 for DMA
   val io = IO(new WidgetIO())
+
+  // This creates the host-side interface of your TargetIO
   val hPort = IO(HostPort(new UARTBridgeTargetIO))
 
+  // Generate some FIFOs to capture tokens...
   val txfifo = Module(new Queue(UInt(8.W), 128))
   val rxfifo = Module(new Queue(UInt(8.W), 128))
 
   val target = hPort.hBits.uart
-  val fire = hPort.toHost.hValid && hPort.fromHost.hReady && txfifo.io.enq.ready
+  // In general, your BridgeModule will not need to do work every host-cycle. In simple Bridges,
+  // we can do everything in a single host-cycle -- fire captures all of the
+  // conditions under which we can consume and input token and produce a new
+  // output token
+  val fire = hPort.toHost.hValid && // We have a valid input token: toHost ~= leaving the transformed RTL
+             hPort.fromHost.hReady && // We have space to enqueue a new output token
+             txfifo.io.enq.ready      // We have space to capture new TX data
   val targetReset = fire & hPort.hBits.reset
   rxfifo.reset := reset.toBool || targetReset
   txfifo.reset := reset.toBool || targetReset
 
   hPort.toHost.hReady := fire
   hPort.fromHost.hValid := fire
-
+  // DOC include end: UART Bridge Header
   val sTxIdle :: sTxWait :: sTxData :: sTxBreak :: Nil = Enum(UInt(), 4)
   val txState = RegInit(sTxIdle)
   val txData = Reg(UInt(8.W))
@@ -127,13 +175,24 @@ class UARTBridgeModule(key: UARTKey)(implicit p: Parameters) extends BridgeModul
   }
   rxfifo.io.deq.ready := (rxState === sRxData) && rxDataWrap && rxBaudWrap && fire
 
+  // DOC include start: UART Bridge Footer
+  // Exposed the head of the queue and the valid bit as a read-only registers
+  // with name "out_bits" and out_valid respectively
   genROReg(txfifo.io.deq.bits, "out_bits")
   genROReg(txfifo.io.deq.valid, "out_valid")
+
+  // Generate a writeable register, "out_ready", that when written to dequeues
+  // a single element in the tx_fifo. Pulsify derives the register back to false
+  // after pulseLength cycles to prevent multiple dequeues
   Pulsify(genWORegInit(txfifo.io.deq.ready, "out_ready", false.B), pulseLength = 1)
 
+  // Generate regisers for the rx-side of the UART; this is eseentially the reverse of the above
   genWOReg(rxfifo.io.enq.bits, "in_bits")
   Pulsify(genWORegInit(rxfifo.io.enq.valid, "in_valid", false.B), pulseLength = 1)
   genROReg(rxfifo.io.enq.ready, "in_ready")
 
+  // This method invocation is required to wire up all of the MMIO registers to
+  // the simulation control bus (AXI4-lite)
   genCRFile()
+  // DOC include end: UART Bridge Footer
 }
