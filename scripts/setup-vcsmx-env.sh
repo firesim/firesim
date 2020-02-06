72ba0adc0e9d748da39349cc7a4a8d973062253c
David Biancolin <david.biancolin@gmail.com>
2020-02-05 17:42:43 -0800

    Fix FL sim; upgrade vivado and vcsmx versions for UCB use

--- scripts/setup-vcsmx-env.sh
+++ scripts/setup-vcsmx-env.sh
@@ -6,12 +6,12 @@
 firesim_root=$(pwd)/../
 cwd=$(pwd)
 
-source /ecad/tools/xilinx/Vivado/2017.4/settings64.sh > /dev/null
+source /ecad/tools/xilinx/Vivado/2018.2/settings64.sh > /dev/null
 # ^ mangles your path; we have to put the native toolchain back in front
 export PATH=/usr/bin:$PATH
 # Need VCSMX for mixed language support. Warning: VCSMX does not work with our
 # MIDAS-level RTL simulation flow
-export VCS_HOME=/ecad/tools/synopsys/vcsmx/current
+export VCS_HOME=/ecad/tools/synopsys/vcs-mx/O-2018.09-SP1
 export PATH=$VCS_HOME/bin:$PATH
 
 cd $firesim_root/platforms/f1/aws-fpga/ && source hdk_setup.sh
