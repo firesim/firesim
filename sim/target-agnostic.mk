9b240cdb280ac635c0692c93cfd2d116c197f028
David Biancolin <david.biancolin@gmail.com>
2020-04-29 12:46:48 -0700

    Factor out some F1-specific code into its own Make fragment

--- sim/target-agnostic.mk
+++ sim/target-agnostic.mk
@@ -155,77 +155,6 @@ $(f1): $(HEADER) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h) $(runtime_conf)
 	GEN_DIR=$(OUTPUT_DIR)/build OUT_DIR=$(OUTPUT_DIR) DRIVER="$(DRIVER_CC)" \
 	TOP_DIR=$(chipyard_dir)
 
-#############################
-# FPGA Build Initialization #
-#############################
-board_dir 	   := $(fpga_dir)/hdk/cl/developer_designs
-fpga_work_dir  := $(board_dir)/cl_$(name_tuple)
-build_dir      := $(fpga_work_dir)/build
-verif_dir      := $(fpga_work_dir)/verif
-fpga_v         := $(fpga_work_dir)/design/cl_firesim_generated.sv
-ila_work_dir   := $(fpga_work_dir)/design/ila_files/
-fpga_vh        := $(fpga_work_dir)/design/cl_firesim_generated_defines.vh
-fpga_tcl_env   := $(fpga_work_dir)/design/cl_firesim_generated_env.tcl
-repo_state     := $(fpga_work_dir)/design/repo_state
-
-$(fpga_work_dir)/stamp: $(shell find $(board_dir)/cl_firesim -name '*')
-	mkdir -p $(@D)
-	cp -rf $(board_dir)/cl_firesim -T $(fpga_work_dir)
-	touch $@
-
-$(fpga_v): $(VERILOG) $(fpga_work_dir)/stamp
-	$(firesim_base_dir)/../scripts/repo_state_summary.sh > $(repo_state)
-	cp -f $< $@
-	sed -i "s/\$$random/64'b0/g" $@
-	sed -i "s/\(^ *\)fatal;\( *$$\)/\1fatal(0, \"\");\2/g" $@
-
-$(fpga_vh): $(VERILOG) $(fpga_work_dir)/stamp
-	cp -f $(GENERATED_DIR)/$(@F) $@
-
-$(fpga_tcl_env): $(VERILOG) $(fpga_work_dir)/stamp
-	cp -f $(GENERATED_DIR)/$(@F) $@
-
-.PHONY: $(ila_work_dir)
-$(ila_work_dir): $(verilog) $(fpga_work_dir)/stamp
-	cp -f $(GENERATED_DIR)/firesim_ila_insert_* $(fpga_work_dir)/design/ila_files/
-	sed -i "s/\$$random/64'b0/g" $(fpga_work_dir)/design/ila_files/*
-	sed -i "s/\(^ *\)fatal;\( *$$\)/\1fatal(0, \"\");\2/g" $(fpga_work_dir)/design/ila_files/*
-
-# Goes as far as setting up the build directory without running the cad job
-# Used by the manager before passing a build to a remote machine
-replace-rtl: $(fpga_v) $(ila_work_dir) $(fpga_vh) $(fpga_tcl_env)
-
-.PHONY: replace-rtl
-
-$(firesim_base_dir)/scripts/checkpoints/$(target_sim_tuple): $(fpga_work_dir)/stamp
-	mkdir -p $(@D)
-	ln -sf $(build_dir)/checkpoints/to_aws $@
-
-# Runs a local fpga-bitstream build. Strongly consider using the manager instead.
-fpga: export CL_DIR := $(fpga_work_dir)
-fpga: $(fpga_v) $(base_dir)/scripts/checkpoints/$(target_sim_tuple)
-	cd $(build_dir)/scripts && ./aws_build_dcp_from_cl.sh -notify
-
-
-#############################
-# FPGA-level RTL Simulation #
-#############################
-
-# Run XSIM DUT
-xsim-dut: replace-rtl $(fpga_work_dir)/stamp
-	cd $(verif_dir)/scripts && $(MAKE) C_TEST=test_firesim
-
-# Compile XSIM Driver #
-xsim = $(GENERATED_DIR)/$(DESIGN)-$(PLATFORM)
-
-$(xsim): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) -D SIMULATION_XSIM -D NO_MAIN
-$(xsim): export LDFLAGS := $(LDFLAGS) $(common_ld_flags)
-$(xsim): $(HEADER) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
-	$(MAKE) -C $(simif_dir) f1 PLATFORM=f1 DESIGN=$(DESIGN) \
-	GEN_DIR=$(GENERATED_DIR) OUT_DIR=$(GENERATED_DIR) DRIVER="$(DRIVER_CC)" \
-	TOP_DIR=$(chipyard_dir)
-
-xsim: $(xsim)
 
 #########################
 # MIDAS Unit Tests      #
@@ -276,8 +205,7 @@ tags: $(HEADER) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 .PHONY: verilator verilator-debug
 .PHONY: vcs vcs-debug
 .PHONY: run
-.PHONY: xsim-dut xsim run-xsim
-.PHONY: $(PLATFORM)-driver fpga
+.PHONY: $(PLATFORM)-driver
 .PHONY: mostlyclean clean
 
 .PRECIOUS: $(OUTPUT_DIR)/%.vpd $(OUTPUT_DIR)/%.out $(OUTPUT_DIR)/%.run
