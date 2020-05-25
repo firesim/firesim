aade767aa146afc1afa333a8436a24bbb5b20bb6
Albert Magyar <albert.magyar@gmail.com>
2020-05-25 15:34:10 +0000

    16-core BOOM going to FPGA but locking up

--- sim/midas/src/main/scala/midas/stage/GoldenGateCli.scala
+++ sim/midas/src/main/scala/midas/stage/GoldenGateCli.scala
@@ -8,6 +8,7 @@ trait GoldenGateCli { this: Shell =>
   parser.note("Golden Gate Compiler Options")
   Seq(ConfigPackageAnnotation,
       ConfigStringAnnotation,
+      firrtl.checks.SkipChecks,
       firrtl.stage.FirrtlFileAnnotation,
       firrtl.stage.OutputFileAnnotation,
       firrtl.stage.FirrtlSourceAnnotation,
