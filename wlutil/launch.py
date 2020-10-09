68fe90d37b0009ac60d7d21b6449fdc7434996db
Nathan Pemberton <nathanp@berkeley.edu>
2020-10-09 14:35:30 -0400

    Add support for launching multiple jobs (in sequence)

--- wlutil/launch.py
+++ wlutil/launch.py
@@ -67,12 +67,13 @@ def getQemuCmd(config, nodisk=False):
 
     return " ".join(cmd) + " " + config.get('qemu-args', '')
 
-def launchWorkload(baseConfig, job='all', spike=False, interactive=True):
+def launchWorkload(baseConfig, jobs=None, spike=False, interactive=True):
     """Launches the specified workload in functional simulation.
 
     cfgName: unique name of the workload in the cfgs
     cfgs: initialized configuration (contains all possible workloads)
-    job: Which job to launch. 'all' launches the parent of all the jobs (i.e. the base workload).
+    jobs: List of job names to launch. If jobs is None we use the parent of
+    all the jobs (i.e.  the top-level workload in the config).
     spike: Use spike instead of the default qemu as the functional simulator
     interactive: If true, the output from the simulator will be displayed to
         stdout. If false, only the uartlog will be written (it is written live and
@@ -86,53 +87,48 @@ def launchWorkload(baseConfig, job='all', spike=False, interactive=True):
     if baseConfig.get('distro') == 'bare' and spike != True:
         raise RuntimeError("Bare-metal workloads do not currently support Qemu. Please run this workload under spike.")
 
-    if 'jobs' in baseConfig.keys() and job != 'all':
-        # Run the specified job
-        config = baseConfig['jobs'][job]
+    if jobs == None:
+        configs = [ baseConfig ]
     else:
-        # Run the base image
-        config = baseConfig
- 
-    if config['launch']:
-        baseResDir = getOpt('res-dir') / getOpt('run-name')
-        runResDir = baseResDir / config['name']
-        uartLog = runResDir / "uartlog"
-        os.makedirs(runResDir)
+        configs = [ baseConfig['jobs'][j] for j in jobs ]
 
-        if spike:
-            if 'img' in config and not config['nodisk']:
-                sys.exit("Spike currently does not support disk-based " +
-                        "configurations. Please use an initramfs based image.")
-            cmd = getSpikeCmd(config, config['nodisk'])
-        else:
-            cmd = getQemuCmd(config, config['nodisk'])
+    baseResDir = getOpt('res-dir') / getOpt('run-name')
+    for config in configs:
+        if config['launch']:
+            runResDir = baseResDir / config['name']
+            uartLog = runResDir / "uartlog"
+            os.makedirs(runResDir)
 
-        log.info("Running: " + "".join(cmd))
-        if not interactive:
-            log.info("For live output see: " + str(uartLog))
-        with open(uartLog, 'wb', buffering=0) as uartF:
-            with sp.Popen(cmd.split(), stderr=sp.STDOUT, stdout=sp.PIPE) as p:
-                    for c in iter(lambda: p.stdout.read(1), b''):
-                        if interactive:
-                            sys.stdout.buffer.write(c)
-                            sys.stdout.flush()
-                        uartF.write(c)
+            if spike:
+                if 'img' in config and not config['nodisk']:
+                    sys.exit("Spike currently does not support disk-based " +
+                            "configurations. Please use an initramfs based image.")
+                cmd = getSpikeCmd(config, config['nodisk'])
+            else:
+                cmd = getQemuCmd(config, config['nodisk'])
 
-        if 'outputs' in config:
-            outputSpec = [ FileSpec(src=f, dst=runResDir) for f in config['outputs']] 
-            copyImgFiles(config['img'], outputSpec, direction='out')
+            log.info("Running: " + "".join(cmd))
+            if not interactive:
+                log.info("For live output see: " + str(uartLog))
+            with open(uartLog, 'wb', buffering=0) as uartF:
+                with sp.Popen(cmd.split(), stderr=sp.STDOUT, stdout=sp.PIPE) as p:
+                        for c in iter(lambda: p.stdout.read(1), b''):
+                            if interactive:
+                                sys.stdout.buffer.write(c)
+                                sys.stdout.flush()
+                            uartF.write(c)
 
-        if 'post_run_hook' in config:
-            prhCmd = [config['post_run_hook'].path] + config['post_run_hook'].args + [baseResDir]
-            log.info("Running post_run_hook script: " + ' '.join([ str(x) for x in prhCmd]))
-            try:
-                run(prhCmd, cwd=config['workdir'])
-            except sp.CalledProcessError as e:
-                log.info("\nRun output available in: " + str(runResDir.parent))
-                raise RuntimeError("Post run hook failed:\n" + e.output)
+            if 'outputs' in config:
+                outputSpec = [ FileSpec(src=f, dst=runResDir) for f in config['outputs']]
+                copyImgFiles(config['img'], outputSpec, direction='out')
 
-        return runResDir
-    else:
-        log.info("Workload launch skipped ('launch'=false in config)")
-        return None
+    if 'post_run_hook' in baseConfig:
+        prhCmd = [baseConfig['post_run_hook'].path] + baseConfig['post_run_hook'].args + [baseResDir]
+        log.info("Running post_run_hook script: " + ' '.join([ str(x) for x in prhCmd]))
+        try:
+            run(prhCmd, cwd=config['workdir'])
+        except sp.CalledProcessError as e:
+            log.info("\nRun output available in: " + str(baseResDir))
+            raise RuntimeError("Post run hook failed:\n" + e.output)
 
+    return baseResDir
