f623ce49f92521d67e352181779a8ed11bed5bdf
Nathan Pemberton <nathanp@berkeley.edu>
2020-10-27 20:22:52 -0400

    Add test for undefined workload config option warnings (also the test for --werr).

---
+++ test/undefinedOpt/test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Call as ./test.py PATH/TO/MARSHAL"""
+
+import subprocess as sp
+import sys
+import os
+import pathlib as pth
+import re
+
+# Should be the directory containing the test
+testSrc = pth.Path(__file__).parent.resolve()
+testCfg = testSrc.parent / "undefinedOpt.json"
+
+if len(sys.argv) > 1:
+    managerPath = pth.Path(sys.argv[1])
+else:
+    # Should be the directory containing marshal
+    managerPath = pth.Path(os.getcwd()) / "marshal"
+    if not managerPath.exists:
+        managerPath = pth.Path(os.getcwd()) / "../../marshal"
+        if not managerPath.exists:
+            print("Can't find marshal, this script should be called either from firesim-software/ or firesim-software/test/incremental/", file=sys.stderr)
+            sys.exit(1)
+
+print("Cleaning the test the first time:")
+proc = sp.run([managerPath, "clean", str(testCfg)])
+if proc.returncode != 0:
+    print("Failure when cleaning test", file=sys.stderr)
+    sys.exit(1)
+
+print("Running with --werr, should fail:")
+proc = sp.run([managerPath, "--werr", "test", str(testCfg)])
+if proc.returncode == 0:
+    print("Test succeeded when it should have failed", file=sys.stderr)
+    sys.exit(1)
+
+print("Running without --werr should pass with a warning:")
+proc = sp.run([managerPath, "test", str(testCfg)], stdout=sp.PIPE)
+if proc.returncode != 0:
+    print("Test failed", file=sys.stderr)
+    sys.exit(1)
+elif "WARNING" not in proc.stdout.decode("utf-8"):
+    print("No warning present!")
+    sys.exit(1)
+
+print("Test Success", file=sys.stderr)
+sys.exit()
