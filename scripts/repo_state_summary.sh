fbc95331300390d4f4435fc01d6930421b4ceb72
Tim Snyder <timothy.snyder@sifive.com>
2020-12-15 10:56:14 -0600

    repo_state_summary introspects toplevel git repo and cd's there (#682)

--- scripts/repo_state_summary.sh
+++ scripts/repo_state_summary.sh
@@ -2,6 +2,17 @@
 # This script provides a quick and dirty means to capture the state of the
 # firesim repo and its submodules, when launching a build so that it can be
 # recreated manually later.
+
+set -e
+
+# if not empty, then you are in a submodule and this is the absolute path
+# to the repo above you, iteratively cd to the topmost repo
+super_root="$(git rev-parse --show-superproject-working-tree)"
+while [[ -n  "$super_root" ]]; do
+    cd "$super_root"
+    super_root="$(git rev-parse --show-superproject-working-tree)"
+done
+
 git --no-pager log -n 1
 
 if [[ -n $(git status -s)  ]]; then
