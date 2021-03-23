e00dcfa2776431b18a4b3ca30c71d9f5bf0053ae
Jiuyang Liu <liu@jiuyang.me>
2021-03-23 07:09:50 +0000

    [Driver] fix driver.

--- sim/midas/src/main/scala/midas/passes/fame/EmitAndWrapRAMModels.scala
+++ sim/midas/src/main/scala/midas/passes/fame/EmitAndWrapRAMModels.scala
@@ -192,9 +192,8 @@ class RAMModelInst(name: String, val readPorts: Seq[ReadPort], val writePorts: S
   }
 
   def elaborateModel(parentCircuitNS: Namespace): Module = {
-    val c3circuit = chisel3.Driver.elaborate(() =>
-      new midas.models.sram.AsyncMemChiselModel(depth.toInt, dataWidth, readPorts.size, writePorts.size))
-    val chirrtl = Parser.parse(chisel3.Driver.emit(c3circuit))
+    val c3circuit = chisel3.stage.ChiselStage.elaborate(new midas.models.sram.AsyncMemChiselModel(depth.toInt, dataWidth, readPorts.size, writePorts.size))
+    val chirrtl = Parser.parse((new chisel3.stage.ChiselStage).emitChirrtl(new midas.models.sram.AsyncMemChiselModel(depth.toInt, dataWidth, readPorts.size, writePorts.size)))
     val state = new MiddleFirrtlCompiler().compile(CircuitState(chirrtl, ChirrtlForm, Nil), Nil)
     require(state.circuit.modules.length == 1)
     state.circuit.modules.collectFirst({
