bf70f8992a2453c88822a2fa5ced48889d945a98
Jiuyang Liu <liu@jiuyang.me>
2021-04-06 02:06:45 +0000

    refactor GoldenGateStage with DependencyAPI.

--- sim/midas/src/main/scala/midas/stage/GoldenGateStage.scala
+++ sim/midas/src/main/scala/midas/stage/GoldenGateStage.scala
@@ -3,30 +3,41 @@
 package midas.stage
 
 import firrtl.AnnotationSeq
-import firrtl.options.{Phase, PhaseManager, PreservesAll, Shell, Stage, StageMain}
+import firrtl.options.{Dependency, Phase, PhaseManager, PreservesAll, Shell, Stage, StageMain}
 import firrtl.options.phases.DeletedWrapper
 import firrtl.options.Viewer.view
+import firrtl.stage.phases.CatchExceptions
 
-import java.io.{StringWriter, PrintWriter}
+import java.io.{PrintWriter, StringWriter}
 
-class GoldenGateStage extends Stage with PreservesAll[Phase] {
+
+class GoldenGatePhase
+    extends PhaseManager(
+      targets = Seq(
+        Dependency[midas.stage.GoldenGateCompilerPhase],
+        Dependency[firrtl.stage.phases.WriteEmitted]
+      )
+    ) {
+
+  override def invalidates(a: Phase) = false
+
+  override val wrappers = Seq(CatchExceptions(_: Phase), DeletedWrapper(_: Phase))
+}
+
+class GoldenGateStage extends Stage {
+  lazy val phase = new GoldenGatePhase
+
+  override def prerequisites = phase.prerequisites
+
+  override def optionalPrerequisites = phase.optionalPrerequisites
+
+  override def optionalPrerequisiteOf = phase.optionalPrerequisiteOf
+
+  override def invalidates(a: Phase): Boolean = phase.invalidates(a)
+
   val shell: Shell = new Shell("goldengate") with GoldenGateCli
 
-  private val phases: Seq[Phase] =
-    Seq(
-        new firrtl.options.phases.GetIncludes,
-        new midas.stage.phases.CreateParametersInstancePhase,
-        new firrtl.stage.phases.AddDefaults,
-        new firrtl.stage.phases.AddImplicitEmitter,
-        new firrtl.stage.phases.Checks,
-        new firrtl.stage.phases.AddCircuit,
-        new firrtl.stage.phases.AddImplicitOutputFile,
-        new midas.stage.GoldenGateCompilerPhase,
-        new firrtl.stage.phases.WriteEmitted )
-      .map(DeletedWrapper(_))
-
-
-  def run(annotations: AnnotationSeq): AnnotationSeq = phases.foldLeft(annotations)((a, f) => f.transform(a))
+  def run(annotations: AnnotationSeq): AnnotationSeq = phase.transform(annotations)
 }
 
 object GoldenGateMain extends StageMain(new GoldenGateStage)
