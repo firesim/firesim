8395744cc109c5b64b8d092702cdfe9dbc9c0464
David Biancolin <david.biancolin@gmail.com>
2021-05-08 19:54:29 +0000

    [ci] Remove AWS creds registration, andt push  use conftest to provide a region

--- deploy/tests/conftest.py
+++ deploy/tests/conftest.py
@@ -20,3 +20,6 @@ def aws_test_credentials():
     os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
     os.environ['AWS_SECURITY_TOKEN'] = 'testing'
     os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    # Provide a default region, since one may not already be configured (true of CI)
+    # This was an arbitrary selection
+    os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
