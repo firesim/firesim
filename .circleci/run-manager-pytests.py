d1c2097ffbfa00f549fdfb415de7274a47ec9ce4
David Biancolin <david.biancolin@gmail.com>
2021-05-25 19:32:41 -0400

    Serialize sbt tests

--- .circleci/run-manager-pytests.py
+++ .circleci/run-manager-pytests.py
@@ -5,7 +5,7 @@ from fabric.api import *
 from common import manager_fsim_dir, manager_hostname
 from ci_variables import ci_workflow_id
 
-def run_manager_pytests(target_project, command):
+def run_manager_pytests():
     """ Runs all manager pytests """
 
     with cd(manager_fsim_dir), prefix('source env.sh'):
