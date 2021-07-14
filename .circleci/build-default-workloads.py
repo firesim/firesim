54b87e725a99e8c2bdb6d1be00d6e205c12cd419
David Biancolin <david.biancolin@gmail.com>
2021-07-14 16:11:58 -0700

    fixup! [ci] Build the linux poweroff workload

--- .circleci/build-default-workloads.py
+++ .circleci/build-default-workloads.py
@@ -8,13 +8,16 @@ from ci_variables import ci_workflow_id
 def build_default_workloads():
     """ Builds workloads that will be run on F1 instances as part of CI """
 
-    with cd(manager_fsim_dir), prefix('source env.sh'):
-        with cd("target-design/chipyard/software/firemarshal"):
-            run("./init-submodules.sh")
-            run("./marshal -v build br-base.json")
+    with cd(manager_fsim_dir), \
+         prefix('source env.sh'), \
+         cd("target-design/chipyard/software/firemarshal"):
+        run("./init-submodules.sh")
+        run("./marshal -v build br-base.json")
 
-        with cd("deploy/workloads"):
-            run("make linux-poweroff")
+    with cd(manager_fsim_dir), \
+         prefix('source env.sh'), \
+         cd("deploy/workloads"):
+        run("make linux-poweroff")
 
 
 if __name__ == "__main__":
