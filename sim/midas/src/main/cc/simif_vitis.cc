d2b863665f07491335ab9bef654d365f7eac034e
abejgonzalez <abe.j.gonza@gmail.com>
2021-10-05 16:45:28 -0700

    Add vitis support in the driver

---
+++ sim/midas/src/main/cc/simif_vitis.cc
@@ -0,0 +1,77 @@
+#include "simif_vitis.h"
+#include <cassert>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+simif_vitis_t::simif_vitis_t(int argc, char** argv) {
+    device_index = -1;
+    binary_file = "";
+
+    // TODO: Properly read out arguments
+    std::vector<std::string> args(argv + 1, argv + argc);
+    for (auto &arg: args) {
+        if (arg.find("+device_index=") == 0) {
+            device_index = 0;//atoi((arg.c_str()) + 8);
+        }
+        if (arg.find("+binary_file=") == 0) {
+            binary_file = "kernel.xclbin"; //atoi((arg.c_str()) + 8);
+        }
+    }
+
+    if (device_index == -1) {
+        fprintf(stderr, "Device ID not specified. Assuming Device 0.\n");
+        device_index = 0;
+    }
+
+    if (binary_file == "") {
+        fprintf(stderr, "No binary file specified.\n");
+        exit(1);
+    }
+
+    // Open the FPGA device
+    device_handle = xrt::device(device_index);
+
+    // Load the XCLBIN (get handle, then load)
+    uuid = device_handle.load_xclbin(binary_file);
+
+    // Open Kernel
+    kernel_handle = xrt::ip(device_handle, uuid, "firesim");
+}
+
+simif_vitis_t::~simif_vitis_t() {
+    fprintf(stdout, "Graceful shutdown\n");
+}
+
+void simif_vitis_t::write(size_t addr, uint32_t data) {
+    // addr is really a (32-byte) word address because of zynq implementation
+    addr <<= CTRL_AXI4_SIZE;
+    kernel_handle.write_register(addr, data);
+}
+
+uint32_t simif_vitis_t::read(size_t addr) {
+    // Convert the word address into a byte-address
+    addr <<= CTRL_AXI4_SIZE;
+    uint32_t value;
+    value = kernel_handle.read_register(addr);
+    return value & 0xFFFFFFFF;
+}
+
+// TODO: Not implemented
+ssize_t simif_vitis_t::pull(size_t addr, char* data, size_t size) {
+  return -1; //::pread(edma_read_fd, data, size, addr);
+}
+
+// TODO: Not implemented
+ssize_t simif_vitis_t::push(size_t addr, char* data, size_t size) {
+  return -1; //::pwrite(edma_write_fd, data, size, addr);
+}
+
+uint32_t simif_vitis_t::is_write_ready() {
+    uint64_t addr = 0x4;
+    uint32_t value;
+    value = kernel_handle.read_register(addr);
+    return value & 0xFFFFFFFF;
+}
