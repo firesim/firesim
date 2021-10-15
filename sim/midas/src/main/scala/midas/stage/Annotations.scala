d93c5a1112fa1700a60fbb5b1675da181b71400f
David Biancolin <david.biancolin@gmail.com>
2021-10-15 18:49:10 +0000

    [stage] Have runtime config generator use the checks phase

--- sim/midas/src/main/scala/midas/stage/Annotations.scala
+++ sim/midas/src/main/scala/midas/stage/Annotations.scala
@@ -36,14 +36,12 @@ object ConfigStringAnnotation extends HasShellOptions {
 
 // Used to specify the name of the desired runtime configuration
 // file.  Will be emitted in the TargetDir
-case class RuntimeConfigNameAnnotation(configString: String) extends NoTargetAnnotation with GoldenGateOption
-
 object RuntimeConfigNameAnnotation extends HasShellOptions {
 
   val options = Seq(
     new ShellOption[String](
       longOption = "golden-gate-runtime-config-name",
-      toAnnotationSeq = (a: String) => Seq(RuntimeConfigNameAnnotation(a)),
+      toAnnotationSeq = (a: String) => Seq(OutputBaseFilenameAnnotation(a)),
       helpText = "Specifies the filename for the generated runtime configuration file.",
       shortOption = Some("ggrc"),
       helpValueName = Some("<filename>") ) )
