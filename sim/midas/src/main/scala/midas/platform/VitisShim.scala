e1110f96ef2d2518aebaf6042ff97dd02f6519bf
abejgonzalez <abe.j.gonza@gmail.com>
2021-10-22 15:24:00 -0700

    Change top-level io | Don't exit immediately

--- sim/midas/src/main/scala/midas/platform/VitisShim.scala
+++ sim/midas/src/main/scala/midas/platform/VitisShim.scala
@@ -16,9 +16,9 @@ class VitisShim(implicit p: Parameters) extends PlatformShim {
   lazy val module = new LazyModuleImp(this) {
     val io = IO(new Bundle{
       val master = Flipped(new NastiIO()(p alterPartial ({ case NastiKey => p(CtrlNastiKey) })))
-      val dma  = Flipped(new NastiIO()(p alterPartial ({ case NastiKey => p(DMANastiKey) })))
+      //val dma  = Flipped(new NastiIO()(p alterPartial ({ case NastiKey => p(DMANastiKey) })))
     })
-    val io_slave  = IO(HeterogeneousBag(top.module.mem.map(x => x.cloneType)))
+    //val io_slave  = IO(HeterogeneousBag(top.module.mem.map(x => x.cloneType)))
 
     top.module.ctrl <> io.master
     //top.module.dma  <> io.dma
@@ -31,11 +31,11 @@ class VitisShim(implicit p: Parameters) extends PlatformShim {
     top.module.dma.r.ready  := false.B
     top.module.dma.b.ready  := false.B
 
-    io.dma.ar.ready := false.B
-    io.dma.aw.ready := false.B
-    io.dma.w.ready  := false.B
-    io.dma.r.valid  := false.B
-    io.dma.b.valid  := false.B
+    //io.dma.ar.ready := false.B
+    //io.dma.aw.ready := false.B
+    //io.dma.w.ready  := false.B
+    //io.dma.r.valid  := false.B
+    //io.dma.b.valid  := false.B
 
     top.module.mem.foreach({ case bundle =>
       bundle.ar.ready := false.B
@@ -45,13 +45,13 @@ class VitisShim(implicit p: Parameters) extends PlatformShim {
       bundle.b.valid  := false.B
     })
 
-    io_slave.foreach({ case bundle =>
-      bundle.ar.valid := false.B
-      bundle.aw.valid := false.B
-      bundle.w.valid  := false.B
-      bundle.r.ready  := false.B
-      bundle.b.ready  := false.B
-    })
+    //io_slave.foreach({ case bundle =>
+    //  bundle.ar.valid := false.B
+    //  bundle.aw.valid := false.B
+    //  bundle.w.valid  := false.B
+    //  bundle.r.ready  := false.B
+    //  bundle.b.ready  := false.B
+    //})
 
     // Biancolin: It would be good to put in writing why ID is being reassigned...
     val (wCounterValue, wCounterWrap) = Counter(io.master.aw.fire(), 1 << p(CtrlNastiKey).idBits)
