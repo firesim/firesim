81ab24ee21be0763d44ef22a3cf178dbd24b7666
David Biancolin <david.biancolin@gmail.com>
2021-11-09 16:01:46 -0800

    junctions: Inline AXI4<>Nasti assigner modules

--- sim/midas/src/main/scala/junctions/Nasti2AXI4.scala
+++ sim/midas/src/main/scala/junctions/Nasti2AXI4.scala
@@ -3,11 +3,13 @@
 package junctions
 
 import chisel3._
+import chisel3.util.experimental.InlineInstance
 
 import freechips.rocketchip.amba.axi4._
 import freechips.rocketchip.config.{Parameters}
 
-class AXI42NastiIdentityModule(params: AXI4BundleParameters)(implicit p: Parameters) extends Module {
+class AXI42NastiIdentityModule(params: AXI4BundleParameters)(implicit p: Parameters)
+    extends RawModule with InlineInstance {
   val io = IO(new Bundle {
     val axi4 = Flipped(new AXI4Bundle(params))
     val nasti = new NastiIO()(p alterPartial { case NastiKey => NastiParameters(params) } )
@@ -15,7 +17,8 @@ class AXI42NastiIdentityModule(params: AXI4BundleParameters)(implicit p: Paramet
   AXI4NastiAssigner.toNasti(io.nasti, io.axi4)
 }
 
-class Nasti2AXI4IdentityModule(params: AXI4BundleParameters)(implicit p: Parameters) extends Module {
+class Nasti2AXI4IdentityModule(params: AXI4BundleParameters)(implicit p: Parameters) 
+    extends RawModule with InlineInstance {
   val io = IO(new Bundle {
     val axi4 = new AXI4Bundle(params)
     val nasti = Flipped(new NastiIO()(p alterPartial { case NastiKey => NastiParameters(params) } ))
@@ -23,7 +26,8 @@ class Nasti2AXI4IdentityModule(params: AXI4BundleParameters)(implicit p: Paramet
   AXI4NastiAssigner.toAXI4(io.axi4, io.nasti)
 }
 
-class Nasti2AXI4Monitor(params: AXI4BundleParameters)(implicit p: Parameters) extends Module {
+class Nasti2AXI4Monitor(params: AXI4BundleParameters)(implicit p: Parameters)
+    extends RawModule with InlineInstance {
   val io = IO(new Bundle {
     val axi4 = Output(new AXI4Bundle(params))
     val nasti = Input(new NastiIO()(p alterPartial { case NastiKey => NastiParameters(params) } ))
@@ -43,8 +47,7 @@ object Nasti2AXI4 {
   def toMonitor(nastiIO: NastiIO)(implicit p: Parameters): AXI4Bundle = {
     val axi4Params =  AXI4BundleParameters(nastiIO.ar.bits.addr.getWidth,
                                            nastiIO.r.bits.data.getWidth,
-                                           nastiIO.ar.bits.id  .getWidth,
-                                           Nil)
+                                           nastiIO.ar.bits.id  .getWidth)
     val conv = Module(new Nasti2AXI4Monitor(axi4Params))
     conv.io.nasti := nastiIO
     conv.io.axi4
