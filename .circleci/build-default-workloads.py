7f9f07de967f642b9733fe6f0369ee8f10592156
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-11-24 19:15:14 +0000

    Add ISCA experiments to CI build check

--- .circleci/build-default-workloads.py
+++ .circleci/build-default-workloads.py
@@ -12,6 +12,7 @@ def build_default_workloads():
          prefix('cd deploy/workloads'):
         run("marshal -v build br-base.json")
         run("make linux-poweroff")
+        run("make allpaper")
 
 if __name__ == "__main__":
     execute(build_default_workloads, hosts=[manager_hostname(ci_workflow_id)])
