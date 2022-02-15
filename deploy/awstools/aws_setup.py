3b20b55cd1fdb2e3bb67bad14809bb34e330de15
Tim Snyder <snyder.tim@gmail.com>
2022-02-15 22:55:43 +0000

    try changing scripts/aws-setup.py to a symlink so that the wget url in setup docs doesn't have to change

--- deploy/awstools/aws_setup.py
+++ deploy/awstools/aws_setup.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
 """ This script configures your AWS account to run FireSim. """
 
--- scripts/aws-setup.py
+++ scripts/aws-setup.py
@@ -1,11 +1,1 @@
-#!/usr/bin/env python3
-
-""" This script configures your AWS account to run FireSim. """
-
-# contents of the script moved into deploy/awstools to make it easier
-# to reuse for testing purposes
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "../deploy"))
-
-from awstools.aws_setup import aws_setup
-aws_setup()
+../deploy/awstools/aws_setup.py
