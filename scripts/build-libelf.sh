7a4608a7bdd4b519a2825b7b2157a0d8d672cb0f
Tim Snyder <snyder.tim@gmail.com>
2022-03-20 14:28:24 +0000

    fixup libdwarf and libelf to build with conda

--- scripts/build-libelf.sh
+++ scripts/build-libelf.sh
@@ -8,10 +8,14 @@ fi
 
 prefix=$1
 
+export CFLAGS+=" -Wno-unused-variable"
+# leave the assertions in elfutils
+export CPPFLAGS="${CPPFLAGS/-DNDEBUG/}"
+
 cd sim/firesim-lib/src/main/cc/lib/elfutils
 test -f configure || autoreconf -i -f
 ./configure --prefix="${prefix}" --enable-maintainer-mode
 make
-make install
+make install 
 # The build process modifies tracked sources. This to clean up after ourselves.
 git reset --hard
