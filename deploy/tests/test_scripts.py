68ae92db1ee9e0847657967d0f7931bd7073ca45
Tim Snyder <snyder.tim@gmail.com>
2022-03-22 05:01:38 +0000

    scripts/update_test_amis.py uses code from moto instead of copy

--- deploy/tests/test_scripts.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.xfail(strict=True, reason="cleanup update_test_amis.py to use moto.ec2.utils.gen_moto_amis")
-def test_for_update_test_amis_cleanup():
-    from moto.ec2.utils import gen_moto_amis
-    return type(gen_moto_amis)
