497d8d22d1738db7da18d148c9aaa4bac26e6701
David Biancolin <david.biancolin@gmail.com>
2022-04-23 21:20:28 -0700

    Try the new flag

--- .github/scripts/run-scala-test.py
+++ .github/scripts/run-scala-test.py
@@ -15,10 +15,14 @@ def run_scala_test(target_project, test_name):
     """
     with cd(manager_fsim_dir), prefix('source env.sh'):
         # avoid logging excessive amounts to prevent GH-A masking secrets (which slows down log output)
+
+        # Use a custom command instead of the make target so that we can pass
+        # an extra argument to ScalaTest to dispatch secondary SBT calls to a
+        # running sbt server (which avoids constainly relaunching the JVM).
+        sbt_command = f"testOnly {test_name} -- -Denable-thin-client=true"
         with settings(warn_only=True):
-            rc = run(f"make -C sim testOnly TARGET_PROJECT={target_project} SCALA_TEST={test_name} ENABLE_SBT_THIN_CLIENT=1 &> scala-test.full.log").return_code
+            rc = run(f"""make -C sim TARGET_PROJECT={target_project} sbt SBT_COMMAND="{sbt_command}" """).return_code
             if rc != 0:
-                run("cat scala-test.full.log")
                 raise Exception("Running scala test failed")
 
 if __name__ == "__main__":
