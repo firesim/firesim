89be5929eb21ac64c344b42b53d362f82f2441e8
David Biancolin <david.biancolin@gmail.com>
2022-05-24 21:58:31 +0000

    make: Add phony targets for scala formatting

--- sim/target-agnostic.mk
+++ sim/target-agnostic.mk
@@ -283,6 +283,27 @@ scaladoc:
 	cd $(base_dir) && $(SBT) "project {file:$(firesim_base_dir)}firesim" "unidoc"
 
 .PHONY: scaladoc
+
+#########################
+# Scalafmt              #
+#########################
+# Checks that all scala main sources under firesim SBT subprojects are formatted.
+scalafmtCheckAll:
+	cd $(base_dir) && $(SBT) ";project {file:$(firesim_base_dir)}firesim; \
+		firesim / scalafmtCheckAll; \
+		firesimLib / scalafmtCheckAll; \
+		midas / scalafmtCheckAll ; \
+		targetutils / scalafmtCheckAll ;"
+
+# Runs the code reformatter in all firesim SBT subprojects
+scalafmtAll:
+	cd $(base_dir) && $(SBT) ";project {file:$(firesim_base_dir)}firesim; \
+		firesim / scalafmtAll; \
+		firesimLib / scalafmtAll; \
+		midas / scalafmtAll ; \
+		targetutils / scalafmtAll ;"
+
+.PHONY: scalafmtCheckAll scalafmtAll
 #########################
 # Cleaning Recipes      #
 #########################
