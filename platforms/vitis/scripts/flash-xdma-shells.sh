957f1152e74e6423ee4c68aaf642ce6f1f54d7ed
abejgonzalez <abe.j.gonza@gmail.com>
2022-06-15 21:49:35 -0700

    Comment out dcp gen | Add warning comment in flash script

--- platforms/vitis/scripts/flash-xdma-shells.sh
+++ platforms/vitis/scripts/flash-xdma-shells.sh
@@ -1,5 +1,6 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
+# WARNING: This script is meant to be used by FireSim developers only. Use at your own risk.
 
 devices=(0000:41:00.0 0000:3d:00.0 0000:1e:00.0 0000:1b:00.0)
 path_to_shell=/lib/firmware/xilinx/bd5fb8abab266c3265918257b5048e88/partition.xsabin
