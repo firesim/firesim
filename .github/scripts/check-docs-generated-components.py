d58d32db41a2fa7018711769816e3362af91589c
Sagar Karandikar <sagark@eecs.berkeley.edu>
2022-06-16 06:18:38 +0000

    debug

--- .github/scripts/check-docs-generated-components.py
+++ .github/scripts/check-docs-generated-components.py
@@ -9,8 +9,16 @@ def run_docs_generated_components_check():
     updated. """
 
     with cd(manager_fsim_dir), prefix('source env.sh'):
+        run("cd deploy && cat config_runtime.yaml")
+        run("cd deploy && cat ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
         run("cd deploy && diff config_runtime.yaml ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
-        run("cd deploy && firesim --help &> TEMP_HELP_OUTPUT && diff TEMP_HELP_OUTPUT ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+
+
+        run("cd deploy && firesim --help")
+        run("cd deploy && cat ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+        run("cd deploy && firesim --help &> TEMP_HELP_OUTPUT")
+        run("cd deploy && cat TEMP_HELP_OUTPUT")
+        run("cd deploy && diff TEMP_HELP_OUTPUT ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
 
 if __name__ == "__main__":
     set_fabric_firesim_pem()
