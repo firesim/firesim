5c78a3d0352caa6dabe72949b24e80670796d5e9
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-06-17 02:44:10 +0000

    Small cleanup

--- .github/scripts/check-docs-generated-components.py
+++ .github/scripts/check-docs-generated-components.py
@@ -9,15 +9,15 @@ def run_docs_generated_components_check():
     updated. """
 
     with cd(manager_fsim_dir), prefix('source sourceme-f1-manager.sh'):
-        run("cd deploy && cat config_runtime.yaml")
-        run("cd deploy && cat ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
-        run("cd deploy && diff config_runtime.yaml ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
-
-        run("cd deploy && firesim --help")
-        run("cd deploy && cat ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
-        run("cd deploy && firesim --help &> TEMP_HELP_OUTPUT")
-        run("cd deploy && cat TEMP_HELP_OUTPUT")
-        run("cd deploy && diff TEMP_HELP_OUTPUT ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+        with prefix("cd deploy"):
+            run("cat config_runtime.yaml")
+            run("cat ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
+            run("diff config_runtime.yaml ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
+            run("firesim --help")
+            run("cat ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+            run("firesim --help &> TEMP_HELP_OUTPUT")
+            run("cat TEMP_HELP_OUTPUT")
+            run("diff TEMP_HELP_OUTPUT ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
 
 if __name__ == "__main__":
     set_fabric_firesim_pem()
