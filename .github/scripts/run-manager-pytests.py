84c0ee0b0516eeb2c39018e2ade13798e2d59d50
Tim Snyder <timothy.snyder@sifive.com>
2022-07-25 16:33:19 +0000

    echo SHA immediately before running pytest

--- .github/scripts/run-manager-pytests.py
+++ .github/scripts/run-manager-pytests.py
@@ -8,6 +8,7 @@ def run_manager_pytests():
     """ Runs all manager pytests """
 
     with cd(manager_fsim_dir), prefix('source env.sh'):
+        run("git rev-parse HEAD")
         run("cd deploy && python3 -m pytest")
 
 if __name__ == "__main__":
