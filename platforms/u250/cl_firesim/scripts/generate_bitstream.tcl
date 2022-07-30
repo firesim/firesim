04558790f02edafcfd7343c00e7767448467d6b7
Björn Gottschall <github.mail@bgottschall.de>
2022-07-30 12:09:37 +0200

    * better u250 implementation script

--- platforms/u250/cl_firesim/scripts/generate_bitstream.tcl
+++ platforms/u250/cl_firesim/scripts/generate_bitstream.tcl
@@ -40,23 +40,64 @@ if {[file exists ./design/firesim_impl.xdc]} {
     set_property constrset impl [get_runs impl_1]
 }
 
+foreach path [glob -nocomplain ./vivado_proj/*.dcp] {
+    file delete -force -- $path
+}
+
+reset_runs synth_1
 launch_runs synth_1 -jobs 6
 wait_on_run synth_1
 
 if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
-    puts "ERROR: synth_1 failed"
+    puts "ERROR: synthesis failed"
     exit 1
 }
 
-set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveFanoutOpt [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
+variable i 0
+variable opt_design_directives {Default Explore Explore Explore Explore Explore Explore}
+variable place_design_directives {Default Explore Explore Explore Explore ExtraTimingOpt ExtraPostPlacementOpt}
+variable phys_opt_design_directives {Default Explore AggressiveExplore AggressiveFanoutOpt AlternateFlowWithRetiming AggressiveExplore AggressiveExplore}
+variable route_design_directives {Default Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
+variable post_route_phys_opt_design_directives {Default Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
+variable WNS -1
+variable WHS -1
 
-launch_runs impl_1 -to_step write_bitstream -jobs 6
+set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
+set_property AUTO_INCREMENTAL_CHECKPOINT.DIRECTORY ./vivado_proj [get_runs impl_1]
+set_property incremental_checkpoint.directive TimingClosure [get_runs impl_1]
+
+while {($i < [llength $opt_design_directives]) && ($WNS < 0 || $WHS < 0)} {
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE [lindex $opt_design_directives $i] [get_runs impl_1]
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE [lindex $place_design_directives $i] [get_runs impl_1]
+    set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE [lindex $route_design_directives $i] [get_runs impl_1]
+    set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE [lindex $phys_opt_design_directives $i] [get_runs impl_1]
+    set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE [lindex $post_route_phys_opt_design_directives $i] [get_runs impl_1]
+
+    reset_runs impl_1
+    launch_runs impl_1 -to_step route_design -jobs 6
+    wait_on_run impl_1
+
+    if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
+        puts "ERROR: implementation failed"
+        exit 1
+    }
+
+    set i [expr $i + 1]
+    set WNS [get_property STATS.WNS [get_runs impl_1]]
+    set WHS [get_property STATS.WHS [get_runs impl_1]]
+}
+
+if {$WNS < 0 || $WHS < 0} {
+    puts "ERROR: did not meet timing!"
+    exit 1
+}
+
+launch_runs impl_1 -next_step -jobs 6
 wait_on_run impl_1
 
 if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
-    puts "ERROR: impl_1 failed"
+    puts "ERROR: implementation failed"
     exit 1
 }
+
+
