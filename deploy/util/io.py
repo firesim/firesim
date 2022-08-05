0e8aec609b31cf59beadb75df9145a9a6b16fbf3
Filip Stamenkovic <92741622+filipstamenkovic-sifive@users.noreply.github.com>
2022-08-05 09:04:37 -0500

    add tests for downloadURI (#1155)
    refactor `localize_xclbin` from the Vitis runner to `util.io.downloadURI()` and add some pytests

--- deploy/util/io.py
+++ deploy/util/io.py
@@ -1,6 +1,7 @@
-
-
 import logging
+from os import PathLike, fspath
+from fsspec.core import url_to_fs # type: ignore
+from pathlib import Path
 
 def firesim_input(prompt: object = None) -> str:
     """wrap builtins.input() understanding the idiocyncracies of firesim+fabric+logging
@@ -21,3 +22,29 @@ def firesim_input(prompt: object = None) -> str:
     rootLogger.debug("User Provided input():'%s'", res)
 
     return res
+
+
+rootLogger = logging.getLogger()
+
+
+def downloadURI(uri: str, local_dest_path: PathLike) -> None:
+    """Uses the fsspec library to fetch a file specified in the uri to the local file system.
+    Args:
+        uri: uri of an object to be fetched
+        local_dest_path: path on the local file system to store the uri object
+    """    
+
+    # TODO consider using fsspec
+    # filecache https://filesystem-spec.readthedocs.io/en/latest/features.html#caching-files-locally
+    # so that multiple slots using the same xclbin only grab it once and
+    # we only download it if it has changed at the source.
+    # HOWEVER, 'filecache' isn't thread/process safe and I'm not sure whether
+    # this runs in @parallel for fabric
+    lpath = Path(local_dest_path)
+    if lpath.exists():
+        rootLogger.debug(f"Overwriting {lpath.resolve(strict=False)}")
+    rootLogger.debug(f"Downloading '{uri}' to '{lpath}'")
+    fs, rpath = url_to_fs(uri)
+    print(fs, type(fs))
+    print(rpath, type(rpath))
+    fs.get_file(rpath, fspath(lpath)) # fspath() b.c. fsspec deals in strings, not PathLike
