25d586e5ea09b18f134c05e229f0a701247cba0d
Tim Snyder <timothy.snyder@sifive.com>
2022-08-08 14:24:59 -0500

    update firesim_input docstring

--- deploy/util/io.py
+++ deploy/util/io.py
@@ -3,11 +3,11 @@
 import logging
 
 def firesim_input(prompt: object = None) -> str:
-    """wrap __builtin__.input() understanding the idiocyncracies of firesim+fabric+logging
+    """wrap builtins.input() understanding the idiocyncracies of firesim+fabric+logging
 
     Log the prompt at CRITICAL level so that it will go to the terminal and the log.
     Log the entered text as DEBUG so that the log contains it.
-    Don't pass the prompt to __builtin__.input() because we don't need StreamLogger to also
+    Don't pass the prompt to builtins.input() because we don't need StreamLogger to also
     be trying to log the prompt.
     
     See 'streamlogger.py' and it's use at the end of 'firesim.py'
