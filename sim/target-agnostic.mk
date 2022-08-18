77fd2b2a197a1d2cb6b3a7fb811d3b497162bdd7
David Biancolin <david.biancolin@gmail.com>
2022-08-18 13:40:36 -0700

    Add no as-needed to workaround vcs link-time issues

--- sim/target-agnostic.mk
+++ sim/target-agnostic.mk
@@ -150,7 +150,10 @@ vcs = $(GENERATED_DIR)/$(DESIGN)
 vcs_debug = $(GENERATED_DIR)/$(DESIGN)-debug
 
 $(vcs) $(vcs_debug): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(VCS_CXXOPTS) -I$(VCS_HOME)/include -D RTLSIM
-$(vcs) $(vcs_debug): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN'
+# VCS can mangle the ordering of libraries and object files at link time such
+# that some valid dependencies are pruned when --as-needed is set.
+# Conservatively set --no-as-needed in case --as-needed is defined in LDFLAGS.
+$(vcs) $(vcs_debug): export LDFLAGS := $(LDFLAGS) -Wl,--no-as-needed $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN'
 
 $(vcs): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h) $(simulator_verilog)
 	$(MAKE) -C $(simif_dir) vcs PLATFORM=$(PLATFORM) DRIVER_NAME=$(DESIGN) GEN_FILE_BASENAME=$(BASE_FILE_NAME) \
