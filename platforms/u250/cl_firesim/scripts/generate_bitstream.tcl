5df6863fdef26bc65d3acfed309ab46e4f7b558e
Björn Gottschall <github.mail@bgottschall.de>
2022-08-29 14:46:33 +0200

    * update u250 script

--- platforms/u250/cl_firesim/scripts/generate_bitstream.tcl
+++ platforms/u250/cl_firesim/scripts/generate_bitstream.tcl
@@ -1,46 +1,48 @@
-create_project -force firesim ./vivado_proj -part xcu250-figd2104-2L-e
+variable root_dir [pwd]
+
+create_project -force firesim ${root_dir}/vivado_proj -part xcu250-figd2104-2L-e
 set_property board_part xilinx.com:au250:part0:1.3 [current_project]
 
-add_files {./design/axi_tieoff_master.v ./design/firesim_wrapper.v ./design/firesim_top.sv}
+add_files [list ${root_dir}/design/axi_tieoff_master.v ${root_dir}/design/firesim_wrapper.v ${root_dir}/design/firesim_top.sv]
 
-if {![file exists ./scripts/create_bd_[version -short].tcl]} {
+if {![file exists ${root_dir}/scripts/create_bd_[version -short].tcl]} {
     puts "ERROR: current platform definition does not support your vivado version"
     exit 1
 }
 
-add_files {./design/firesim_defines.vh}
-set_property IS_GLOBAL_INCLUDE 1 [get_files ./design/firesim_defines.vh]
+add_files [list ${root_dir}/design/firesim_defines.vh]
+set_property IS_GLOBAL_INCLUDE 1 [get_files ${root_dir}/design/firesim_defines.vh]
 
-source ./scripts/firesim_env.tcl
-source ./scripts/create_bd_[version -short].tcl
+source ${root_dir}/scripts/firesim_env.tcl
+source ${root_dir}/scripts/create_bd_[version -short].tcl
 
-make_wrapper -files [get_files ./vivado_proj/firesim.srcs/sources_1/bd/design_1/design_1.bd] -top
+make_wrapper -files [get_files ${root_dir}/vivado_proj/firesim.srcs/sources_1/bd/design_1/design_1.bd] -top
 
-add_files -norecurse ./vivado_proj/firesim.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+add_files -norecurse ${root_dir}/vivado_proj/firesim.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
 
-if {[file exists ./design/firesim_synth.xdc]} {
+if {[file exists ${root_dir}/design/firesim_synth.xdc]} {
     create_fileset -constrset synth
-    add_files -fileset synth -norecurse ./design/firesim_synth.xdc
+    add_files -fileset synth -norecurse ${root_dir}/design/firesim_synth.xdc
 }
 
-if {[file exists ./design/firesim_impl.xdc]} {
+if {[file exists ${root_dir}/design/firesim_impl.xdc]} {
     create_fileset -constrset impl
-    add_files -fileset impl -norecurse ./design/firesim_impl.xdc
+    add_files -fileset impl -norecurse ${root_dir}/design/firesim_impl.xdc
 }
 
 update_compile_order -fileset sources_1
 set_property top design_1_wrapper [current_fileset]
 update_compile_order -fileset sources_1
 
-if {[file exists ./design/firesim_synth.xdc]} {
+if {[file exists ${root_dir}/design/firesim_synth.xdc]} {
     set_property constrset synth [get_runs synth_1]
 }
 
-if {[file exists ./design/firesim_impl.xdc]} {
+if {[file exists ${root_dir}/design/firesim_impl.xdc]} {
     set_property constrset impl [get_runs impl_1]
 }
 
-foreach path [glob -nocomplain ./vivado_proj/*.dcp] {
+foreach path [glob -nocomplain ${root_dir}/vivado_proj/*.dcp] {
     file delete -force -- $path
 }
 
@@ -56,14 +58,14 @@ if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
 variable i 0
 variable opt_design_directives {Default Explore Explore Explore Explore Explore Explore}
 variable place_design_directives {Default Explore Explore Explore Explore ExtraTimingOpt ExtraPostPlacementOpt}
-variable phys_opt_design_directives {Default Explore AggressiveExplore AggressiveFanoutOpt AlternateFlowWithRetiming AggressiveExplore AggressiveExplore}
-variable route_design_directives {Default Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
-variable post_route_phys_opt_design_directives {Default Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
+variable phys_opt_design_directives {Default AggressiveExplore Explore AggressiveFanoutOpt AlternateFlowWithRetiming AggressiveExplore AggressiveExplore}
+variable route_design_directives {Default AggressiveExplore Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
+variable post_route_phys_opt_design_directives {Default AggressiveExplore Explore AggressiveExplore AggressiveExplore AggressiveExplore AggressiveExplore}
 variable WNS -1
 variable WHS -1
 
 set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
-set_property AUTO_INCREMENTAL_CHECKPOINT.DIRECTORY ./vivado_proj [get_runs impl_1]
+set_property AUTO_INCREMENTAL_CHECKPOINT.DIRECTORY ${root_dir}/vivado_proj [get_runs impl_1]
 set_property incremental_checkpoint.directive TimingClosure [get_runs impl_1]
 
 while {($i < [llength $opt_design_directives]) && ($WNS < 0 || $WHS < 0)} {
