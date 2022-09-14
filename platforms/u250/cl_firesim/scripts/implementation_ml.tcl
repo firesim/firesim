5e4f4077806e08443cd4b31ae707a364071fdff7
Björn Gottschall <github.mail@bgottschall.de>
2022-09-14 14:09:31 +0200

    * refined ml strategy implementation

--- platforms/u250/cl_firesim/scripts/implementation_ml.tcl
+++ platforms/u250/cl_firesim/scripts/implementation_ml.tcl
@@ -1,9 +1,12 @@
-variable ml_start_directive Default
+variable ml_start_directive Explore
 variable ml_max_critical_paths 150
 variable ml_max_strategies 5
 
+variable ml_qor_suggestions ${root_dir}/vivado_proj/ml_qor_suggestions.rqs
+variable ml_strategy_dir ${root_dir}/vivado_proj/ml_strategies
+
 # Cleanup
-foreach path [list ${root_dir}/vivado_proj/ml_strategies ${root_dir}/vivado_proj/ml_qor_suggestions.rqs] {
+foreach path [list ${ml_qor_suggestions} ${ml_strategy_dir}] {
     if {[file exists ${path}]} {
         file delete -force -- ${path}
     }
@@ -17,6 +20,7 @@ reset_runs ${impl_run}
 set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ${ml_start_directive} ${impl_run}
 set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ${ml_start_directive} ${impl_run}
 set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE ${ml_start_directive} ${impl_run}
+set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true ${impl_run}
 set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE ${ml_start_directive} ${impl_run}
 set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE ${ml_start_directive} ${impl_run}
 launch_runs ${impl_run} -to_step route_design -jobs ${jobs}
@@ -31,16 +35,47 @@ set WNS [get_property STATS.WNS [get_runs ${impl_run}]]
 set WHS [get_property STATS.WHS [get_runs ${impl_run}]]
 
 if {$WNS < 0 || $WHS < 0} {
-    # Doing ML directives
+    variable ml_tcls [list]
+
     open_run ${impl_run}
     report_qor_suggestions -max_paths ${ml_max_critical_paths} -max_strategies ${ml_max_strategies} -no_split -quiet
-    write_qor_suggestions -force -strategy_dir ${root_dir}/vivado_proj/ml_strategies ${root_dir}/vivado_proj/ml_qor_suggestions.rqs
+    write_qor_suggestions -force -strategy_dir ${ml_strategy_dir} ${ml_qor_suggestions}
     close_design
 
-    variable i 1
-    while {($i <= ${ml_max_strategies}) && ($WNS < 0 || $WHS < 0)} {
+    for {set i 1} {$i <= ${ml_max_strategies}} {incr i} {
         variable tclFile ${root_dir}/vivado_proj/ml_strategies/impl_1Project_MLStrategyCreateRun${i}.tcl
         if {[file exists ${tclFile}]} {
+            lappend ml_tcls ${tclFile}
+        }
+    }
+
+    if {([llength ${ml_tcls}] == 0) && ([file exists ${ml_qor_suggestions}])} {
+        puts "INFO: no ML strategies were found, using base qor suggestions"
+
+        add_files -force -fileset utils_1 ${ml_qor_suggestions}
+
+        reset_runs ${impl_run}
+
+        set_property RQS_FILES ${ml_qor_suggestions} ${impl_run}
+        set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE RQS ${impl_run}
+        set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE RQS ${impl_run}
+        set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true ${impl_run}
+        set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE RQS ${impl_run}
+        set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE RQS ${impl_run}
+
+        launch_runs ${impl_run} -to_step route_design -jobs ${jobs}
+        wait_on_run ${impl_run}
+
+        if {[get_property PROGRESS [get_runs ${impl_run}]] != "100%"} {
+            puts "ERROR: implementation failed"
+            exit 1
+        }
+
+        set WNS [get_property STATS.WNS [get_runs ${impl_run}]]
+        set WHS [get_property STATS.WHS [get_runs ${impl_run}]]
+    } else {
+        foreach tclFile ${ml_tcls} {
+            puts "INFO: using ML strategy from ${tclFile}"
             source ${tclFile}
             set impl_run ${ml_strategy_run}
 
@@ -54,9 +89,13 @@ if {$WNS < 0 || $WHS < 0} {
 
             set WNS [get_property STATS.WNS [get_runs ${impl_run}]]
             set WHS [get_property STATS.WHS [get_runs ${impl_run}]]
+
+            if ($WNS >= 0 && $WHS >= 0) {
+                break
+            }
         }
-        set i [expr $i + 1]
     }
+
     if {$WNS < 0 || $WHS < 0} {
         puts "INFO: no more ML strategies available"
     }
