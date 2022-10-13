7227c622b13344569156e3f38a3e792f3c4a6b84
Tianrui Wei <tianrui@tianruiwei.com>
2022-10-12 22:34:59 -0700

    fix: use the --force flag for rm in case dir empty
    Signed-off-by: Tianrui Wei <tianrui@tianruiwei.com>

--- boards/default/distros/br/br.py
+++ boards/default/distros/br/br.py
@@ -181,8 +181,8 @@ class Builder:
             # # This is unfortunate but buildroot can't remove things from the
             # # image without rebuilding everything from scratch. It adds 20min
             # # to the unit tests and anyone who builds a custom buildroot.
-            wlutil.run(['rm', '-r', 'overlay/*'], cwd=br_dir, env=env)
-            wlutil.run(['rm', '-r', "buildroot/output/target/*"], cwd=br_dir , env=env)
+            wlutil.run(['rm', '-rf', 'overlay/*'], cwd=br_dir, env=env)
+            wlutil.run(['rm', '-rf', "buildroot/output/target/*"], cwd=br_dir , env=env)
             wlutil.run(['make'], cwd=br_dir / "buildroot", env=env)
             shutil.move(img_dir / 'rootfs.ext2', self.outputImg)
 
