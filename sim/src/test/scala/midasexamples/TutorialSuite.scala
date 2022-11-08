2fbe9647a27f68ada5e185f75aa88adbde7748f0
Benjamin Morse <benjamin.morse@sifive.com>
2022-11-08 22:28:06 +0000

    reduce verilator runs from 4 to 2 for Plusargs tests

--- sim/src/test/scala/midasexamples/TutorialSuite.scala
+++ sim/src/test/scala/midasexamples/TutorialSuite.scala
@@ -205,27 +205,27 @@ trait PlusargsKey {
 
 class PlusargsGroup0 extends TutorialSuite("PlusargsModule", "PlusargsModuleTestConfigGroup0") with PlusargsKey {
   it should "provide the correct default value, 3 slice" in {
-    assert(run("verilator", true, args = Seq(getKey(0,0))) == 0)
+    assert(run("verilator", false, args = Seq(getKey(0,0))) == 0)
   }
 
   it should "accept an int from the command line" in {
-    assert(run("verilator", true, args = Seq(s"+plusar_v=3", getKey(0,1))) == 0)
-    assert(run("verilator", true, args = Seq(s"+plusar_v=${BigInt("f00000000", 16)}", getKey(0,2))) == 0)
-    assert(run("verilator", true, args = Seq(s"+plusar_v=${BigInt("f0000000000000000", 16)}", getKey(0,3))) == 0)
+    assert(run("verilator", false, args = Seq(s"+plusar_v=3", getKey(0,1))) == 0)
+    assert(run("verilator", false, args = Seq(s"+plusar_v=${BigInt("f00000000", 16)}", getKey(0,2))) == 0)
+    assert(run("verilator", false, args = Seq(s"+plusar_v=${BigInt("f0000000000000000", 16)}", getKey(0,3))) == 0)
   }
 
   it should "reject large runtime values" in {
-    assert(run("verilator", true, args = Seq(s"+plusar_v=${BigInt("ff0000000000000000", 16)}", getKey(0,4))) != 0)
+    assert(run("verilator", false, args = Seq(s"+plusar_v=${BigInt("ff0000000000000000", 16)}", getKey(0,4))) != 0)
   }
 }
 
 class PlusargsGroup1 extends TutorialSuite("PlusargsModule", "PlusargsModuleTestConfigGroup1") with PlusargsKey {
   it should "provide the correct default value, 1 slice" in {
-    assert(run("verilator", true, args = Seq(getKey(1,0))) == 0)
+    assert(run("verilator", false, args = Seq(getKey(1,0))) == 0)
   }
 
   it should "accept an int from the command line, 1 slice" in {
-    assert(run("verilator", true, args = Seq(s"+plusar_v=${BigInt("1eadbeef", 16)}", getKey(1,1))) == 0)
+    assert(run("verilator", false, args = Seq(s"+plusar_v=${BigInt("1eadbeef", 16)}", getKey(1,1))) == 0)
   }
 }
 
