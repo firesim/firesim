4a3703d54bd228280ba92208b4a813cd14bf0b24
Benjamin Morse <benjamin.morse@sifive.com>
2022-12-12 22:31:37 +0000

    add debug message

--- deploy/runtools/firesim_topology_with_passes.py
+++ deploy/runtools/firesim_topology_with_passes.py
@@ -400,6 +400,7 @@ class FireSimTopologyWithPasses:
                 resolved_cfg = server.get_resolved_server_hardware_config()
 
                 if resolved_cfg.driver_tar_uri is not None:
+                    rootLogger.debug(f"skipping driver build because we're using {resolved_cfg.driver_tar_uri}")
                     continue # skip building or tarballing if we have a prebuilt one
 
                 resolved_cfg.build_sim_driver()
