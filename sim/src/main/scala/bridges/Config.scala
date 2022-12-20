c87557edc60913129e7c37fc3df358d8fa8ecf38
Nandor Licker <n@ndor.email>
2022-12-20 21:07:42 +0200

    Added test for the BlockDevBridge (#1335)
    Implemented a test which pulls data from a blockdev and writes it to another.

--- sim/src/main/scala/bridges/Config.scala
+++ sim/src/main/scala/bridges/Config.scala
@@ -3,8 +3,10 @@
 package firesim.bridges
 
 import firesim.configs.WithDefaultMemModel
+
 import freechips.rocketchip.config._
 import freechips.rocketchip.subsystem.{PeripheryBusKey, PeripheryBusParams}
+import testchipip.{BlockDeviceConfig, BlockDeviceKey}
 
 class NoConfig extends Config(Parameters.empty)
 
@@ -27,4 +29,10 @@ class DefaultVitisConfig
 class UARTConfig
     extends Config((site, here, up) => { case PeripheryBusKey =>
       PeripheryBusParams(beatBytes = 1, blockBytes = 8, dtsFrequency = Some(100000000))
+    })
+
+class BlockDevConfig
+    extends Config((site, here, up) => {
+      case PeripheryBusKey =>
+      case BlockDeviceKey  => Some(BlockDeviceConfig())
     })
