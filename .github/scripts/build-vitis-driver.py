5f5a219301e3a6c4ec2c02e507e2b07084c29503
abejgonzalez <abe.j.gonza@gmail.com>
2023-01-27 13:55:55 -0800

    Fix Vitis driver build CI

--- .github/scripts/build-vitis-driver.py
+++ .github/scripts/build-vitis-driver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from fabric.api import prefix, run, cd, execute # type: ignore
+from fabric.api import prefix, run, execute # type: ignore
 
 from ci_variables import ci_env
 
@@ -13,7 +13,7 @@ def build_vitis_driver():
 
     with prefix(f"cd {ci_env['GITHUB_WORKSPACE']}"):
         with prefix('source sourceme-f1-manager.sh --skip-ssh-setup'):
-            with cd("sim"):
+            with prefix("cd ./sim"):
                 run("make vitis")
 
 if __name__ == "__main__":
