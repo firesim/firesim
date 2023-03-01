a81e3725c11406ea740c3f1a893d344e049f458f
Nandor Licker <n@ndor.email>
2023-03-02 01:42:43 +0200

    Removed test_harness_bridge and simplified harnesses (#1442)
    * Removed test_harness_bridge and simplified harnesses
    
    Instead, the test is now performed by `fasedtests_top`, in line with other tests.
    Also slightly simplified the test harnessed for both FASED and FireSim.
    
    * Update sim/midas/src/main/cc/core/systematic_scheduler.h
    
    Co-authored-by: David Biancolin <david.biancolin@sifive.com>
    
    ---------
    
    Co-authored-by: David Biancolin <david.biancolin@sifive.com>

--- sim/midas/src/main/cc/core/systematic_scheduler.cc
+++ sim/midas/src/main/cc/core/systematic_scheduler.cc
@@ -1,19 +1,35 @@
 // See LICENSE for license details
-#include "systematic_scheduler.h"
-#include <assert.h>
 
-void systematic_scheduler_t::register_task(task_t &&task,
-                                           uint64_t first_cycle) {
+#include "systematic_scheduler.h"
+
+#include <cassert>
+
+systematic_scheduler_t::systematic_scheduler_t(
+    const std::vector<std::string> &args) {
+  for (const auto &arg : args) {
+    if (arg.find("+max-cycles=") == 0) {
+      max_cycles = atoi(arg.c_str() + 12);
+    }
+  }
+}
+
+void systematic_scheduler_t::register_task(uint64_t first_cycle,
+                                           task_t &&task) {
   tasks.push_back(task_tuple_t{std::move(task), first_cycle});
 }
 
 uint32_t systematic_scheduler_t::get_largest_stepsize() {
-  uint64_t next_cycle = std::min(current_cycle + default_step_size, max_cycles);
+  uint64_t next_cycle = current_cycle + default_step_size;
+  if (max_cycles && *max_cycles < next_cycle) {
+    next_cycle = *max_cycles;
+  }
+
   for (auto &t : tasks) {
     if (t.next_cycle < next_cycle) {
       next_cycle = t.next_cycle;
     }
   }
+
   assert(next_cycle - current_cycle <= MAX_MIDAS_STEP);
   uint32_t step = next_cycle - current_cycle;
   assert(step != 0); // Check for forward progress.
