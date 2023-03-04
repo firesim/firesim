2f13158e0ee8f5880db993d1a50e1d87c32ebe31
abejgonzalez <abe.j.gonza@gmail.com>
2023-03-03 17:17:16 -0800

    Initial support for fat jars

--- sim/make/chipyard.mk
+++ sim/make/chipyard.mk
@@ -18,8 +18,17 @@ SCALA_BUILDTOOL_DEPS ?= $(sbt_sources)
 
 SBT ?= java -jar $(rocketchip_dir)/sbt-launch.jar $(SBT_OPTS)
 
+# (1) - classpath of the fat jar
+# (2) - main class
+# (3) - main class arguments
 define run_scala_main
-	cd $(base_dir) && $(SBT) ";project $(1); runMain $(2) $(3)"
+	cd $(base_dir) && java -cp $(1) $(2) $(3)
+endef
+
+# (1) - sbt project to assemble
+# (2) - classpath file(s) to create
+define run_sbt_assembly
+	cd $(base_dir) && $(SBT) ";project $(1); set assembly / assemblyOutputPath := file(\"$(2)\"); assembly"
 endef
 
 else # FIRESIM_STANDALONE
