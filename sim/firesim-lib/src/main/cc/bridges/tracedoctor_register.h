15d0ba0c359d4f92406002b65893f991611ea79b
Björn Gottschall <github.mail@bgottschall.de>
2023-04-18 10:48:50 +0200

    * small cleanup

--- sim/firesim-lib/src/main/cc/bridges/tracedoctor_register.h
+++ sim/firesim-lib/src/main/cc/bridges/tracedoctor_register.h
@@ -9,26 +9,26 @@
 #include <functional>
 #include <memory>
 
-#define __ADD_TRACEDOCTOR_WORKER(__name, __class, ...)                       \
+#define __REGISTER_TRACEDOCTOR_WORKER(__name, __class, ...)                  \
   {                                                                          \
     __name,                                                                  \
-    [](std::vector<std::string> const &args, struct traceInfo const &info){  \
+    [](std::vector<std::string> const &args, struct traceInfo const &info) { \
       return std::unique_ptr<tracedoctor_worker>(new __class(__VA_ARGS__));  \
     }                                                                        \
-  },
+  }
 
-#define ADD_TRACEDOCTOR_WORKER(__name, __class) __ADD_TRACEDOCTOR_WORKER(__name, __class, args, info)
+#define REGISTER_TRACEDOCTOR_WORKER(__name, __class) __REGISTER_TRACEDOCTOR_WORKER(__name, __class, args, info)
 
 typedef std::map<std::string, std::function<std::unique_ptr<tracedoctor_worker>(std::vector<std::string> const &, struct traceInfo const &)>> tracedoctor_register_t;
 
 // This is the worker register. Add your entries in this map to register new workers.
 static tracedoctor_register_t const tracedoctor_register = {
-  ADD_TRACEDOCTOR_WORKER("dummy", tracedoctor_dummy)
-  ADD_TRACEDOCTOR_WORKER("filer", tracedoctor_filer)
-  ADD_TRACEDOCTOR_WORKER("tracerv", tracedoctor_tracerv)
+  REGISTER_TRACEDOCTOR_WORKER("dummy",   tracedoctor_dummy),
+  REGISTER_TRACEDOCTOR_WORKER("filer",   tracedoctor_filer),
+  REGISTER_TRACEDOCTOR_WORKER("tracerv", tracedoctor_tracerv),
 };
 
-// HINT: if the compiler complains about 'expected primary-expressin'
+// HINT: if the compiler complains about 'expected primary-expression'
 // it most likely couldn't find the tracedoctor worker class you have
 // specificed. Make sure you have included the correct header file and
 // that the class is spelled correctly.
