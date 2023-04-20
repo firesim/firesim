148f291391935a103eec1538af27822cdcca7b73
abejgonzalez <abe.j.gonza@gmail.com>
2023-04-20 15:36:57 -0700

    Better run script

--- sim/run-midas-test.sh
+++ sim/run-midas-test.sh
@@ -2,9 +2,19 @@
 
 set -ex
 
+pushd ../target-design/chipyard/tests
+make
+popd
+
+rm -rf generated-src/f1/*/*.out
+rm -rf generated-src/f1/*/TRACEFILE*
+
 make \
     DESIGN=FireSim \
     TARGET_CONFIG=VitisFireSimRocketConfig \
     PLATFORM_CONFIG=BaseVitisConfig \
-    SIM_BINARY=/scratch/abejgonza/fs-fix-dma/target-design/chipyard/tests/hello.riscv \
+    SIM_BINARY=$PWD/../target-design/chipyard/tests/hello.riscv \
     run-vcs
+
+tail -n 10 generated-src/f1/*/*.out
+tail -n 10 generated-src/f1/*/TRACEFILE*
