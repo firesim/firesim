7cd03f49763b1775bd51627368fec8a6805d74b1
abejgonzalez <abe.j.gonza@gmail.com>
2023-04-19 18:04:22 -0700

    Update

--- scripts/full-tutorial-run.sh
+++ scripts/full-tutorial-run.sh
@@ -3,7 +3,9 @@
 set -ex
 set -o pipefail
 
-# AMI SETUP #
+# AMI SETUP START #
+
+export MAKEFLAGS=-j16
 
 ################################### CHIPYARD AM
 cd ~/
@@ -12,7 +14,8 @@ cd ~/
 # get a subshell so can source sourceme-f1-manager.sh later
 
 # get chipyard
-git clone https://github.com/ucb-bar/chipyard -b hpca-2023-morning chipyard-morning
+git clone https://github.com/ucb-bar/chipyard -b 1.9.1 chipyard-morning
+
 # chipyard init
 cd chipyard-morning
 
@@ -20,7 +23,6 @@ cd chipyard-morning
 ./build-setup.sh riscv-tools -f
 
 source env.sh
-conda install networkx
 
 # firesim init
 ./scripts/firesim-setup.sh
@@ -28,14 +30,12 @@ cd sims/firesim
 source sourceme-f1-manager.sh
 
 # verilator pre-build
-cd ~/chipyard-morning/sims/verilator/
+cd $MCYDIR/sims/verilator/
 make
 make clean
 
-cd ~/chipyard-morning
-chmod +x scripts/repo-clean.sh
+cd $MCYDIR
 ./scripts/repo-clean.sh
-git checkout scripts/repo-clean.sh
 
 )
 
@@ -48,7 +48,7 @@ cd ~/
 # get a subshell so can source sourceme-f1-manager.sh later
 
 # get chipyard
-git clone https://github.com/ucb-bar/chipyard -b hpca-2023-morning chipyard-afternoon
+git clone https://github.com/ucb-bar/chipyard -b 1.9.1 chipyard-afternoon
 # chipyard init
 cd chipyard-afternoon
 
@@ -64,73 +64,73 @@ source sourceme-f1-manager.sh
 
 # run through elaboration flow once to get chisel/sbt all setup
 cd sim
-unset MAKEFLAGS
 make f1
-export MAKEFLAGS=-j16
 
 # build Linux target software once
 cd ../sw/firesim-software
 ./init-submodules.sh
 marshal -v build br-base.json
 
-# sha3 stuff
-cd ~/chipyard-afternoon/generators/sha3/software/
-git submodule update --init esp-isa-sim
-git submodule update --init linux
-./build-spike.sh
-./build.sh
-
-# more sha3 workload stuff
-cd ~/chipyard-afternoon/generators/sha3/software/
-marshal -v build marshal-configs/sha3-linux-jtr-test.yaml
-marshal -v build marshal-configs/sha3-linux-jtr-crack.yaml
-marshal -v install marshal-configs/sha3*.yaml
-
 # pre-build stuff
 cd ~/chipyard-afternoon/sims/firesim/sim/
-unset MAKEFLAGS
 make f1 DESIGN=FireSim TARGET_CONFIG=WithNIC_DDR3FRFCFSLLC4MB_WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.QuadRocketConfig PLATFORM_CONFIG=F90MHz_BaseF1Config
 make f1 DESIGN=FireSim TARGET_CONFIG=WithNIC_DDR3FRFCFSLLC4MB_WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.LargeBoomConfig PLATFORM_CONFIG=F65MHz_BaseF1Config
 make f1 DESIGN=FireSim TARGET_CONFIG=WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.RocketConfig PLATFORM_CONFIG=F90MHz_BaseF1Config
-make f1 DESIGN=FireSim TARGET_CONFIG=WithNIC_DDR3FRFCFSLLC4MB_WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.Sha3RocketConfig PLATFORM_CONFIG=F65MHz_BaseF1Config
-make f1 DESIGN=FireSim TARGET_CONFIG=DDR3FRFCFSLLC4MB_WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.Sha3RocketConfig PLATFORM_CONFIG=F65MHz_BaseF1Config
-make f1 DESIGN=FireSim TARGET_CONFIG=DDR3FRFCFSLLC4MB_WithDefaultFireSimBridges_WithFireSimHighPerfConfigTweaks_chipyard.Sha3RocketPrintfConfig PLATFORM_CONFIG=F30MHz_WithPrintfSynthesis_BaseF1Config
-export MAKEFLAGS=-j16
 
 cd ~/chipyard-afternoon
-chmod +x scripts/repo-clean.sh
 ./scripts/repo-clean.sh
-git checkout scripts/repo-clean.sh
 
 )
 
+#################################### END CHIPYARD AM PREP
+
 # AMI SETUP END #
 
-# 03 - building_custom_socs
+MCYDIR="~/chipyard-morning"
+ACYDIR="~/chipyard-afternoon"
 
-cd ~/chipyard-morning/generators/chipyard/src/main/scala/config/
-sed -i '120s/\/\/\( new sha3.*\)/\1/' TutorialConfigs.scala
+cd $ACYDIR
+source ./env.sh
 
-cd ~/chipyard-morning/sims/verilator
-make CONFIG=TutorialNoCConfig -j16
-cd verilator/generated-src/chipyard*Tutorial/
-ls
-
-cd ~/chipyard-morning/tests
-make -j16
-
-cd ~/chipyard-morning/sims/verilator
-make CONFIG=TutorialNoCConfig run-binary-hex BINARY=../../tests/fft.riscv
-
-cd ~/chipyard-morning/generators/sha3/software
-./build.sh
-
-cd ~/chipyard-morning/sims/verilator
-make CONFIG=TutorialNoCConfig run-binary-hex BINARY=$SHA3SW/sha3-rocc.riscv
+cd $FDIR
+source ./sourceme-f1-manager.sh
 
 exit 0 # TODO FINISH AFTER THIS
 
-# 06 - building_hw_firesim
+# 03 - building_custom_socs
+
+cd $MCYDIR/sims/verilator
+make CONFIG=TutorialLeanGemminiConfig -j16
+cd generated-src/chipyard*Tutorial/
+ls
+cd gen-collateral/
+ls
+
+cd $MCYDIR/tests
+make -j16
+spike hello.riscv
+spike mt-hello.riscv
+spike -p4 mt-hello.riscv
+
+spike mt-gemmini.riscv
+spike --extension=gemmini mt-gemmini.riscv
+spike --extension=gemmini -p4 mt-gemmini.riscv
+
+cd $MCYDIR/sims/verilator
+make CONFIG=TutorialLeanGemminiConfig BINARY=$MCYDIR/tests/hello.riscv run-binary-hex
+make CONFIG=TutorialLeanGemminiConfig BINARY=$MCYDIR/tests/mt-hello.riscv run-binary-hex
+make CONFIG=TutorialLeanGemminiConfig BINARY=$MCYDIR/tests/mt-gemmini.riscv run-binary-hex
+
+cd $MCYDIR/sims/verilator
+make CONFIG=TutorialManyCoreNoCConfig verilog
+
+# TODO: when xcelium support added, run TutorialManyCoreNoCConfig with mt-hello.riscv
+
+make find-config-fragments
+
+# 07 - building_hw_firesim
+
+FDIR="~/chipyard-afternoon/sims/firesim"
 
 cd $FDIR
 ls
@@ -147,51 +147,63 @@ firesim managerinit --platform f1
 ls
 
 cd $FDIR/deploy
-cat built-hwdb-entries/*
 
-cd ~/chipyard-afternoon/generators/sha3/software/
-marshal -d build marshal-configs/sha3-linux-test.yaml
+# 08 - building_sw_firesim
 
-# 07 - running_firesim_simulations
 
-cd ~/chipyard-afternoon
-source ./env.sh
+cd $ACYDIR/software/tutorial
+ls
 
-cd $FDIR
-source ./sourceme-f1-manager.sh
+cd marshal-configs
+marshal -v -d build resnet50-linux.yaml
+marshal -d launch -s resnet50-linux.yaml
 
-sed -i 's/\(- f1.2xlarge: \).*/1/' $FDIR/deploy/config_runtime.yaml
-sed -i 's/\(- f1.4xlarge: \).*/1/' $FDIR/deploy/config_runtime.yaml
-sed -i 's/\(- f1.16xlarge: \).*/1/' $FDIR/deploy/config_runtime.yaml
-sed -i 's/\(topology: \).*/no_net_config/' $FDIR/deploy/config_runtime.yaml
-sed -i 's/\(no_net_num_nodes: \).*/1/' $FDIR/deploy/config_runtime.yaml
-sed -i 's/\(default_hw_config: \).*/firesim_rocket_singlecore_no_nic_l2_lbp/' $FDIR/deploy/config_runtime.yaml
+marshal -v -d build mobilenet-baremetal.yaml
+marshal -v -d install mobilenet-baremetal.yaml
+
+cd $FDIR/deploy/workloads
+cat mobilenet-baremetal.json
+
+# 09 - running_firesim_simulations
+
+sed -i 's/\(- f1.2xlarge: \).*/\1/' $FDIR/deploy/config_runtime.yaml
+sed -i 's/\(topology: \).*/\1no_net_config/' $FDIR/deploy/config_runtime.yaml
+sed -i 's/\(no_net_num_nodes: \).*/\1/' $FDIR/deploy/config_runtime.yaml
+sed -i 's/\(default_hw_config: \).*/\1firesim_rocket_singlecore_no_nic_l2_lbp/' $FDIR/deploy/config_runtime.yaml
+
+# TODO: running the poweroff so simulation finishes
+make linux-poweroff
+sed -i 's/\(workload: \).*/\1linux-poweroff-uniform.json/' $FDIR/deploy/config_runtime.yaml
+
+# DEBUG
+cat $FDIR/deploy/config_runtime.yaml
 
 cd $FDIR/deploy
 cat built-hwdb-entries/firesim_rocket_singlecore_no_nic_l2_lbp >> config_hwdb.yaml
 
-firesim launchrunfarm
-firesim infrasetup
-firesim runworkload > /dev/null &
+# DEBUG
+TIMEOUTPREFIX="timeout 1h"
 
-sleep 1m # wait for simulation to start
+$TIMEOUTPREFIX firesim launchrunfarm
+$TIMEOUTPREFIX firesim infrasetup
+$TIMEOUTPREFIX firesim runworkload
 
-function waitForBoot {
-    cd $FDIR/deploy/results-workload/
-    LAST_DIR=$(ls | tail -n1)
-    if [ -d "$LAST_DIR" ]; then
-        while ! grep -i "Welcome to Buildroot" $LAST_DIR/*/uartlog;
-        do
-            echo "Waiting on boot";
-            sleep 2;
-        done
-    else
-        echo "Unable to find output directory"
-        exit 111
-    fi
-}
-export -f waitForBoot
-timeout 10m bash -c waitForBoot
+# DEBUG
+cd results-workload/ && LAST_DIR=$(ls | tail -n1) && if [ -d "$LAST_DIR" ]; then tail -n300 $LAST_DIR/*/*; fi
 
-kill $(jobs -p) # should be noop
-firesim terminaterunfarm -q
+cd $FDIR/deploy
+sed -i 's/\(default_hw_config: \).*/\1firesim_gemmini_rocket_singlecore_no_nic/' $FDIR/deploy/config_runtime.yaml
+sed -i 's/\(workload: \).*/\1mobilenet-baremetal.json/' $FDIR/deploy/config_runtime.yaml
+$TIMEOUTPREFIX firesim infrasetup
+$TIMEOUTPREFIX firesim runworkload
+
+# DEBUG
+cd results-workload/ && LAST_DIR=$(ls | tail -n1) && if [ -d "$LAST_DIR" ]; then tail -n300 $LAST_DIR/*/*; fi
+
+# 10 - instrumenting_debugging_firesim
+
+sed -i 's/\(uartlog",\).*/\1 "synthesized-prints.out*"/' $FDIR/deploy/workloads/mobilenet-baremetal.json
+$TIMEOUTPREFIX firesim infrasetup
+$TIMEOUTPREFIX firesim runworkload
+
+$TIMEOUTPREFIX firesim terminaterunfarm -q
