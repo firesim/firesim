2b94ac1560e92283a5cadcee341d207392a873cd
abejgonzalez <abe.j.gonza@gmail.com>
2023-05-06 22:56:49 -0700

    Fix some CI errors

--- .github/scripts/check-docs-generated-components.py
+++ .github/scripts/check-docs-generated-components.py
@@ -11,13 +11,15 @@ def run_docs_generated_components_check():
     with cd(manager_fsim_dir), prefix('source sourceme-f1-manager.sh'):
         with prefix("cd deploy"):
             run("cat config_runtime.yaml")
-            run("cat ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
-            run("diff config_runtime.yaml ../docs/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml")
+            path = 'docs/Getting-Started-Guides/AWS-EC2-F1-Tutorial/Running-Simulations-Tutorial/DOCS_EXAMPLE_config_runtime.yaml'
+            run(f"cat ../{path}")
+            run(f"diff config_runtime.yaml ../{path}")
             run("firesim --help")
-            run("cat ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+            path = "docs/Advanced-Usage/Manager/HELP_OUTPUT"
+            run(f"cat ../{path}")
             run("firesim --help &> TEMP_HELP_OUTPUT")
             run("cat TEMP_HELP_OUTPUT")
-            run("diff TEMP_HELP_OUTPUT ../docs/Advanced-Usage/Manager/HELP_OUTPUT")
+            run(f"diff TEMP_HELP_OUTPUT ../{path}")
 
 if __name__ == "__main__":
     set_fabric_firesim_pem()
