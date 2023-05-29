9e2a859b9c5bb0b91a67ecde37bc0cc9f40cf783
abejgonzalez <abe.j.gonza@gmail.com>
2023-05-29 15:49:09 -0700

    Update _vcs_t to _all_t

--- sim/midas/src/main/cc/simif_emul_all.cc
+++ sim/midas/src/main/cc/simif_emul_all.cc
@@ -2,13 +2,13 @@
 #include <signal.h>
 
 /**
- * VCS-specific metasimulator implementation.
+ * Emulator-specific metasimulator implementation.
  */
-class simif_emul_vcs_t final : public simif_emul_t {
+class simif_emul_all_t final : public simif_emul_t {
 public:
-  simif_emul_vcs_t(const TargetConfig &config, int argc, char **argv);
+  simif_emul_all_t(const TargetConfig &config, int argc, char **argv);
 
-  ~simif_emul_vcs_t() {}
+  ~simif_emul_all_t() {}
 
   int run(simulation_t &sim);
 
@@ -17,18 +17,18 @@ private:
   char **argv;
 };
 
-simif_emul_vcs_t::simif_emul_vcs_t(const TargetConfig &config,
+simif_emul_all_t::simif_emul_all_t(const TargetConfig &config,
                                    int argc,
                                    char **argv)
     : simif_emul_t(config, std::vector<std::string>(argv + 1, argv + argc)),
       argc(argc), argv(argv) {}
 
-int simif_emul_vcs_t::run(simulation_t &sim) {
+int simif_emul_all_t::run(simulation_t &sim) {
   start_driver(sim);
   return 0;
 }
 
 std::unique_ptr<simif_t>
 create_simif(const TargetConfig &config, int argc, char **argv) {
-  return std::make_unique<simif_emul_vcs_t>(config, argc, argv);
+  return std::make_unique<simif_emul_all_t>(config, argc, argv);
 }
