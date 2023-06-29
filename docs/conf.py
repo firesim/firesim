8c496a387ad2c42ea27e130017e054a6572ac0fe
Sagar Karandikar <sagark@eecs.berkeley.edu>
2023-06-29 02:00:49 +0000

    test

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', '**/*-Template.rst']
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', '**/*-Template.rst', '*/*-Template.rst', '*-Template.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
