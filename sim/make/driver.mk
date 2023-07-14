c3b3ae40673b78b7ba8a76a69861a708395876f8
Yangyu Chen <cyy@cyyself.name>
2023-07-15 02:49:46 +0800

    driver: use libs from conda

--- sim/make/driver.mk
+++ sim/make/driver.mk
@@ -35,7 +35,7 @@ $(f1): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 $(xilinx_alveo_u250): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
               -idirafter ${CONDA_PREFIX}/include -idirafter /usr/include
 $(xilinx_alveo_u250): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu
+              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64
 
 # Compile Driver
 $(xilinx_alveo_u250): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
@@ -52,7 +52,7 @@ $(xilinx_alveo_u250): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 $(xilinx_alveo_u280): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
               -idirafter ${CONDA_PREFIX}/include -idirafter /usr/include
 $(xilinx_alveo_u280): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu
+              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64
 
 # Compile Driver
 $(xilinx_alveo_u280): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
@@ -70,7 +70,7 @@ $(xilinx_alveo_u280): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 $(xilinx_alveo_u200): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
               -idirafter ${CONDA_PREFIX}/include -idirafter /usr/include
 $(xilinx_alveo_u200): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu
+              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64
 
 # Compile Driver
 $(xilinx_alveo_u200): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
@@ -88,7 +88,7 @@ $(xilinx_alveo_u200): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 $(xilinx_vcu118): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
               -idirafter ${CONDA_PREFIX}/include -idirafter /usr/include
 $(xilinx_vcu118): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu
+              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64
 
 # Compile Driver
 $(xilinx_vcu118): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
@@ -105,7 +105,7 @@ $(xilinx_vcu118): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
 $(rhsresearch_nitefury_ii): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
               -idirafter ${CONDA_PREFIX}/include -idirafter /usr/include
 $(rhsresearch_nitefury_ii): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu
+              -L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64
 
 # Compile Driver
 $(rhsresearch_nitefury_ii): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
@@ -123,7 +123,7 @@ $(vitis): export CXXFLAGS := $(CXXFLAGS) $(common_cxx_flags) $(DRIVER_CXXOPTS) \
 	-idirafter ${CONDA_PREFIX}/include -idirafter /usr/include -idirafter $(XILINX_XRT)/include
 # -ldl needed for Ubuntu 20.04 systems (is backwards compatible with U18.04 systems)
 $(vitis): export LDFLAGS := $(LDFLAGS) $(common_ld_flags) -Wl,-rpath='$$$$ORIGIN' \
-	-L${CONDA_PREFIX}/lib -Wl,-rpath-link=/usr/lib/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -L$(XILINX_XRT)/lib -luuid -lxrt_coreutil -ldl
+	-L${CONDA_PREFIX}/lib -Wl,-rpath-link=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 -L$(XILINX_XRT)/lib -luuid -lxrt_coreutil -ldl
 
 # Compile Driver
 $(vitis): $(header) $(DRIVER_CC) $(DRIVER_H) $(midas_cc) $(midas_h)
