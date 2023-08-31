08b565c8c69f125a1de292eb8efe03ee08408d2e
abejgonzalez <abe.j.gonza@gmail.com>
2023-08-30 22:16:42 -0700

    Bump Chipyard + Formatting fixes

--- sim/firesim-lib/src/main/scala/bridges/CospikeBridge.scala
+++ sim/firesim-lib/src/main/scala/bridges/CospikeBridge.scala
@@ -5,27 +5,26 @@ import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 
-import testchipip.{TileTraceIO, TraceBundleWidths, SerializableTileTraceIO, SpikeCosimConfig}
+import testchipip.{SerializableTileTraceIO, SpikeCosimConfig, TileTraceIO, TraceBundleWidths}
 
 import midas.widgets._
 
 case class CospikeBridgeParams(
   widths: TraceBundleWidths,
   hartid: Int,
-  cfg: SpikeCosimConfig
+  cfg:    SpikeCosimConfig,
 )
 
 class CospikeTargetIO(widths: TraceBundleWidths) extends Bundle {
   val trace = Input(new SerializableTileTraceIO(widths))
 }
 
-/**
- * Blackbox that is instantiated in the target
- */
-class CospikeBridge(params: CospikeBridgeParams) extends BlackBox
-    with Bridge[HostPortIO[CospikeTargetIO], CospikeBridgeModule]
-{
-  val io = IO(new CospikeTargetIO(params.widths))
+/** Blackbox that is instantiated in the target
+  */
+class CospikeBridge(params: CospikeBridgeParams)
+    extends BlackBox
+    with Bridge[HostPortIO[CospikeTargetIO], CospikeBridgeModule] {
+  val io       = IO(new CospikeTargetIO(params.widths))
   val bridgeIO = HostPort(io)
 
   // give the Cospike params to the GG module
@@ -35,17 +34,16 @@ class CospikeBridge(params: CospikeBridgeParams) extends BlackBox
   generateAnnotations()
 }
 
-/**
- * Helper function to connect blackbox
- */
+/** Helper function to connect blackbox
+  */
 object CospikeBridge {
   def apply(trace: TileTraceIO, hartid: Int, cfg: SpikeCosimConfig) = {
     val params = new CospikeBridgeParams(trace.traceBundleWidths, hartid, cfg)
-    val cosim = withClockAndReset(trace.clock, trace.reset) {
+    val cosim  = withClockAndReset(trace.clock, trace.reset) {
       Module(new CospikeBridge(params))
     }
     cosim.io.trace.trace.insns.map(t => {
-      t := DontCare
+      t       := DontCare
       t.valid := false.B
     })
     cosim.io.trace := trace.asSerializableTileTrace
@@ -58,23 +56,23 @@ object CospikeBridge {
 //* This lives in the host (still runs on the FPGA)
 //*************************************************
 
-class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) extends BridgeModule[HostPortIO[CospikeTargetIO]]()(p)
-    with StreamToHostCPU
-{
+class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters)
+    extends BridgeModule[HostPortIO[CospikeTargetIO]]()(p)
+    with StreamToHostCPU {
   // CONSTANTS: DMA Parameters
   val toHostCPUQueueDepth = 6144
 
   lazy val module = new BridgeModuleImp(this) {
 
     // setup io
-    val io = IO(new WidgetIO)
+    val io    = IO(new WidgetIO)
     val hPort = IO(HostPort(new CospikeTargetIO(params.widths)))
 
     // helper to get number to round up to nearest multiple
     def roundUp(num: Int, mult: Int): Int = { (num.toFloat / mult).ceil.toInt * mult }
 
     // get the traces
-    val traces = hPort.hBits.trace.trace.insns.map({ unmasked =>
+    val traces             = hPort.hBits.trace.trace.insns.map({ unmasked =>
       val masked = WireDefault(unmasked)
       masked.valid := unmasked.valid && !hPort.hBits.trace.reset
       masked
@@ -100,7 +98,7 @@ class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) e
         trace.priv.asUInt.pad(8),
         trace.insn.pad(insnWidth),
         trace.iaddr.pad(iaddrWidth),
-        boolPad(trace.valid, 8)
+        boolPad(trace.valid, 8),
       )
 
       if (wdataWidth == 0) {
@@ -110,7 +108,7 @@ class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) e
       }
     }
 
-    val maxTraceSize = paddedTraces.map(t => t.getWidth).max
+    val maxTraceSize  = paddedTraces.map(t => t.getWidth).max
     val outDataSzBits = streamEnq.bits.getWidth
 
     // constant
@@ -123,7 +121,7 @@ class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) e
     require(maxTraceSize < bitsPerTrace, "All instruction trace bits (i.e. valid, pc, instBits...) must fit in 256b")
 
     // how many traces being sent over
-    val numTraces = traces.size
+    val numTraces      = traces.size
     // num tokens needed to display full set of instructions from one cycle
     val numTokenForAll = ((numTraces - 1) / totalTracesPerToken) + 1
 
@@ -131,15 +129,15 @@ class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) e
     val counterFire = streamEnq.fire
     val (cnt, wrap) = Counter(counterFire, numTokenForAll)
 
-    val paddedTracesAligned = paddedTraces.map(t => t.asUInt.pad(bitsPerTrace))
+    val paddedTracesAligned   = paddedTraces.map(t => t.asUInt.pad(bitsPerTrace))
     val paddedTracesTruncated = if (numTraces == 1) {
       (VecInit(paddedTracesAligned).asUInt >> (outDataSzBits.U * cnt))
     } else {
-      (VecInit(paddedTracesAligned).asUInt >> (outDataSzBits.U * cnt))(outDataSzBits-1, 0)
+      (VecInit(paddedTracesAligned).asUInt >> (outDataSzBits.U * cnt))(outDataSzBits - 1, 0)
     }
 
     streamEnq.valid := hPort.toHost.hValid
-    streamEnq.bits := paddedTracesTruncated
+    streamEnq.bits  := paddedTracesTruncated
 
     // tell the host that you are ready to get more
     hPort.toHost.hReady := streamEnq.ready && wrap
@@ -151,26 +149,32 @@ class CospikeBridgeModule(params: CospikeBridgeParams)(implicit p: Parameters) e
 
     // modify the output header file
     override def genHeader(base: BigInt, memoryRegions: Map[String, BigInt], sb: StringBuilder): Unit = {
-      genConstructor(base, sb, "cospike_t", "cospike", Seq(
-        UInt32(iaddrWidth),
-        UInt32(insnWidth),
-        UInt32(causeWidth),
-        UInt32(wdataWidth),
-        UInt32(numTraces),
-        CStrLit(params.cfg.isa),
-        UInt32(params.cfg.vlen),
-        CStrLit(params.cfg.priv),
-        UInt32(params.cfg.pmpregions),
-        UInt64(params.cfg.mem0_base),
-        UInt64(params.cfg.mem0_size),
-        UInt32(params.cfg.nharts),
-        CStrLit(params.cfg.bootrom),
-        UInt32(params.hartid),
-        UInt32(toHostStreamIdx),
-        UInt32(toHostCPUQueueDepth),
-      ),
-      hasStreams = true,
-    ) }
+      genConstructor(
+        base,
+        sb,
+        "cospike_t",
+        "cospike",
+        Seq(
+          UInt32(iaddrWidth),
+          UInt32(insnWidth),
+          UInt32(causeWidth),
+          UInt32(wdataWidth),
+          UInt32(numTraces),
+          CStrLit(params.cfg.isa),
+          UInt32(params.cfg.vlen),
+          CStrLit(params.cfg.priv),
+          UInt32(params.cfg.pmpregions),
+          UInt64(params.cfg.mem0_base),
+          UInt64(params.cfg.mem0_size),
+          UInt32(params.cfg.nharts),
+          CStrLit(params.cfg.bootrom),
+          UInt32(params.hartid),
+          UInt32(toHostStreamIdx),
+          UInt32(toHostCPUQueueDepth),
+        ),
+        hasStreams = true,
+      )
+    }
 
     // general information printout
     println(s"Cospike Bridge Information")
