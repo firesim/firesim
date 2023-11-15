704cc5e1814fd596d1c555bb73f02a99078c2fc4
abejgonzalez <abe.j.gonza@gmail.com>
2023-11-14 19:05:59 -0800

    Small fixes

--- platforms/xilinx_alveo_u250/cl_firesim/scripts/implementation.tcl
+++ platforms/xilinx_alveo_u250/cl_firesim/scripts/implementation.tcl
@@ -6,23 +6,19 @@ set_property -dict [ list \
   STEPS.OPT_DESIGN.IS_ENABLED $opt \
   STEPS.OPT_DESIGN.DIRECTIVE $opt_directive \
   {STEPS.OPT_DESIGN.MORE OPTIONS} "$opt_options" \
-
   STEPS.PLACE_DESIGN.DIRECTIVE $place_directive \
   {STEPS.PLACE_DESIGN.MORE OPTIONS} "$place_options" \
-
   STEPS.PHYS_OPT_DESIGN.IS_ENABLED $phys_opt \
   STEPS.PHYS_OPT_DESIGN.DIRECTIVE $phys_directive \
   {STEPS.PHYS_OPT_DESIGN.MORE OPTIONS} "$phys_options" \
-
   STEPS.ROUTE_DESIGN.DIRECTIVE $route_directive \
   {STEPS.ROUTE_DESIGN.MORE OPTIONS} "$route_options" \
-
   STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED $route_phys_opt \
   STEPS.POST_ROUTE_PHYS_OPT_DESIGN.DIRECTIVE $post_phys_directive \
   {STEPS.POST_ROUTE_PHYS_OPT_DESIGN.MORE OPTIONS} "$post_phys_options" \
 ] ${impl_run}
 
-launch_runs ${impl_run} -to_step post_route_phys_opt_design -jobs ${jobs}
+launch_runs ${impl_run} -to_step route_design -jobs ${jobs}
 wait_on_run ${impl_run}
 
 proc check_progress { run errmsg } {
@@ -39,7 +35,7 @@ set WHS [get_property STATS.WHS ${impl_run}]
 
 # run idr or ml flow to close timing
 if {$WNS < 0 || $WHS < 0} {
-  check_file_exists [set sourceFile [${root_dir}/scripts/implementation_idr_ml/${vivado_version}.tcl]]
+  check_file_exists [set sourceFile ${root_dir}/scripts/implementation_idr_ml/${vivado_version}.tcl]
   source $sourceFile
   # expects that $WHS/WNS is re-set
 }
