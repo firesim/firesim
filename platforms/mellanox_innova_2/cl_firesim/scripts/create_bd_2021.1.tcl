57f95c7e31d92b6de61c746d4ade653eebc0f26c
Ethan Gao <eygao@berkeley.edu>
2023-12-04 08:10:28 +0000

    Tightened up memory timings to run at 2400MT instead of 1833MT and deduplicated AXI Tieoff since mellanox is now using same DRAM Frequency as Alveo

--- platforms/mellanox_innova_2/cl_firesim/scripts/create_bd_2021.1.tcl
+++ platforms/mellanox_innova_2/cl_firesim/scripts/create_bd_2021.1.tcl
@@ -270,14 +270,14 @@ proc create_root_design { parentCell firesim_freq } {
   # Create instance: ddr4_0, and set properties
   set ddr4_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:ddr4:2.2 ddr4_0 ]
   set_property -dict [ list \
-   CONFIG.C0.DDR4_CasLatency {12} \
-   CONFIG.C0.DDR4_CasWriteLatency {11} \
+   CONFIG.C0.DDR4_CasLatency {16} \
+   CONFIG.C0.DDR4_CasWriteLatency {12} \
    CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} \
    CONFIG.C0.DDR4_DataWidth {72} \
    CONFIG.C0.DDR4_Ecc {true} \
    CONFIG.C0.DDR4_InputClockPeriod {9996} \
    CONFIG.C0.DDR4_MemoryPart {MT40A1G16WBU-083E} \
-   CONFIG.C0.DDR4_TimePeriod {1428} \
+   CONFIG.C0.DDR4_TimePeriod {833} \
  ] $ddr4_0
 
   # Create instance: firesim_wrapper_0, and set properties
