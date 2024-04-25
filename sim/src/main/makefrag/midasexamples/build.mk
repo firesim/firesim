c1cb49c57fb32db8cbefbe5f15aa1bbf53deb931
Jerry Zhao <jerryz123@berkeley.edu>
2024-04-24 17:12:21 -0700

    Switch goldengate/midas/firesim JAR to use firesim's build.sbt in all cases

--- sim/src/main/makefrag/midasexamples/build.mk
+++ sim/src/main/makefrag/midasexamples/build.mk
@@ -2,7 +2,7 @@
 
 $(FIRRTL_FILE) $(ANNO_FILE): $(TARGET_CP)
 	@mkdir -p $(@D)
-	$(call run_jar_scala_main,$(TARGET_CP),midas.chiselstage.Generator,\
+	$(call run_jar_scala_main,$(firesim_base_dir),$(TARGET_CP),midas.chiselstage.Generator,\
 		--target-dir $(GENERATED_DIR) \
 		--name $(long_name) \
 		--top-module $(DESIGN_PACKAGE).$(DESIGN) \
