d5f78fcf166474fd992dba6b7f8ae93d4ddae04b
abejgonzalez <abe.j.gonza@gmail.com>
2024-04-30 12:05:14 -0700

    Update spacing

--- platforms/xilinx_alveo_u250/cl_firesim/design/overall_fpga_top.v
+++ platforms/xilinx_alveo_u250/cl_firesim/design/overall_fpga_top.v
@@ -18,75 +18,75 @@ module overall_fpga_top(
     input wire resetn
 );
 
-  `define AMBA_AXI4
-  `define AMBA_AXI_CACHE
-  `define AMBA_AXI_PROT
-  `define AMBA_AXI_ID
-  `AMBA_AXI_WIRE(PCIE_M_AXI, 4, 64, 512)
-  `undef AMBA_AXI4
-  `undef AMBA_AXI_CACHE
-  `undef AMBA_AXI_PROT
-  `undef AMBA_AXI_ID
-
-  `define AMBA_AXI_PROT
-  `AMBA_AXI_WIRE(PCIE_M_AXI_LITE, unused, 32, 32)
-  `undef AMBA_AXI_PROT
-
-  wire PCIE_axi_aclk;
-  wire PCIE_axi_aresetn;
-
-  shell shell_i (
-      .pci_express_x16_rxn(pci_express_x16_rxn)
-    , .pci_express_x16_rxp(pci_express_x16_rxp)
-    , .pci_express_x16_txn(pci_express_x16_txn)
-    , .pci_express_x16_txp(pci_express_x16_txp)
-    , .pcie_perstn(pcie_perstn)
-    , .pcie_refclk_clk_n(pcie_refclk_clk_n)
-    , .pcie_refclk_clk_p(pcie_refclk_clk_p)
-
     `define AMBA_AXI4
     `define AMBA_AXI_CACHE
     `define AMBA_AXI_PROT
     `define AMBA_AXI_ID
-    `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI, PCIE_M_AXI)
+    `AMBA_AXI_WIRE(PCIE_M_AXI, 4, 64, 512)
     `undef AMBA_AXI4
     `undef AMBA_AXI_CACHE
     `undef AMBA_AXI_PROT
     `undef AMBA_AXI_ID
 
     `define AMBA_AXI_PROT
-    `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI_LITE, PCIE_M_AXI_LITE)
+    `AMBA_AXI_WIRE(PCIE_M_AXI_LITE, unused, 32, 32)
     `undef AMBA_AXI_PROT
 
-    , .axi_aclk_0(PCIE_axi_aclk)
-    , .axi_aresetn_0(PCIE_axi_aresetn)
-  );
+    wire PCIE_axi_aclk;
+    wire PCIE_axi_aresetn;
 
-  custom_logic custom_logic_i (
-      .resetn(resetn)
+    shell shell_i (
+          .pci_express_x16_rxn(pci_express_x16_rxn)
+        , .pci_express_x16_rxp(pci_express_x16_rxp)
+        , .pci_express_x16_txn(pci_express_x16_txn)
+        , .pci_express_x16_txp(pci_express_x16_txp)
+        , .pcie_perstn(pcie_perstn)
+        , .pcie_refclk_clk_n(pcie_refclk_clk_n)
+        , .pcie_refclk_clk_p(pcie_refclk_clk_p)
 
-    `define DDR4_PAR
-    `DDR4_CONNECT(ddr4_sdram_c0, ddr4_sdram_c0)
-    `define DDR4_PAR
+        `define AMBA_AXI4
+        `define AMBA_AXI_CACHE
+        `define AMBA_AXI_PROT
+        `define AMBA_AXI_ID
+        `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI, PCIE_M_AXI)
+        `undef AMBA_AXI4
+        `undef AMBA_AXI_CACHE
+        `undef AMBA_AXI_PROT
+        `undef AMBA_AXI_ID
 
-    `DIFF_CLK_CONNECT(default_300mhz_clk0, default_300mhz_clk0)
+        `define AMBA_AXI_PROT
+        `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI_LITE, PCIE_M_AXI_LITE)
+        `undef AMBA_AXI_PROT
 
-    `define AMBA_AXI4
-    `define AMBA_AXI_CACHE
-    `define AMBA_AXI_PROT
-    `define AMBA_AXI_ID
-    `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI, PCIE_M_AXI)
-    `undef AMBA_AXI4
-    `undef AMBA_AXI_CACHE
-    `undef AMBA_AXI_PROT
-    `undef AMBA_AXI_ID
+        , .axi_aclk_0(PCIE_axi_aclk)
+        , .axi_aresetn_0(PCIE_axi_aresetn)
+    );
 
-    `define AMBA_AXI_PROT
-    `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI_LITE, PCIE_M_AXI_LITE)
-    `undef AMBA_AXI_PROT
+    custom_logic custom_logic_i (
+          .resetn(resetn)
 
-    , .PCIE_axi_aclk(PCIE_axi_aclk)
-    , .PCIE_axi_aresetn(PCIE_axi_aresetn)
-  );
+        `define DDR4_PAR
+        `DDR4_CONNECT(ddr4_sdram_c0, ddr4_sdram_c0)
+        `define DDR4_PAR
+
+        `DIFF_CLK_CONNECT(default_300mhz_clk0, default_300mhz_clk0)
+
+        `define AMBA_AXI4
+        `define AMBA_AXI_CACHE
+        `define AMBA_AXI_PROT
+        `define AMBA_AXI_ID
+        `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI, PCIE_M_AXI)
+        `undef AMBA_AXI4
+        `undef AMBA_AXI_CACHE
+        `undef AMBA_AXI_PROT
+        `undef AMBA_AXI_ID
+
+        `define AMBA_AXI_PROT
+        `AMBA_AXI_PORT_CONNECTION(PCIE_M_AXI_LITE, PCIE_M_AXI_LITE)
+        `undef AMBA_AXI_PROT
+
+        , .PCIE_axi_aclk(PCIE_axi_aclk)
+        , .PCIE_axi_aresetn(PCIE_axi_aresetn)
+    );
 
 endmodule
