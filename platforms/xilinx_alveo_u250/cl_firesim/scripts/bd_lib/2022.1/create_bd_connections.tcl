220903a2b8f1e3b95b228575ad65a346b2f37a7c
abejgonzalez <abe.j.gonza@gmail.com>
2024-05-01 13:26:42 -0700

    Update BD connections (formatting + fix)

--- platforms/xilinx_alveo_u250/cl_firesim/scripts/bd_lib/2022.1/create_bd_connections.tcl
+++ platforms/xilinx_alveo_u250/cl_firesim/scripts/bd_lib/2022.1/create_bd_connections.tcl
@@ -1,14 +1,10 @@
-connect_bd_intf_net -intf_net pcie_refclk_net [get_bd_intf_ports pcie_refclk] [get_bd_intf_pins util_ds_buf/CLK_IN_D]
-connect_bd_intf_net -intf_net xdma_0_pcie_mgt_net [get_bd_intf_ports pci_express_x16] [get_bd_intf_pins xdma_0/pcie_mgt]
-connect_bd_net -net pcie_perstn_net [get_bd_ports pcie_perstn] [get_bd_pins xdma_0/sys_rst_n]
-connect_bd_net -net xdma_0_axi_aclk [get_bd_ports axi_aclk_0] [get_bd_pins xdma_0/axi_aclk]
-connect_bd_net -net xdma_0_axi_aresetn [get_bd_ports axi_aresetn_0]  [get_bd_pins xdma_0/axi_aresetn]
-
-connect_bd_intf_net -intf_net PCIE_M_AXI [get_bd_intf_ports xdma_0_M_AXI_0] [get_bd_intf_pins xdma_0/M_AXI]
-connect_bd_intf_net -intf_net PCIE_M_AXI_LITE [get_bd_intf_ports xdma_0_M_AXI_LITE_0] [get_bd_intf_pins xdma_0/M_AXI_LITE]
-
-connect_bd_net -net util_ds_buf_IBUF_DS_ODIV2 [get_bd_pins util_ds_buf/IBUF_DS_ODIV2] [get_bd_pins xdma_0/sys_clk]
-
-connect_bd_net -net util_ds_buf_IBUF_OUT [get_bd_pins util_ds_buf/IBUF_OUT] [get_bd_pins xdma_0/sys_clk_gt]
-
-connect_bd_net -net xlconstant_0_dout [get_bd_pins xdma_0/usr_irq_req] [get_bd_pins xlconstant_0/dout]
+connect_bd_intf_net -intf_net pcie_refclk_net               [get_bd_intf_ports pcie_refclk] [get_bd_intf_pins util_ds_buf/CLK_IN_D]
+connect_bd_intf_net -intf_net pcie_mgt_net                  [get_bd_intf_ports pci_express_x16] [get_bd_intf_pins xdma_0/pcie_mgt]
+connect_bd_net           -net pcie_perstn_net               [get_bd_ports pcie_perstn] [get_bd_pins xdma_0/sys_rst_n]
+connect_bd_net           -net util_ds_buf_ibuf_ds_odiv2_net [get_bd_pins util_ds_buf/IBUF_DS_ODIV2] [get_bd_pins xdma_0/sys_clk]
+connect_bd_net           -net util_ds_buf_ibuf_out_net      [get_bd_pins util_ds_buf/IBUF_OUT] [get_bd_pins xdma_0/sys_clk_gt]
+connect_bd_net           -net xlconstant_0_dout_net         [get_bd_pins xdma_0/usr_irq_req] [get_bd_pins xlconstant_0/dout]
+connect_bd_net           -net xdma_0_axi_aclk_net           [get_bd_ports axi_aclk_0]    [get_bd_pins xdma_0/axi_aclk]
+connect_bd_net           -net xdma_0_axi_aresetn_net        [get_bd_ports axi_aresetn_0] [get_bd_pins xdma_0/axi_aresetn]
+connect_bd_intf_net -intf_net pcie_m_axi_net                [get_bd_intf_ports PCIE_M_AXI]      [get_bd_intf_pins xdma_0/M_AXI]
+connect_bd_intf_net -intf_net pcie_m_axi_lite_net           [get_bd_intf_ports PCIE_M_AXI_LITE] [get_bd_intf_pins xdma_0/M_AXI_LITE]
