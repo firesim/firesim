403ab6f90ecca3afe3dc4a4ce611ef6b31efb5bb
joonho hwangbo <joonho.whangbo@berkeley.edu>
2024-06-19 13:46:20 -0700

    Update deploy/buildtools/bitbuilder.py
    Co-authored-by: Abraham Gonzalez <abe.j.gonza@gmail.com>

--- deploy/buildtools/bitbuilder.py
+++ deploy/buildtools/bitbuilder.py
@@ -695,7 +695,7 @@ class XilinxAlveoBitBuilder(BitBuilder):
 
         # store bitfile (and mcs if it exists)
         local(f"cp {bit_path} {tar_staging_path}")
-        if self.build_config.PLATFORM != "xilinx_vcu118":
+        if self.build_config.PLATFORM == "xilinx_vcu118":
             local(f"cp {mcs_path} {tar_staging_path}")
 
         # store metadata string
