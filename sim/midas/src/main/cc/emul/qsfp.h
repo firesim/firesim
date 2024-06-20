0e7340bc597a260e0d230e0e71fe5a94cb2f280d
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-06-19 20:38:30 -0700

    CLANG...

--- sim/midas/src/main/cc/emul/qsfp.h
+++ sim/midas/src/main/cc/emul/qsfp.h
@@ -43,8 +43,10 @@ public:
   bool tx_ready();
   bool channel_up();
 
-  void
-  tick(bool reset, bool tx_valid, std::vector<uint64_t>& tx_bits, bool rx_ready);
+  void tick(bool reset,
+            bool tx_valid,
+            std::vector<uint64_t> &tx_bits,
+            bool rx_ready);
 
   void setup_shmem(char *owned_name, char *other_name);
 
