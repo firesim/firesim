5052a158af8bc0d6a565c028ce9ee6131b2f621a
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-06-22 15:00:48 -0700

    Copy secondary mcs file when building bitstream for vcu118

--- platforms/xilinx_vcu118/build-bitstream.sh
+++ platforms/xilinx_vcu118/build-bitstream.sh
@@ -82,3 +82,4 @@ vivado -mode batch -source ../tcl/build.tcl -tclargs $FREQUENCY $STRATEGY $BOARD
 mkdir -p ../vivado_proj
 cp example_pblock_partition_partial.bit ../vivado_proj/firesim.bit
 cp ../../shell/prebuilt/empty_primary.mcs ../vivado_proj/firesim.mcs
+cp ../../shell/prebuilt/empty_secondary.mcs ../vivado_proj/firesim_secondary.mcs
