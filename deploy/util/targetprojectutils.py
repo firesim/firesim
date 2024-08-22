418e2fa5c7093824b2017f2eeea17c04e43cc3ce
abejgonzalez <abe.j.gonza@gmail.com>
2024-08-21 20:08:57 -0700

    Fix path to makefrag in manager + Add built bitstreams

--- deploy/util/targetprojectutils.py
+++ deploy/util/targetprojectutils.py
@@ -1,3 +1,5 @@
+import os
+
 def extra_target_project_make_args(targetproject: str, deploydir: str) -> str:
     """Create extra make args for target projects that live outside FireSim.
     Eventually will be replaced by using YAML hooks.
@@ -9,8 +11,9 @@ def extra_target_project_make_args(targetproject: str, deploydir: str) -> str:
     Returns:
         any extra args needed for FireSim's make command to function
     """
+    chipyard_dir = f"{deploydir}/../target-design/chipyard" if os.environ.get('FIRESIM_STANDALONE') else f"{deploydir}/../../.."
     if targetproject == "firesim":
-        return f"TARGET_PROJECT_MAKEFRAG={deploydir}/../target-design/chipyard/generators/firechip/chip/src/main/makefrag/{targetproject}"
+        return f"TARGET_PROJECT_MAKEFRAG={chipyard_dir}/generators/firechip/chip/src/main/makefrag/{targetproject}"
     if targetproject == "bridges":
-        return f"TARGET_PROJECT_MAKEFRAG={deploydir}/../target-design/chipyard/generators/firechip/bridgestubs/src/main/makefrag/{targetproject}"
+        return f"TARGET_PROJECT_MAKEFRAG={chipyard_dir}/generators/firechip/bridgestubs/src/main/makefrag/{targetproject}"
     return ""
