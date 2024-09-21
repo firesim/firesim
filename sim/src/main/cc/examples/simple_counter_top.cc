cadd64bf5a6835c07996c35afbdbbcf8e8b8e48e
abejgonzalez <abe.j.gonza@gmail.com>
2024-09-20 17:57:10 -0700

    Simple counter example (clock + reset + max-cycle timeout)

---
+++ sim/src/main/cc/examples/simple_counter_top.cc
@@ -0,0 +1,74 @@
+// See LICENSE for license details
+
+#include "bridges/clock.h"
+#include "bridges/heartbeat.h"
+#include "bridges/peek_poke.h"
+#include "core/simif.h"
+#include "core/simulation.h"
+#include "core/systematic_scheduler.h"
+
+// simulation top-level class. always should extend 'simulation_t' (and implement it's virtual functions).
+// it also extends 'systematic_scheduler_t' to provide the '+max-cycles' simulator CLI arg ("plusarg") to terminate a simulation.
+class simple_counter_top_t : public systematic_scheduler_t, public simulation_t {
+public:
+  simple_counter_top_t(simif_t &simif,
+                widget_registry_t &registry,
+                const std::vector<std::string> &args);
+
+  int simulation_run() override;
+
+  bool simulation_timed_out() override { return !terminated; }
+
+private:
+  simif_t &simif;
+  /// Reference to the peek-poke bridge.
+  peek_poke_t &peek_poke;
+  /// Flag to indicate that the simulation was terminated.
+  bool terminated = false;
+};
+
+simple_counter_top_t::simple_counter_top_t(simif_t &simif,
+                             widget_registry_t &registry,
+                             const std::vector<std::string> &args)
+    : systematic_scheduler_t(args), simulation_t(registry, args), simif(simif),
+      peek_poke(registry.get_widget<peek_poke_t>()) {
+  // helper to verify that simulation is running
+  registry.add_widget(
+      new heartbeat_t(simif, registry.get_widget<clockmodule_t>(), args));
+}
+
+// DOC include start: Loop
+int simple_counter_top_t::simulation_run() {
+  int exit_code = 0;
+  // infinite loop until '+max-cycles' value is reached (within 'systematic_scheduler_t')
+  while (!terminated && !finished_scheduled_tasks()) {
+    // step forward maximum amount of allowable cycles
+    peek_poke.step(get_largest_stepsize(), false);
+    // while the simulation is running N cycles, run all simulation bridges
+    while (!peek_poke.is_done() && !terminated) {
+      for (auto *bridge : registry.get_all_bridges()) {
+	// do bridge work
+        bridge->tick();
+	// if a bridge has finished then fully exit
+        if (bridge->terminate()) {
+          exit_code = bridge->exit_code();
+          terminated = true;
+          break;
+        }
+      }
+    }
+  }
+  return exit_code;
+}
+// DOC include end: Loop
+
+// DOC include start: RegisterWithMain
+// used in firesim's 'main' to instantiate the custom C++ class you want for a simulation.
+// in this case our 'simple_counter_top_t'
+std::unique_ptr<simulation_t>
+create_simulation(simif_t &simif,
+                  widget_registry_t &registry,
+                  const std::vector<std::string> &args) {
+  return std::make_unique<simple_counter_top_t>(simif, registry, args);
+}
+// DOC include end: RegisterWithMain
---
+++ sim/src/main/scala/examples/SimpleCounter.scala
@@ -0,0 +1,82 @@
+// See LICENSE for license details.
+
+package firesim.examples
+
+import chisel3._
+
+import org.chipsalliance.cde.config.Parameters
+
+import firesim.lib.bridges.{PeekPokeBridge, RationalClockBridge, ResetPulseBridge, ResetPulseBridgeParameters}
+import midas.targetutils.{GlobalResetCondition}
+
+// DOC include start: Counter
+// Simple module that when started (i.e. after reset) counts to 1000 then signals 'done'
+class SimpleCounter extends Module {
+  val io = IO(new Bundle {
+    val done = Output(Bool())
+  })
+
+  val cnt = RegInit(0.U(16.W))
+
+  when (cnt === 1000.U) {
+    io.done := true.B
+  } .otherwise {
+    io.done := false.B
+    cnt := cnt + 1.U
+  }
+
+  when (cnt % 100.U === 0.U && cnt =/= 1000.U) {
+    printf("Counter reached %d\n", cnt)
+  }
+}
+// DOC include end: Counter
+
+// DOC include start: TestHarness
+// Simple example harness that runs a simulation. This harness does not terminate the simulation,
+// instead it is the job of the C++ top-level to terminate the simulation.
+class SimpleCounterHarness(implicit val p: Parameters) extends RawModule {
+// DOC include start: ClockResetWire
+  val clock = Wire(Clock())
+  val reset = Wire(Bool())
+// DOC include end: ClockResetWire
+
+  // Boilerplate code:
+  // The peek-poke bridge must still be instantiated even though it's
+  // functionally unused. This will be removed in a future PR.
+  val dummy = WireInit(false.B)
+  val peekPokeBridge = PeekPokeBridge(clock, dummy)
+
+// DOC include start: Bridges
+  // Drive with default clock provided by a bridge.
+  clock := RationalClockBridge().io.clocks.head
+
+  // Drive reset with a bridge.
+  val resetBridge = Module(new ResetPulseBridge(ResetPulseBridgeParameters()))
+  // In effect, the bridge counts the length of the reset in terms of this clock.
+  resetBridge.io.clock := clock
+  // Drive with pulsed reset for a default amount of time.
+  reset := resetBridge.io.reset
+// DOC include end: Bridges
+
+  // Boilerplate code:
+  // Ensures FireSim-synthesized assertions and instrumentation is disabled
+  // while 'resetBridge.io.reset' is asserted.  This ensures assertions do not fire at
+  // time zero in the event their local reset is delayed (typically because it
+  // has been pipelined).
+  GlobalResetCondition(resetBridge.io.reset)
+
+// DOC include start: CL
+  // Custom logic.
+  withClockAndReset(clock, reset) {
+    val simpleCounter = Module(new SimpleCounter)
+
+    // Print once when counter 'done' signal asserted.
+    val printDone = RegInit(false.B)
+    when (simpleCounter.io.done && !printDone) {
+      printDone := true.B
+      printf("Counter has completed!\n")
+    }
+  }
+// DOC include end: CL
+}
+// DOC include end: TestHarness
